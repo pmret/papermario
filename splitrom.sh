aa0693e1d5ee9ce1a055cdd6705a75768b5fe816
Retro <admin@retroreversing.com>
2019-03-10 12:20:29 +0000

    updated n64split for game music files

--- splitrom.sh
+++ splitrom.sh
@@ -1,1 +1,10 @@
-./n64split -v -o papermario -c PAPER_MARIO.u.yaml Paper\ Mario\ \(U\)\ \[\!\].z64
+if [ ! -f ./n64splitter/bin/n64split ];
+then
+    cd ./n64splitter
+    mkdir -p ./bin
+    export C_INCLUDE_PATH=../n64splitter:./ext:/usr/local/Cellar/capstone/4.0.1/include/:/usr/local/Cellar/libyaml/0.2.1/include
+    export LIBRARY_PATH=/usr/local/opt/capstone/lib/:/usr/local/Cellar/libyaml/0.2.1/lib:/usr/local/Cellar/libpng/1.6.36/lib
+    make
+    cd ..
+fi
+./n64splitter/bin/n64split -v -o papermario -c PAPER_MARIO.u.yaml Paper\ Mario\ \(U\)\ \[\!\].z64
