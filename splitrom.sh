96e9011228170d2c7c4d5696c8de1af6fd863b38
Ethan Roseman <ethteck@gmail.com>
2020-05-08 00:05:43 -0400

    Rename new flag, skip writing .ld by default too

--- splitrom.sh
+++ splitrom.sh
@@ -1,3 +1,3 @@
 rm -rf papermario/bin
 rm -rf papermario/bgm
-./n64splitter/bin/n64split -z -v -o papermario -c PAPER_MARIO.u.yaml baserom.z64
+./n64splitter/bin/n64split -b -v -o papermario -c PAPER_MARIO.u.yaml baserom.z64
