80f3931bff718f589e151d15a533e60a78e3d720
Ethan Roseman <ethteck@gmail.com>
2020-05-12 00:56:46 -0400

    Symbol rename

--- papermario/src/code_dd930_len_1c0.c
+++ papermario/src/code_dd930_len_1c0.c
@@ -1,29 +1,25 @@
-typedef struct
-{
-  int unk0;
-  int unk4;
-  int unk8;
-  int unkC;
+typedef struct {
+    int unk0;
+    int unk4;
+    int unk8;
+    int unkC;
 } struct_80147230;
 
-extern struct_80147230 unk1;
-extern struct_80147230 unk2;
+extern struct_80147230 D_8014F140;
+extern struct_80147230 D_8015C7C0;
 
-void func_80147230(void)
-{
-  unk2 = unk1;
+void func_80147230(void) {
+    D_8015C7C0 = D_8014F140;
 }
 
-void __attribute__((naked)) UpdateMusicPlayers(void)
-{
-  __asm__(
+void __attribute__((naked)) UpdateMusicPlayers(void) {
+    __asm__(
     ".include \"asm/code_dd930_len_1c0/UpdateMusicPlayers.s\"\n"
-  );
+    );
 }
 
-void __attribute__((naked)) play_ambient_sounds(void)
-{
-  __asm__(
+void __attribute__((naked)) play_ambient_sounds(void) {
+    __asm__(
     ".include \"asm/code_dd930_len_1c0/play_ambient_sounds.s\"\n"
-  );
+    );
 }
