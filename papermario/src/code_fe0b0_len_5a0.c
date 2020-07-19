9d777793c7eee650afcae6646d490e982f72ef37
dark-samus <mathmcclintic@gmail.com>
2020-07-19 00:15:50 -0400

    rename gGameStatus and add INCLUDE_ASM macro

--- papermario/src/code_fe0b0_len_5a0.c
+++ papermario/src/code_fe0b0_len_5a0.c
@@ -1,10 +1,6 @@
 #include "common.h"
 
-void __attribute__((naked)) SetSpriteShading(void) {
-    __asm__(
-".include \"asm/code_fe0b0_len_5a0/SetSpriteShading.s\"\n"
-    );
-}
+INCLUDE_ASM(code_fe0b0_len_5a0, SetSpriteShading);
 
 s32 EnableSpriteShading(ScriptContext* script) {
     if (get_variable(script, *script->ptrReadPos) != 0) {
@@ -16,49 +12,49 @@ s32 EnableSpriteShading(ScriptContext* script) {
 }
 
 s32 getDemoState(ScriptContext* script) {
-    set_variable(script, *script->ptrReadPos, (*gGameStatus)->demoState);
+    set_variable(script, *script->ptrReadPos, (*gGameStatusPtr)->demoState);
     return 2;
 }
 
 s32 DemoPressButton(ScriptContext* script) {
-    (*gGameStatus)->demoButtonInput |= get_variable(script, *script->ptrReadPos);
+    (*gGameStatusPtr)->demoButtonInput |= get_variable(script, *script->ptrReadPos);
     return 2;
 }
 
 s32 DemoReleaseButton(ScriptContext* script) {
-    (*gGameStatus)->demoButtonInput &= ~get_variable(script, *script->ptrReadPos);
+    (*gGameStatusPtr)->demoButtonInput &= ~get_variable(script, *script->ptrReadPos);
     return 2;
 }
 
 s32 DemoSetButtons(ScriptContext* script) {
-    (*gGameStatus)->demoButtonInput = get_variable(script, *script->ptrReadPos);
+    (*gGameStatusPtr)->demoButtonInput = get_variable(script, *script->ptrReadPos);
     return 2;
 }
 
 s32 DemoJoystickRadial(ScriptContext* script) {
-    float a;
-    float b;
+    f32 a;
+    f32 b;
     s32* thisPos = script->ptrReadPos;
 
     a = get_float_variable(script, *thisPos++);
     b = get_float_variable(script, *thisPos++);
 
-    (*gGameStatus)->demoStickX = a * sin_deg(b);
-    (*gGameStatus)->demoStickY = a * cos_deg(b);
+    (*gGameStatusPtr)->demoStickX = a * sin_deg(b);
+    (*gGameStatusPtr)->demoStickY = a * cos_deg(b);
     
     return 2;
 }
 
 s32 DemoJoystickXY(ScriptContext* script) {
-    float a;
-    float b;
+    f32 a;
+    f32 b;
     s32* thisPos = script->ptrReadPos;
 
     a = get_float_variable(script, *thisPos++);
     b = get_float_variable(script, *thisPos++);
 
-    (*gGameStatus)->demoStickX = a;
-    (*gGameStatus)->demoStickY = b;
+    (*gGameStatusPtr)->demoStickX = a;
+    (*gGameStatusPtr)->demoStickY = b;
 
     return 2;
 }
