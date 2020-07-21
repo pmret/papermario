39e8ee477f66e49169297ffa8a1fac396a082fde
dark-samus <mathmcclintic@gmail.com>
2020-07-21 04:42:21 -0400

    split giantchunk.s in yaml, fix diff.py for some situations, fix some structs and include commented source for fishy matching

--- papermario/src/code_dd930_len_1c0.c
+++ papermario/src/code_dd930_len_1c0.c
@@ -1,7 +1,8 @@
 #include "common.h"
 
 typedef struct {
-    s32 unk0;
+    s16 unk0;
+    s16 unk2;
     s32 unk4;
     s32 unk8;
     s32 unkC;
@@ -17,3 +18,28 @@ void func_80147230(void) {
 INCLUDE_ASM(code_dd930_len_1c0, UpdateMusicPlayers);
 
 INCLUDE_ASM(code_dd930_len_1c0, play_ambient_sounds);
+
+// matches, but clearly isn't correct. Investigate later.
+/*int play_ambient_sounds(int a0, int a1)
+{
+  struct_80147230 *temp2 = &D_8015C7C0;
+  struct_80147230 **temp3 = &temp2;
+
+  if ((*gGameStatusPtr)->audio_unk == 0)
+  {
+    func_800554A4(temp2->unk8);
+    temp2->unk0 &= ~1;
+    return 1;
+  }
+
+
+  if (temp2->unk8 == a0)
+  {
+    return 2;
+  }
+
+  temp2->unk8 = a0;
+  temp2->unk4 = a1;
+  temp2->unk2 = 1;
+  return 1;
+}*/
