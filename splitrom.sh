0782fa07a335d79d1fa511a10df1c17438f0e88a
Ethan Roseman <ethteck@gmail.com>
2020-08-12 21:56:23 -0400

    Refactoring stuff

--- splitrom.sh
+++ splitrom.sh
@@ -1,3 +1,3 @@
-rm -rf papermario/bin
-rm -rf papermario/bgm
-./n64splitter/bin/n64split -b -v -o papermario -c PAPER_MARIO.u.yaml baserom.z64
+rm -rf bin
+rm -rf bgm
+./tools/n64splitter/bin/n64split -b -v -o . -c tools/n64split.yaml baserom.z64
