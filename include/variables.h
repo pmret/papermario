6aa628f3acb63e3271966cc523575fc4a8709788
Jdog <jwiens@JOSH-DESKTOP.localdomain>
2020-08-13 19:53:38 -0700

    Added gCurrentScriptList and some more matching funcs

--- include/variables.h
+++ include/variables.h
@@ -10,6 +10,7 @@ extern player_data gPlayerData;
 extern player_status gPlayerStatus;
 extern game_status* gGameStatusPtr[1];
 extern item_table_entry gItemTable[364];
+extern s32* gCurrentScriptList;
 extern s8 D_800A0900;
 extern s16* D_80151328;
 extern s16 D_8010CD10;
--- src/code_e79b0_len_1920.c
+++ src/code_e79b0_len_1920.c
@@ -26,7 +26,19 @@ INCLUDE_ASM(code_e79b0_len_1920, func_802C3EE4);
 
 INCLUDE_ASM(code_e79b0_len_1920, kill_script);
 
-INCLUDE_ASM(code_e79b0_len_1920, kill_script_by_ID);
+void* kill_script_by_ID(s32 id) {
+    s32 i;
+    script_context* scriptContextPtr;
+
+    for (i=0; i < 128; i++) {
+        if (gCurrentScriptList[i] != 0) {
+            scriptContextPtr = (script_context*) gCurrentScriptList[i];
+            if (scriptContextPtr->uniqueID == id) {
+                kill_script(scriptContextPtr);
+            }
+        }
+    }
+}
 
 INCLUDE_ASM(code_e79b0_len_1920, kill_all_scripts);
 
@@ -68,9 +80,24 @@ INCLUDE_ASM(code_e79b0_len_1920, suspend_group_others);
 
 INCLUDE_ASM(code_e79b0_len_1920, resume_group_others);
 
-INCLUDE_ASM(code_e79b0_len_1920, get_script_by_index);
+s32 get_script_by_index(s32 index) {
+    return gCurrentScriptList[index];
+}
 
-INCLUDE_ASM(code_e79b0_len_1920, get_script_by_id);
+void* get_script_by_id(s32 id) {
+    s32 i;
+    script_context* scriptContextPtr;
+
+    for (i=0; i < 128; i++) {
+        if (gCurrentScriptList[i] != 0) {
+            scriptContextPtr = (script_context*) gCurrentScriptList[i];
+            if (scriptContextPtr->uniqueID == id) {
+                return scriptContextPtr;
+            }
+        }
+    }
+    return 0;
+}
 
 INCLUDE_ASM(code_e79b0_len_1920, set_script_flags);
 
