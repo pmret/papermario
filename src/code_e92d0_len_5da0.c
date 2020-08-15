d6986180a7f6e3b001b1ece6963b3b3af66dc039
Alex Bates <hi@imalex.xyz>
2020-08-15 15:55:41 +0100

    improve match

--- src/code_e92d0_len_5da0.c
+++ src/code_e92d0_len_5da0.c
@@ -280,21 +280,18 @@ s32 si_handle_OR_const(script_context* script) {
 INCLUDE_ASM(code_e92d0_len_5da0, si_handle_call);
 /*
 s32 si_handle_call(script_context* script) {
-    s32 (*blockingFn)(script_context*, u8);
-    s32 (*callFn)(script_context*, u8);
     bytecode* ptrReadPos = script->ptrReadPos;
 
     if (!script->blocked) {
         // Call function.
-        script->callFunction = callFn = get_variable(script, *ptrReadPos++);
+        script->callFunction = get_variable(script, *ptrReadPos++);
         script->ptrReadPos = ptrReadPos;
         script->blocked = 1;
         script->currentArgc--;
-        return callFn(script, 1);
+        return (script->callFunction)(script, 1);
     } else {
         // Keep calling function, as it's blocking.
-        blockingFn = script->callFunction;
-        return blockingFn(script, 0);
+        return (script->callFunction)(script, 0);
     }
 }
 */
