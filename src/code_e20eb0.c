65f1db2e87b4f94a1a2ef649ffcdc9fe6b4176a8
Jdog <jwiens@JOSH-DESKTOP.localdomain>
2020-08-19 09:55:52 -0700

    Match func_802B7000

--- src/code_e20eb0.c
+++ src/code_e20eb0.c
@@ -1,6 +1,20 @@
 #include "common.h"
 
-INCLUDE_ASM(code_e20eb0, func_802B7000);
+// TODO: Figure out what this function is doing
+s32 func_802B7000(f32 angle) {
+    s32 angleDiff;
+
+    if (270.0f <= angle) {
+        angleDiff = 360.0f - angle;
+    } else if (180.0f <= angle) {
+        angleDiff = angle - 180.0f;
+    } else if (90.0f <= angle) {
+        angleDiff = 180.0f - angle;
+    } else {
+        angleDiff = angle;
+    }
+    return 0xFF - angleDiff - angleDiff;
+}
 
 INCLUDE_ASM(code_e20eb0, func_802B70B4);
 
