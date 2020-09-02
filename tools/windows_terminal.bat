ab754807fc542853f738611c3c48aeb36688b857
sonicspiral <49568191+sonicspiral@users.noreply.github.com>
2020-09-01 19:26:28 -0700

    delete windows_terminal.bat

--- tools/windows_terminal.bat
+++
@@ -1,6 +0,0 @@
-:: replace with your distro 
-set distro="Ubuntu-18.04"
-wt -p %distro% -d "../" ; ^
-split-pane -p %distro% -d "../" -V --title "diff.py" ; ^
-new-tab -p %distro% -d . --title "mips_to_c.py" ; ^
-focus-tab -t 0
