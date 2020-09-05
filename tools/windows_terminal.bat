39d8a13f73046ebd237a2c1604344f42b3ed1d19
sonicspiral <>
2020-09-05 12:10:35 -0700

    convert starter windows terminal to bat * note the requirement to run wsl --set-default <Distro> first

---
+++ tools/windows_terminal.bat
@@ -0,0 +1,2 @@
+REM first set your default distro using: wsl --set-default <Distro>
+wt --title "diff.py" -d "../" ; split-pane -d "../" -V ; new-tab --title "mips_to_c.py" -d "../" ; focus-tab -t 0
