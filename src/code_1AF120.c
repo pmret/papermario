3fcceae5be3e3167f119b31a6d4427ec6f26c39b
Ethan Roseman <ethteck@gmail.com>
2020-10-15 18:44:42 -0400

    format

--- src/code_1AF120.c
+++ src/code_1AF120.c
@@ -5,7 +5,7 @@ ApiStatus GetDamageIntensity(ScriptInstance* script, s32 isInitialCall) {
         script->varTable[0] = 0;
     } else if (gBattleStatus.lastAttackDamage < 7) {
         script->varTable[0] = 1;
-    } else if (gBattleStatus.lastAttackDamage < 10){
+    } else if (gBattleStatus.lastAttackDamage < 10) {
         script->varTable[0] = 2;
     } else {
         script->varTable[0] = 3;
