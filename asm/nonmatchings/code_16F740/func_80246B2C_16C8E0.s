f33010a8c2a28524c508fe4e51aead1652052f08
Ethan Roseman <ethteck@gmail.com>
2020-10-17 22:28:10 -0400

    deleting junk

--- asm/nonmatchings/code_16F740/func_80246B2C_16C8E0.s
+++
@@ -1,6 +0,0 @@
-.set noat      # allow manual use of $at
-.set noreorder # don't insert nops after branches
-
-glabel func_80246B2C_16C8E0
-/* 17540C 80246B2C 03E00008 */  jr        $ra
-/* 175410 80246B30 00000000 */   nop      
