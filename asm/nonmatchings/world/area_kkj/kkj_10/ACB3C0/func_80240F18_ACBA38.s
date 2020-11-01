3a134d6a7d5f5fb2166708e365e528ba4f3579f7
Ethan Roseman <ethteck@gmail.com>
2020-11-01 18:37:28 -0500

    gPlayerStatus offsets

--- asm/nonmatchings/world/area_kkj/kkj_10/ACB3C0/func_80240F18_ACBA38.s
+++ asm/nonmatchings/world/area_kkj/kkj_10/ACB3C0/func_80240F18_ACBA38.s
@@ -5,8 +5,8 @@ glabel func_80240F18_ACBA38
 /* ACBA38 80240F18 27BDFFE8 */  addiu     $sp, $sp, -0x18
 /* ACBA3C 80240F1C AFBF0010 */  sw        $ra, 0x10($sp)
 /* ACBA40 80240F20 8C82000C */  lw        $v0, 0xc($a0)
-/* ACBA44 80240F24 3C068011 */  lui       $a2, 0x8011
-/* ACBA48 80240F28 80C6EFD4 */  lb        $a2, -0x102c($a2)
+/* ACBA44 80240F24 3C068011 */  lui       $a2, %hi(gPlayerStatus+0xC)
+/* ACBA48 80240F28 80C6EFD4 */  lb        $a2, %lo(gPlayerStatus+0xC)($a2)
 /* ACBA4C 80240F2C 0C0B2026 */  jal       set_variable
 /* ACBA50 80240F30 8C450000 */   lw       $a1, ($v0)
 /* ACBA54 80240F34 8FBF0010 */  lw        $ra, 0x10($sp)
--- asm/nonmatchings/world/area_kkj/kkj_20/AFDE00/func_802405BC_AFE0FC.s
+++ asm/nonmatchings/world/area_kkj/kkj_20/AFDE00/func_802405BC_AFE0FC.s
@@ -5,8 +5,8 @@ glabel func_802405BC_AFE0FC
 /* AFE0FC 802405BC 27BDFFE8 */  addiu     $sp, $sp, -0x18
 /* AFE100 802405C0 AFBF0010 */  sw        $ra, 0x10($sp)
 /* AFE104 802405C4 8C82000C */  lw        $v0, 0xc($a0)
-/* AFE108 802405C8 3C068011 */  lui       $a2, 0x8011
-/* AFE10C 802405CC 80C6EFD4 */  lb        $a2, -0x102c($a2)
+/* AFE108 802405C8 3C068011 */  lui       $a2, %hi(gPlayerStatus+0xC)
+/* AFE10C 802405CC 80C6EFD4 */  lb        $a2, %lo(gPlayerStatus+0xC)($a2)
 /* AFE110 802405D0 0C0B2026 */  jal       set_variable
 /* AFE114 802405D4 8C450000 */   lw       $a1, ($v0)
 /* AFE118 802405D8 8FBF0010 */  lw        $ra, 0x10($sp)
