087b30ab35ba833d6064221456adbbfc0ab10af9
Ethan Roseman <ethteck@gmail.com>
2020-11-02 16:04:59 -0500

    a couple more kmrs

--- src/world/area_kmr/kmr_10/8D84D0.c
+++ src/world/area_kmr/kmr_10/8D84D0.c
@@ -8,7 +8,22 @@ INCLUDE_ASM(s32, "world/area_kmr/kmr_10/8D84D0", func_80240000_8D84D0);
 
 #include "world/common/CheckItemFlags40.inc.c"
 
+#ifdef NON_MATCHING
+ApiStatus func_802402BC_8D878C(ScriptInstance* script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
+    Entity *entity = get_entity_by_index(get_variable(script, *args++));
+    s32 something = 0x1E4;
+
+    if (entity == NULL) {
+        return ApiStatus_BLOCK;
+    }
+
+    play_model_animation(entity->unk_14, 0x1E4);
+    return ApiStatus_DONE2;
+}
+#else
 INCLUDE_ASM(s32, "world/area_kmr/kmr_10/8D84D0", func_802402BC_8D878C);
+#endif
 
 ApiStatus func_80240304_8D87D4(ScriptInstance* script, s32 isInitialCall) {
     Bytecode* args = script->ptrReadPos;
