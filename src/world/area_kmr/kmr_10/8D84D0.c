d16d6a3c1a7f16fd3afb02e377d37b670c9f2458
Ethan Roseman <ethteck@gmail.com>
2020-11-02 16:31:54 -0500

    comment

--- src/world/area_kmr/kmr_10/8D84D0.c
+++ src/world/area_kmr/kmr_10/8D84D0.c
@@ -9,6 +9,8 @@ INCLUDE_ASM(s32, "world/area_kmr/kmr_10/8D84D0", func_80240000_8D84D0);
 #include "world/common/CheckItemFlags40.inc.c"
 
 #ifdef NON_MATCHING
+// 0x1E4 is probably a linker constant. We need to learn more about animations,
+// play_model_animation, and entity->unk_14 before we can tackle this
 ApiStatus func_802402BC_8D878C(ScriptInstance* script, s32 isInitialCall) {
     Bytecode* args = script->ptrReadPos;
     Entity* entity = get_entity_by_index(get_variable(script, *args++));
