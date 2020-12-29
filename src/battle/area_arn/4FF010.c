a15093ed6bff020c62825e215bc3ace074e35bcf
Ethan Roseman <ethteck@gmail.com>
2020-12-30 06:23:49 +0900

    1 more

--- src/battle/area_arn/4FF010.c
+++ src/battle/area_arn/4FF010.c
@@ -6,4 +6,15 @@
 #include "common/StartRumbleWithParams.inc.c"
 #define NAMESPACE b_area_arn
 
-INCLUDE_ASM(s32, "battle/area_arn/4FF010", func_80218618_4FF068);
+ApiStatus func_80218618_4FF068(ScriptInstance* script, s32 isInitialCall) {
+    s32 temp_s0 = get_variable(script, *script->ptrReadPos);
+    Actor* actor = get_actor(script->owner1.actorID);
+
+    if (temp_s0 == 0) {
+        actor->staticActorData->spinSmashReq = 4;
+    } else {
+        actor->staticActorData->spinSmashReq = 0;
+    }
+
+    return ApiStatus_DONE2;
+}
