a9a3ae4682da7eabe7e2ad2b0f5b6dbb0e7cee0f
Ethan Roseman <ethteck@gmail.com>
2021-01-10 19:43:17 +0900

    another :fish:

---
+++ src/code_406B40.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+INCLUDE_ASM(s32, "code_406B40", func_E011C000);
+
+INCLUDE_ASM(s32, "code_406B40", func_E011C0F4);
+
+INCLUDE_ASM(s32, "code_406B40", func_E011C0FC);
+
+INCLUDE_ASM(s32, "code_406B40", func_E011C17C);
+
+INCLUDE_ASM(s32, "code_406B40", func_E011C1C4);
