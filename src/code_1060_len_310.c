f670e0e7fa0f3ce5c91e3b32b8f6f163c6a10db4
Zelllll <56516451+Zelllll@users.noreply.github.com>
2021-01-14 18:49:15 -0600

    code_7345A0 OK

--- src/code_1060_len_310.c
+++ src/code_1060_len_310.c
@@ -1,6 +1,14 @@
 #include "common.h"
 
-INCLUDE_ASM(s32, "code_1060_len_310", func_80025C60);
+#define ASCII_TO_U32(a, b, c, d) ((u32)((a << 24) | (b << 16) | (c << 8) | (d << 0)))
+
+void func_80025C60(void) {
+    OSPiHandle** handle = &carthandle;
+
+    osEPiWriteIo(*handle, 0xB3FF0014, 0);
+    osEPiWriteIo(*handle, 0xB3FF0004, 0);
+    osEPiWriteIo(*handle, 0xB3FF0000, ASCII_TO_U32('I', 'S', '6', '4'));
+}
 
 INCLUDE_ASM(s32, "code_1060_len_310", func_80025CC0);
 
--- src/code_7345A0.c
+++ src/code_7345A0.c
@@ -1,5 +1,27 @@
 #include "common.h"
 
-INCLUDE_ASM(s32, "code_7345A0", func_802A1000_7345A0);
+ApiStatus func_802A1000_7345A0(ScriptInstance *script, s32 isInitialCall) {
+    script->varTable[0] = BATTLE_STATUS->unk_83;
+    script->varTable[1] = 15;
 
-INCLUDE_ASM(s32, "code_7345A0", func_802A1050_7345F0);
+    if (is_ability_active(Ability_BERSERKER)) {
+        script->varTable[0] = 0;
+        script->varTable[1] = 40;
+    }
+
+    return ApiStatus_DONE2;
+}
+
+ApiStatus func_802A1050_7345F0(ScriptInstance *script, s32 isInitialCall) {
+    script->varTable[0] = 0;
+
+    if (is_ability_active(Ability_RIGHT_ON)) {
+        script->varTable[0] = 1;
+    }
+
+    if(gBattleStatus.flags1 & 0x1000) {
+        script->varTable[0] = 1;
+    }
+
+    return ApiStatus_DONE2;
+}
