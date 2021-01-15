08e1fc532f676411222d772350eb6ecbddd76720
Zelllll <56516451+Zelllll@users.noreply.github.com>
2021-01-14 19:07:47 -0600

    renamed char macro thingy

--- src/code_1060_len_310.c
+++ src/code_1060_len_310.c
@@ -1,13 +1,13 @@
 #include "common.h"
 
-#define ASCII_TO_U32(a, b, c, d) ((u32)((a << 24) | (b << 16) | (c << 8) | (d << 0)))
+#define U32_CHAR_LITERAL(a, b, c, d) ((u32)((a << 24) | (b << 16) | (c << 8) | (d << 0)))
 
 void func_80025C60(void) {
     OSPiHandle** handle = &carthandle;
 
     osEPiWriteIo(*handle, 0xB3FF0014, 0);
     osEPiWriteIo(*handle, 0xB3FF0004, 0);
-    osEPiWriteIo(*handle, 0xB3FF0000, ASCII_TO_U32('I', 'S', '6', '4'));
+    osEPiWriteIo(*handle, 0xB3FF0000, U32_CHAR_LITERAL('I', 'S', '6', '4'));
 }
 
 INCLUDE_ASM(s32, "code_1060_len_310", func_80025CC0);
