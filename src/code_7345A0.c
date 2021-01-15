4c5fc9b05f62e95e36981daa4fb9917e4a71b0ab
Zelllll <56516451+Zelllll@users.noreply.github.com>
2021-01-14 18:53:47 -0600

    format files

--- src/code_7345A0.c
+++ src/code_7345A0.c
@@ -1,6 +1,6 @@
 #include "common.h"
 
-ApiStatus func_802A1000_7345A0(ScriptInstance *script, s32 isInitialCall) {
+ApiStatus func_802A1000_7345A0(ScriptInstance* script, s32 isInitialCall) {
     script->varTable[0] = BATTLE_STATUS->unk_83;
     script->varTable[1] = 15;
 
@@ -12,14 +12,14 @@ ApiStatus func_802A1000_7345A0(ScriptInstance *script, s32 isInitialCall) {
     return ApiStatus_DONE2;
 }
 
-ApiStatus func_802A1050_7345F0(ScriptInstance *script, s32 isInitialCall) {
+ApiStatus func_802A1050_7345F0(ScriptInstance* script, s32 isInitialCall) {
     script->varTable[0] = 0;
 
     if (is_ability_active(Ability_RIGHT_ON)) {
         script->varTable[0] = 1;
     }
 
-    if(gBattleStatus.flags1 & 0x1000) {
+    if (gBattleStatus.flags1 & 0x1000) {
         script->varTable[0] = 1;
     }
 
