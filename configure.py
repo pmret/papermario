267acf3ad32e70de430371150222195503815c66
Alex Bates <hi@imalex.xyz>
2021-02-01 20:01:34 +0000

    fix configure.py on old python for github actions

--- configure.py
+++ configure.py
@@ -454,4 +454,5 @@ async def main():
     print(f"to compile '{TARGET}.z64'.")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
