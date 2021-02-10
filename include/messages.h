b2d0b2c6c8734a318c5402a6778182ff1ce6fd99
Alex Bates <hi@imalex.xyz>
2021-02-10 13:45:00 +0000

    MESSAGE_ID macro: 0x10 -> 16

--- include/messages.h
+++ include/messages.h
@@ -6,6 +6,6 @@
 typedef s32 MessageID;
 
 // Prefer editing splat.yaml's msg ids section than using this directly!
-#define MESSAGE_ID(section, index) (((section << 0x10) + index))
+#define MESSAGE_ID(section, index) (((section << 16) + index))
 
 #endif
