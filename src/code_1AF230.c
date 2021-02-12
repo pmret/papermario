1dd4568b6b22dcc841df0fe7d368b8d308c091f8
brotenko@gmail.com <brotenko@gmail.com>
2021-02-13 00:40:55 +0100

    decompile LoadStarPowerScript

--- src/code_1AF230.c
+++ src/code_1AF230.c
@@ -1,3 +1,21 @@
 #include "common.h"
 
-INCLUDE_ASM(s32, "code_1AF230", LoadStarPowerScript);
+typedef struct StarPower { void* dmaStart; void* dmaEnd; void* dmaDest; Bytecode* init; } StarPower;
+
+extern StarPower D_8029C7D0[13];
+
+typedef struct WhateverThisIs { s8 starpoints; s8 a; s8 b; s8 c; s32 w; s32 x; s32 y; s32 z; } WhateverThisIs;
+
+extern WhateverThisIs D_8008F071[];
+
+ApiStatus LoadStarPowerScript(ScriptInstance *script, s32 isInitialCall) {
+    BattleStatus *battleStatus = &gBattleStatus;
+    PlayerData *playerData = &gPlayerData;
+    s16 selectedItemID;
+
+    playerData->specialBarsFilled -= D_8008F071[battleStatus->selectedMoveID].starpoints * 256;
+    selectedItemID = battleStatus->selectedItemID;
+    dma_copy((&D_8029C7D0[selectedItemID])->dmaStart, (&D_8029C7D0[selectedItemID])->dmaEnd, (&D_8029C7D0[selectedItemID])->dmaDest);
+    script->varTable[0] = (&D_8029C7D0[selectedItemID])->init;
+    return ApiStatus_DONE2;
+}
