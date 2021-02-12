0ca39b161b0a9f5552e5bd5de96ddf1aa5110a18
PonderMan <ponmander@gmail.com>
2021-02-12 07:23:23 -0600

    Fixed Syntax

--- src/code_EF2650.c
+++ src/code_EF2650.c
@@ -3,6 +3,6 @@
 extern s16 D_80169B16;
 
 ApiStatus func_80240040_EF2650(ScriptInstance* script, s32 isInitialCall) {
-    func_802D5468(script,*script->ptrReadPos, D_80169B16);
-    return 2;
+    func_802D5468(script, *script->ptrReadPos, D_80169B16);
+    return ApiStatus_DONE2;
 }
