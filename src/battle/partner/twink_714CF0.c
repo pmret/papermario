5f91533980818e091c20dbb7262217300d44bb53
PonderMan <ponmander@gmail.com>
2021-02-12 11:17:49 -0600

    80238028 Part 1

--- src/battle/partner/twink_714CF0.c
+++ src/battle/partner/twink_714CF0.c
@@ -1,5 +1,12 @@
 #include "common.h"
 
-INCLUDE_ASM(s32, "battle/partner/twink_714CF0", func_80238000_714CF0);
-
+ApiStatus func_80238000_714CF0(ScriptInstance *arg0) {
+    BattleStatus* battleStatus = BATTLE_STATUS;
+    if ((battleStatus->flags2 & 0x40) != 0) {
+        arg0->varTable[0] = 1;
+    } else {
+        arg0->varTable[0] = 0;
+    }
+    return ApiStatus_DONE2;
+}
 INCLUDE_ASM(s32, "battle/partner/twink_714CF0", func_80238028_714D18);
