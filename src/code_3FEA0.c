4f0b5d2c413d0f80968bebc2058aa7cf15ab0ef1
Ethan Roseman <ethteck@gmail.com>
2021-03-04 17:14:45 +0900

    file split, moving things into os

--- src/code_3FEA0.c
+++
@@ -1,9 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM(s32, "code_3FEA0", _Printf);
-
-INCLUDE_ASM(s32, "code_3FEA0", _Putfld);
-
-INCLUDE_ASM(int, "code_3FEA0", sprintf, char* s, const char* fmt, ...);
-
-INCLUDE_ASM(s32, "code_3FEA0", proutSprintf);
---
+++ src/os/code_3CF80.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+INCLUDE_ASM(s32, "os/code_3CF80", osContInit, OSMesgQueue* arg0, u8* arg1, OSContStatus* arg2);
+
+INCLUDE_ASM(s32, "os/code_3CF80", osContGetInitData);
+
+INCLUDE_ASM(s32, "os/code_3CF80", osPackRequestData);
