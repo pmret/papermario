0488245e2733bcca986dc68c5abeeeb04de3b50f
Ethan Roseman <ethteck@gmail.com>
2021-03-08 00:45:33 +0900

    git subrepo pull tools/splat
    subrepo:
      subdir:   "tools/splat"
      merged:   "4e79cc1a4e"
    upstream:
      origin:   "https://github.com/ethteck/splat.git"
      branch:   "master"
      commit:   "4e79cc1a4e"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "2f68596"

--- tools/splat/segtypes/n64/code.py
+++ tools/splat/segtypes/n64/code.py
@@ -326,7 +326,7 @@ class N64SegCode(N64Segment):
             new_segment = subsegment_class(start, end, name, typ, vram, args, parent)
             ret.append(new_segment)
 
-            if not typ.startswith("."):
+            if typ == ("c"):
                 base_segments[name] = new_segment
 
             prev_start = start
