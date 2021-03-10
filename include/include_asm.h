c0b67ce0d48a84b2faee2596399ad69e3526dea0
Ethan Roseman <ethteck@gmail.com>
2021-03-10 17:05:27 +0900

    Permuter fix 3

--- include/include_asm.h
+++ include/include_asm.h
@@ -4,17 +4,13 @@
 #define STRINGIFY_(x) #x
 #define STRINGIFY(x) STRINGIFY_(x)
 
-#if !defined(SPLAT) && !defined(__CTX__)
+#if !defined(SPLAT) && !defined(__CTX__) && !defined(PERMUTER)
 #ifndef INCLUDE_ASM
 #define INCLUDE_ASM(TYPE, FOLDER, NAME, ARGS...) \
    TYPE NAME(ARGS); \
   TYPE __attribute__((naked)) NAME(ARGS) { __asm__( ".include \"ver/"STRINGIFY(VERSION)"/asm/nonmatchings/"FOLDER"/"#NAME".s\"\n.set reorder\n.set at"); }
 #endif
-#ifndef PERMUTER
 __asm__( ".include \"include/macro.inc\"\n");
-#else
-void __attribute__((naked)) dummy_permuter_func(void) { __asm__( ".include \"include/macro.inc\"\n"); }
-#endif
 #else
 #define INCLUDE_ASM(TYPE, FOLDER, NAME, ARGS...)
 #endif
