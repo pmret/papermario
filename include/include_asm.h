12be5dde06cf476c9db31a8d74aa1a97f955eba8
Ethan Roseman <ethteck@gmail.com>
2021-03-10 13:02:11 +0900

    Wrap __asm__ for macro inclusion in a dummy function for the permuter

--- include/include_asm.h
+++ include/include_asm.h
@@ -10,7 +10,11 @@
    TYPE NAME(ARGS); \
   TYPE __attribute__((naked)) NAME(ARGS) { __asm__( ".include \"ver/"STRINGIFY(VERSION)"/asm/nonmatchings/"FOLDER"/"#NAME".s\"\n.set reorder\n.set at"); }
 #endif
+#ifndef PERMUTER
 __asm__( ".include \"include/macro.inc\"\n");
+#else
+void __attribute__((naked)) dummy_permuter_func(void) { __asm__( ".include \"include/macro.inc\"\n"); }
+#endif
 #else
 #define INCLUDE_ASM(TYPE, FOLDER, NAME, ARGS...)
 #endif
