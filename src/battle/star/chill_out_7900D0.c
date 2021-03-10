c1d0cb9cea8b0fe97c71aaa470d50255924859e2
Tashi <65480653+tashi-lp@users.noreply.github.com>
2021-03-09 21:18:32 -0700

    Matched chill_out functions (#199)
    * matched functions in chill_out
    
    * ran coverage.py
    
    * removed redundant temp variables

--- src/battle/star/chill_out_7900D0.c
+++ src/battle/star/chill_out_7900D0.c
@@ -2,20 +2,75 @@
 
 extern s32 D_802A2CC0;
 
-INCLUDE_ASM(s32, "battle/star/chill_out_7900D0", func_802A1000_7900D0);
+ApiStatus func_802A1000_7900D0(ScriptInstance* script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
+    s32 a = get_variable(script, *args++);
+    s32 b = get_variable(script, *args++);
+    s32 c = get_variable(script, *args++);
 
-INCLUDE_ASM(s32, "battle/star/chill_out_7900D0", func_802A10AC_79017C);
+    func_8006FEF0(0, a, b, c, 40.0f);
+    return ApiStatus_DONE2;
+}
 
-INCLUDE_ASM(s32, "battle/star/chill_out_7900D0", func_802A116C_79023C);
+ApiStatus func_802A10AC_79017C(ScriptInstance* script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
+    s32 a = get_variable(script, *args++);
+    s32 b = get_variable(script, *args++);
+    s32 c = get_variable(script, *args++);
+
+    func_80070550(5, a, b, c, 50.0f, 50.0f, 40, 30);
+    return ApiStatus_DONE2;
+}
+
+ApiStatus func_802A116C_79023C(ScriptInstance* script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
+    s32 a = get_variable(script, *args++);
+    s32 b = get_variable(script, *args++);
+    s32 c = get_variable(script, *args++);
+
+    func_8006FEF0(0, a, b, c, 30.0f);
+    return ApiStatus_DONE2;
+}
 
 INCLUDE_ASM(s32, "battle/star/chill_out_7900D0", func_802A1218_7902E8);
 
 #define NAMESPACE battle_star_chill_out
 #include "common/FadeBackgroundToBlack.inc.c"
 
-INCLUDE_ASM(s32, "battle/star/chill_out_7900D0", func_802A137C_79044C);
+ApiStatus func_802A137C_79044C(ScriptInstance* script, s32 isInitialCall) {
+    if (isInitialCall) {
+        func_8011D82C(1);
+        *D_801512F0 = 1;
+        set_background_color_blend(0, 0, 0, 0);
+        script->functionTemp[0].s = 0;
+    }
 
-INCLUDE_ASM(s32, "battle/star/chill_out_7900D0", func_802A1414_7904E4);
+    set_background_color_blend(0, 0, 0, script->functionTemp[0].s);
+    script->functionTemp[0].s += 10;
+
+    if (script->functionTemp[0].s >= 230) {
+        set_background_color_blend(0, 0, 0, 230);
+        return ApiStatus_DONE2;
+    }
+
+    return ApiStatus_BLOCK;
+}
+
+ApiStatus func_802A1414_7904E4(ScriptInstance* script, s32 isInitialCall) {
+    if (isInitialCall) {
+        script->functionTemp[0].s = 20;
+    }
+
+    set_background_color_blend(0, 0, 0, script->functionTemp[0].s * 10);
+    script->functionTemp[0].s -= 1;
+
+    if (script->functionTemp[0].s == 0) {
+        set_background_color_blend(0, 0, 0, 0);
+        return ApiStatus_DONE2;
+    }
+
+    return ApiStatus_BLOCK;
+}
 
 INCLUDE_ASM(s32, "battle/star/chill_out_7900D0", func_802A1494_790564);
 
@@ -37,7 +92,20 @@ s32 func_802A1528_7905F8(ActorPart* actorPart) {
     return 2;
 }
 
-INCLUDE_ASM(s32, "battle/star/chill_out_7900D0", func_802A153C_79060C);
+ApiStatus func_802A153C_79060C(ScriptInstance* script, s32 isInitialCall) {
+    f32 a = rand_int(200) - 25;
+    f32 b = rand_int(120) + 7;
+    f32 c = rand_int(50) - 25;
+
+    func_80072230(2, a, b, c, 2.0f, 20);
+
+    a = rand_int(200) - 25;
+    b = rand_int(120) + 7;
+    c = rand_int(50) - 25;
+
+    func_8006FEF0(4, a, b, c, 40.0f);
+    return ApiStatus_DONE2;
+}
 
 INCLUDE_ASM(s32, "battle/star/chill_out_7900D0", func_802A163C_79070C);
 
