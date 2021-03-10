1a93142a085ef6723fcfe56ffd780b832901e529
Ethan Roseman <ethteck@gmail.com>
2021-03-10 21:31:39 +0900

    reorganization

--- src/os/code_3cb20_len_130.c
+++
@@ -1,5 +0,0 @@
-#include "common.h"
-
-INCLUDE_ASM(void, "os/code_3cb20_len_130", osInvalDCache, void* buf, s32 size);
-
-INCLUDE_ASM(void, "os/code_3cb20_len_130", osInvalICache, void* buf, s32 size);
--- ver/us/asm/os/osInvalDCache.s
+++ ver/us/asm/os/osInvalDCache.s
@@ -1,5 +1,11 @@
+.include "macro.inc"
+
+# assembler directives
 .set noat      # allow manual use of $at
 .set noreorder # don't insert nops after branches
+.set gp=64     # allow use of 64-bit general purpose registers
+
+.section .text, "ax"
 
 glabel osInvalDCache
 /* 3CB20 80061720 18A00020 */  blez      $a1, .L800617A4
