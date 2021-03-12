5a5445420b7ac623e493064662327238e25b1786
Ethan Roseman <ethteck@gmail.com>
2021-03-13 00:47:52 +0900

    fix lullaby

--- src/battle/star/lullaby_78D150.c
+++ src/battle/star/lullaby_78D150.c
@@ -15,20 +15,22 @@ INCLUDE_ASM(s32, "battle/star/lullaby_78D150", func_802A1494_78D5E4);
 #include "common/SetNpcCollision32.inc.c"
 
 ApiStatus func_802A1518_78D668(ScriptInstance* script, s32 isInitialCall) {
-    s32 temp;
-
     if (isInitialCall) {
         script->functionTemp[0].s = 0;
     }
 
     switch (script->functionTemp[0].s) {
+        s32 temp;
+        s32 temp2;
+
         case 0:
             script->functionTemp[2].s = -80;
             script->functionTemp[1].s = 0;
             script->functionTemp[0].s = 1;
         case 1:
             temp = script->functionTemp[2].s;
-            func_800706D0(0, temp, (s32)((sin_rad((script->functionTemp[1].s * 6.28318f) / 360.0f) * 30.0f) + 30.0f), 50.0f);
+            temp2 = (sin_rad((script->functionTemp[1].s * TAU) / 360.0f) * 30.0f) + 30.0f;
+            func_800706D0(0, temp, temp2, 50.0f);
 
             script->functionTemp[2].s += 14;
             script->functionTemp[1].s += 20;
