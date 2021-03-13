e6b0d0c7d24c2b0638c628e41e2ffcf8c9bb5434
Ethan Roseman <ethteck@gmail.com>
2021-03-13 19:19:27 +0900

    matched a fire flower func to explain decomp to a friend so here's a PR

--- src/battle/item/fire_flower_716C60.c
+++ src/battle/item/fire_flower_716C60.c
@@ -2,7 +2,18 @@
 
 INCLUDE_ASM(s32, "battle/item/fire_flower_716C60", func_802A1000_716C60);
 
-INCLUDE_ASM(s32, "battle/item/fire_flower_716C60", func_802A11D4_716E34);
+extern s32 D_802A1D70;
+
+//INCLUDE_ASM(s32, "battle/item/fire_flower_716C60", func_802A11D4_716E34);
+ApiStatus func_802A11D4_716E34(ScriptInstance* script, s32 isInitialCall) {
+    BattleStatus* battlestatus = &gBattleStatus;
+    s32 sellValue = gItemTable[battlestatus->selectedItemID].sellValue;
+
+    if (heroes_is_ability_active(battlestatus->playerActor, Ability_REFUND) && sellValue > 0) {
+        free_icon(D_802A1D70);
+    }
+    return ApiStatus_DONE2;
+}
 
 INCLUDE_ASM(s32, "battle/item/fire_flower_716C60", func_802A123C_716E9C);
 
