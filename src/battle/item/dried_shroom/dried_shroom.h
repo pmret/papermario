21babbf3e2219292c74ef2bb8c795d9eeac086a0
Lavos1 <saintlouisxx@gmail.com>
2021-03-19 12:31:32 +0000

    Dried shroom

---
+++ src/battle/item/dried_shroom/dried_shroom.h
@@ -0,0 +1,11 @@
+#ifndef BATTLE_ITEM_DRIED_SHROOM
+#define BATTLE_ITEM_DRIED_SHROOM
+
+#include "common.h"
+#include "script_api/battle.h"
+
+#undef NAMESPACE
+#define NAMESPACE battle_item_dried_shroom
+
+
+#endif
--- src/battle/item/dried_shroom/lib.c
+++ src/battle/item/dried_shroom/lib.c
@@ -1,13 +1,14 @@
-#include "dizzy_dial.h"
+#include "dried_shroom.h"
 
 extern s32 D_80108A64;
-MenuIcon* D_802A1CD4;
-s32* D_802A1CD0;
+MenuIcon* D_802A1A90;
 
-void func_8024ECF8(void);
-ApiStatus func_80252B3C(ScriptInstance* script, s32 isInitialCall);
+void func_802A123C_73330C();
+void func_802D7520();
+void func_802A12FC_75E8CC();
+void func_802D75D8();
 
-ApiStatus N(func_802A1000_727910)(ScriptInstance* script, s32 isInitialCall) {
+ApiStatus N(func_802A1000_71CCE0)(ScriptInstance* script, s32 isInitialCall) {
     BattleStatus* battleStatus = &gBattleStatus;
     Actor* player = battleStatus->playerActor;
     s32 sellValue = gItemTable[battleStatus->selectedItemID].sellValue;
@@ -37,8 +38,8 @@ ApiStatus N(func_802A1000_727910)(ScriptInstance* script, s32 isInitialCall) {
         posY = player->currentPos.y;
         posZ = player->currentPos.z;
         get_screen_coords(gCurrentCameraID, posX, posY, posZ, &iconPosX, &iconPosY, &iconPosZ);
-        D_802A1CD4 = create_icon(&D_80108A64);
-        set_icon_render_pos(D_802A1CD4, iconPosX + 36, iconPosY - 63);
+        D_802A1A90 = create_icon(&D_80108A64);
+        set_icon_render_pos(D_802A1A90, iconPosX + 36, iconPosY - 63);
     }
 
     script->varTable[0] = sleepTime;
@@ -46,61 +47,44 @@ ApiStatus N(func_802A1000_727910)(ScriptInstance* script, s32 isInitialCall) {
     return ApiStatus_DONE2;
 }
 
-ApiStatus N(func_802A11D4_727AE4)(ScriptInstance* script, s32 isInitialCall) {
+ApiStatus N(func_802A11D4_71CEB4)(ScriptInstance* script, s32 isInitialCall) {
     BattleStatus* battleStatus = &gBattleStatus;
     Actor* player = battleStatus->playerActor;
     s32 sellValue = gItemTable[battleStatus->selectedItemID].sellValue;
 
     if (heroes_is_ability_active(player, Ability_REFUND) && sellValue > 0) {
-        free_icon(D_802A1CD4);
+        free_icon(D_802A1A90);
     }
 
     return ApiStatus_DONE2;
 }
 
-void func_802A123C_727B4C(void) {
-    func_80138D88(0, 0, 0x140, 0xF0, 160.0f);
+ApiStatus N(func_802A123C_71CF1C)(ScriptInstance* script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
+    s32 a = get_variable(script, *args++);
+    s32 b = get_variable(script, *args++);
+    s32 c = get_variable(script, *args++);
+    s32 d = get_variable(script, *args++);
+
+    func_80071090(0, a, b, c, d);
+
+    return ApiStatus_DONE2;
 }
 
-ApiStatus N(func_802A1270_727B80)(ScriptInstance* script, s32 isInitialCall) {
-    Camera *camera = &gCameras[1];
-    f32 a;
+ApiStatus N(func_802A12FC_71CFDC)(ScriptInstance* script, s32 isInitialCall) {
+    PlayerData* playerData = &gPlayerData;
 
-    if (isInitialCall) {
-        script->functionTemp[0].s = 0;
+    playerData->curHP++;
+    if (playerData->curHP > playerData->curMaxHP) {
+        playerData->curHP = playerData->curMaxHP;
     }
-
-    switch(script->functionTemp[0].s) {
-        case 0:
-            script->functionTemp[1].s = 0;
-            script->functionTemp[2].s = 0;
-            play_sound(0x2033);
-            camera->unk_1C = 0;
-            D_802A1CD0 = bind_dynamic_entity_7(NULL, func_802A123C_727B4C);
-            script->functionTemp[0].s = 1;
-        case 1:
-            camera->flags |= 8;
-            a = script->functionTemp[1].s;
-            guRotateF(camera->viewMtxShaking, a, 0.0f, 0.0f, 1.0f);
-            script->functionTemp[1].s = 2.0 * ((1.0 - sin_rad(((script->functionTemp[2].s + 0x5A) * 6.28318f) / 360.0f)) * 360.0);
-            script->functionTemp[2].s++;
-            if (script->functionTemp[2].s < 0x5B) {
-                return ApiStatus_BLOCK;
-            }
-            camera->unk_1C = 0;
-            camera->flags &= ~8;
-            func_801235C0(D_802A1CD0);
-            return ApiStatus_DONE2;
-    }
-
-    return ApiStatus_BLOCK;
+    return ApiStatus_DONE2;
 }
 
 Script N(script1) = SCRIPT({
     if (SI_VAR(1) == 0) {
         UseCamPreset(69);
         sleep 10;
-
         PlaySoundAtActor(0, 8333);
         SetAnimation(0, 0, 0x1001F);
         GetActorPos(0, SI_VAR(0), SI_VAR(1), SI_VAR(2));
@@ -108,7 +92,6 @@ Script N(script1) = SCRIPT({
         SetActorSpeed(0, 4.0);
         SetGoalPos(0, SI_VAR(0), SI_VAR(1), SI_VAR(2));
         PlayerRunToGoal(0);
-
         SI_VAR(1) += 45;
         SI_VAR(3) = SI_VAR(1);
         SI_VAR(3) += 10;
@@ -116,20 +99,16 @@ Script N(script1) = SCRIPT({
         PlayEffect(51, 1, SI_VAR(0), SI_VAR(3), SI_VAR(2), 1.0, 30, 0, 0, 0, 0, 0, 0, 0);
         MakeItemEntity(SI_VAR(10), SI_VAR(0), SI_VAR(1), SI_VAR(2), 1, 0);
         SI_VAR(10) = SI_VAR(0);
-
-        N(func_802A1000_727910)();
+        N(func_802A1000_71CCE0)();
         sleep SI_VAR(0);
-
         sleep 15;
-
-        N(func_802A11D4_727AE4)();
+        N(func_802A11D4_71CEB4)();
         RemoveItemEntity(SI_VAR(10));
     } else {
         GetActorPos(0, SI_VAR(0), SI_VAR(1), SI_VAR(2));
         PlaySoundAtActor(0, 8333);
         SetAnimation(0, 0, 0x1001F);
         sleep 4;
-
         SI_VAR(1) += 45;
         SI_VAR(3) = SI_VAR(1);
         SI_VAR(3) += 10;
@@ -137,7 +116,6 @@ Script N(script1) = SCRIPT({
         PlayEffect(51, 1, SI_VAR(0), SI_VAR(3), SI_VAR(2), 1.0, 30, 0, 0, 0, 0, 0, 0, 0);
         MakeItemEntity(SI_VAR(10), SI_VAR(0), SI_VAR(1), SI_VAR(2), 1, 0);
         SI_VAR(10) = SI_VAR(0);
-
         sleep 15;
         RemoveItemEntity(SI_VAR(10));
     }
@@ -155,10 +133,10 @@ Script N(script2) = SCRIPT({
     SI_VAR(1) += 45;
     MakeItemEntity(SI_VAR(10), SI_VAR(0), SI_VAR(1), SI_VAR(2), 1, 0);
     SI_VAR(14) = SI_VAR(0);
-    N(func_802A1000_727910)();
+    N(func_802A1000_71CCE0)();
     sleep SI_VAR(0);
     sleep 15;
-    N(func_802A11D4_727AE4)();
+    N(func_802A11D4_71CEB4)();
     RemoveItemEntity(SI_VAR(14));
 });
 
@@ -195,51 +173,25 @@ Script N(script5) = SCRIPT({
 });
 
 Script N(script6) = SCRIPT({
-    SI_VAR(10) =c 0x9A;
-    await 0x802A13D0;
-    UseCamPreset(2);
-    MoveBattleCamOver(20);
-    sleep 10;
-    spawn {
-        sleep 5;
-        AddBattleCamZoom(0xFFFFFF06);
-        MoveBattleCamOver(80);
-        func_8024ECF8(0, 0, 1);
-        sleep 80;
-        AddBattleCamZoom(250);
-        MoveBattleCamOver(3);
-        func_8024ECF8(0, 0, 1);
-    }
-    N(func_802A1270_727B80)();
-    spawn {
-        StartRumble(8);
-        ShakeCam(1, 0, 2, 1.0);
-        ShakeCam(1, 0, 2, 4.0);
-        ShakeCam(1, 0, 2, 3.0);
-        ShakeCam(1, 0, 2, 2.0);
-        ShakeCam(1, 0, 2, 1.0);
-        ShakeCam(1, 0, 2, 0.5);
-        sleep 10;
-        UseCamPreset(3);
-        MoveBattleCamOver(10);
-        func_8024ECF8(0, 0, 0);
-    }
-    InitTargetIterator();
-0:
-    SetGoalToTarget(0xFFFFFF81);
-    ItemCheckHit(SI_VAR(0), 0x10000000, 0, SI_VAR(0), 0);
-    if (SI_VAR(0) == 6) {
-        goto 1;
-    }
-    GetItemPower(154, SI_VAR(0), SI_VAR(1));
-    MakeStatusField(SI_VAR(0), 0x40000, 100, SI_VAR(0));
-    func_80252B3C(SI_VAR(0), 0x50000000, SI_VAR(0), 0, 32);
-1:
-    sleep 5;
-    ChooseNextTarget(0, SI_VAR(0));
-    if (SI_VAR(0) != -1) {
-        goto 0;
-    }
+    SI_VAR(10) =c 0x8D;
+    await 0x802A1340;
+    await 0x802A180C;
+    SetAnimation(0, 0, 0x1001D);
+    GetActorPos(0, SI_VAR(0), SI_VAR(1), SI_VAR(2));
+    SI_VAR(0) += 0;
+    SI_VAR(1) += 35;
+    func_802A123C_73330C(SI_VAR(0), SI_VAR(1), SI_VAR(2), 1);
+    GetActorPos(0, SI_VAR(0), SI_VAR(1), SI_VAR(2));
+    SI_VAR(1) += 25;
+    SI_VAR(2) += 5;
+    func_802D7520(SI_VAR(0), SI_VAR(1), SI_VAR(2), 1);
+    func_802A12FC_75E8CC();
     sleep 30;
-    await 0x802A1800;
+    GetActorPos(0, SI_VAR(0), SI_VAR(1), SI_VAR(2));
+    SI_VAR(2) += 5;
+    func_802D75D8(SI_VAR(0), SI_VAR(1), SI_VAR(2), 1);
+    SetAnimation(0, 0, 0x10002);
+    sleep 20;
+    await 0x802A1770;
 });
+
