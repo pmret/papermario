4eeb44e03a6c4900ad35074caaf515e4e8a35cdc
Maide <eeeedddccc@hotmail.co.uk>
2021-03-20 14:17:58 +0000

    Mushroom

---
+++ src/battle/item/mushroom/mushroom.h
@@ -0,0 +1,15 @@
+#ifndef BATTLE_ITEM_MUSHROOM
+#define BATTLE_ITEM_MUSHROOM
+
+#include "common.h"
+#include "script_api/battle.h"
+
+#undef NAMESPACE
+#define NAMESPACE battle_item_mushroom
+
+ApiStatus func_802D3F74(ScriptInstance* script, s32 isInitialCall);
+ApiStatus func_802D378C(ScriptInstance* script, s32 isInitialCall);
+ApiStatus func_802D7520(ScriptInstance* script, s32 isInitialCall);
+ApiStatus func_802D75D8(ScriptInstance* script, s32 isInitialCall);
+
+#endif
