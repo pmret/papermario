b792c510f16a10117b70e8a81b5de1227e25b7c3
Maide <eeeedddccc@hotmail.co.uk>
2021-03-23 16:46:40 +0000

    Thunder Bolt

---
+++ src/battle/item/thunder_bolt/thunder_bolt.c
@@ -0,0 +1,34 @@
+#include "thunder_bolt.h"
+
+Script N(main) = SCRIPT({
+    SI_VAR(10) =c 0x84;
+    await N(UseItemWithEffect);
+    spawn {
+        sleep 5;
+        UseCamPreset(2);
+        MoveBattleCamOver(20);
+    }
+    N(FadeBackgroundToBlack)();
+    PlaySound(869);
+    sleep 10;
+    InitTargetIterator();
+    SetGoalToTarget(ActorID_SELF);
+    ItemCheckHit(SI_VAR(0), 0x10000000, 0, SI_VAR(0), 0);
+    if (SI_VAR(0) == 6) {
+        goto 1;
+    }
+    N(func_802A123C_722D7C)();
+    sleep 5;
+    StartRumble(10);
+    ShakeCam(1, 0, 5, 1.0);
+    GetItemPower(ItemId_THUNDER_RAGE, SI_VAR(0), SI_VAR(1));
+    ItemDamageEnemy(SI_VAR(0), 0x38000020, 0, SI_VAR(0), 32);
+1:
+    sleep 5;
+    UseCamPreset(3);
+    MoveBattleCamOver(20);
+    SetAnimation(ActorID_PLAYER, 0, PlayerAnim_2);
+    sleep 30;
+    N(func_802A1420_722F60)();
+    await N(PlayerGoHome);
+});
