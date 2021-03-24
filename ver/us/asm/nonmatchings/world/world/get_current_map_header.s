1b2e728e4dccfa4afd2bc851224fb795db3a7f41
Mr-Wiseguy <68165316+Mr-Wiseguy@users.noreply.github.com>
2021-03-24 04:36:06 -0400

    Decompiled some functions in world.c (#223)

--- ver/us/asm/nonmatchings/world/world/get_current_map_header.s
+++
@@ -1,8 +0,0 @@
-.set noat      # allow manual use of $at
-.set noreorder # don't insert nops after branches
-
-glabel get_current_map_header
-/* 35FE8 8005ABE8 3C02800A */  lui       $v0, %hi(D_800A41E8)
-/* 35FEC 8005ABEC 244241E8 */  addiu     $v0, $v0, %lo(D_800A41E8)
-/* 35FF0 8005ABF0 03E00008 */  jr        $ra
-/* 35FF4 8005ABF4 00000000 */   nop
