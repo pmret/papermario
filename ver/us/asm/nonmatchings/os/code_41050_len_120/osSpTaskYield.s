d4091729bed7cb0d869d3e91f956b3cd24b90eaa
Ethan Roseman <ethteck@gmail.com>
2021-03-28 13:51:52 +0900

    Revert "Integrated kmc gcc for matching libgultra (#224)"
    This reverts commit c83d233482a6389b0b9ab49e30777f0b0ecaff06.

--- ver/us/asm/nonmatchings/os/code_41050_len_120/osSpTaskYield.s
+++ ver/us/asm/nonmatchings/os/code_41050_len_120/osSpTaskYield.s
@@ -4,7 +4,7 @@
 glabel osSpTaskYield
 /* 41050 80065C50 27BDFFE8 */  addiu     $sp, $sp, -0x18
 /* 41054 80065C54 AFBF0010 */  sw        $ra, 0x10($sp)
-/* 41058 80065C58 0C01B1BC */  jal       __osSpSetStatus
+/* 41058 80065C58 0C01B1BC */  jal       osSpSetStatus
 /* 4105C 80065C5C 24040400 */   addiu    $a0, $zero, 0x400
 /* 41060 80065C60 8FBF0010 */  lw        $ra, 0x10($sp)
 /* 41064 80065C64 03E00008 */  jr        $ra
