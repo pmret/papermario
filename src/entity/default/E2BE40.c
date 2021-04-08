0a03f9bb12da56320da468fa02727b8b151831ff
Ethan Roseman <ethteck@gmail.com>
2021-04-08 23:06:48 +0900

    more renames

---
+++ src/entity/default/E2BE40.c
@@ -0,0 +1,13 @@
+#include "common.h"
+
+INCLUDE_ASM(s32, "entity/default/E2BE40", func_802BB710_E2BE40);
+
+INCLUDE_ASM(s32, "entity/default/E2BE40", func_802BB870_E2BFA0);
+
+INCLUDE_ASM(s32, "entity/default/E2BE40", func_802BB8A4_E2BFD4);
+
+INCLUDE_ASM(s32, "entity/default/E2BE40", func_802BBE3C_E2C56C);
+
+INCLUDE_ASM(s32, "entity/default/E2BE40", func_802BC148_E2C878);
+
+INCLUDE_ASM(s32, "entity/default/E2BE40", func_802BC1A8_E2C8D8);
---
+++ src/entity/jan_iwa/E2D730.c
@@ -0,0 +1,31 @@
+#include "common.h"
+
+INCLUDE_ASM(s32, "entity/jan_iwa/E2D730", func_802BAE00_E2D730);
+
+INCLUDE_ASM(s32, "entity/jan_iwa/E2D730", func_802BB000_E2D930);
+
+INCLUDE_ASM(s32, "entity/jan_iwa/E2D730", func_802BB0A0_E2D9D0);
+
+INCLUDE_ASM(s32, "entity/jan_iwa/E2D730", func_802BB228_E2DB58);
+
+INCLUDE_ASM(s32, "entity/jan_iwa/E2D730", func_802BB298_E2DBC8);
+
+INCLUDE_ASM(s32, "entity/jan_iwa/E2D730", func_802BB314_E2DC44);
+
+INCLUDE_ASM(s32, "entity/jan_iwa/E2D730", func_802BB34C_E2DC7C);
+
+INCLUDE_ASM(s32, "entity/jan_iwa/E2D730", func_802BB374_E2DCA4);
+
+INCLUDE_ASM(s32, "entity/jan_iwa/E2D730", func_802BB648_E2DF78);
+
+INCLUDE_ASM(s32, "entity/jan_iwa/E2D730", func_802BB6E0_E2E010);
+
+INCLUDE_ASM(s32, "entity/jan_iwa/E2D730", func_802BB778_E2E0A8);
+
+INCLUDE_ASM(s32, "entity/jan_iwa/E2D730", func_802BB78C_E2E0BC);
+
+INCLUDE_ASM(s32, "entity/jan_iwa/E2D730", func_802BB8D4_E2E204);
+
+INCLUDE_ASM(s32, "entity/jan_iwa/E2D730", func_802BB98C_E2E2BC);
+
+INCLUDE_ASM(s32, "entity/jan_iwa/E2D730", func_802BB9E0_E2E310);
