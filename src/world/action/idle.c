a81e9914aef5dfc75db1fb91be6abf36a84d46b7
Ethan Roseman <ethteck@gmail.com>
2021-04-14 02:58:09 +0900

    name temp anim

--- src/world/action/idle.c
+++ src/world/action/idle.c
@@ -1,9 +1,9 @@
 #include "idle.h"
 
 // Awkward func_800DFEFC (set player anim?) branching
-#ifndef NON_MATCHING
-INCLUDE_ASM(void, "world/action/idle", world_action_idle_update, void);
-#else
+// #ifndef NON_MATCHING
+// INCLUDE_ASM(void, "world/action/idle", world_action_idle_update, void);
+// #else
 void input_to_move_vector(f32* angle, f32* magnitude);
 s32 check_input_jump(void);
 s32 check_input_hammer(void);
@@ -11,9 +11,7 @@ s32 check_input_hammer(void);
 void world_action_idle_update(void) {
     PlayerStatus* playerStatus = &gPlayerStatus;
     PlayerData* playerData = &gPlayerData;
-    f32 moveX;
-    f32 moveZ;
-    s32 phi_s1;
+    s32 phi_s1 = 0;
 
     if (playerStatus->animFlags & 0x1000) {
         func_802B61E4_E23444();
@@ -21,11 +19,13 @@ void world_action_idle_update(void) {
     }
 
     playerStatus->framesOnGround++;
-    phi_s1 = 0;
-    if (playerStatus->flags < 0) {
+
+    if (playerStatus->flags & 0x80000000) {
+        s32 anim;
+
         playerStatus->flags &= ~0x8008000E;
-        playerStatus->fallState = 0;
         phi_s1 = 1;
+        playerStatus->fallState = 0;
         playerStatus->framesOnGround = 0;
         playerStatus->decorationList = 0;
         playerStatus->unk_C2 = 0;
@@ -33,53 +33,43 @@ void world_action_idle_update(void) {
         playerStatus->unk_8C = 0;
 
         if (playerStatus->animFlags & 0x4000) {
-            func_800DFEFC(0x90002);
+            anim = 0x90002;
+        } else if (!(playerStatus->animFlags & 1)) {
+            anim = 0x10002;
+        } else if ((s8)playerStatus->prevActionState == ACTION_STATE_IDLE) {
+            anim = 0x60005;
         } else {
-            if (playerStatus->animFlags & 1) {
-                if ((s8)playerStatus->prevActionState == ACTION_STATE_IDLE) {
-                    func_800DFEFC(0x60007);
-                } else {
-                    func_800DFEFC(0x60005);
-
-                }
-
-            } else {
-                func_800DFEFC(0x10002);
-            }
-
+            anim = 0x60007;
         }
-
+        func_800DFEFC(anim);
     }
 
     if (playerStatus->animFlags & 0x200) {
         set_action_state(ACTION_STATE_1C);
-        return;
-    }
-
-    input_to_move_vector(&moveX, &moveZ);
-    func_800E5150();
-    if (check_input_jump()) {
-        if (moveZ != 0.0f || playerStatus->targetYaw != moveX) {
-            playerStatus->targetYaw = moveX;
-            return;
-        }
-
     } else {
-        if (phi_s1 || (!check_input_hammer())) {
-            if (moveZ == 0.0f) {
-                playerData->idleFrameCounter++;
-                return;
-            }
+        f32 angle;
+        f32 magnitude;
 
-            playerStatus->framesOnGround = 0;
-            set_action_state(ACTION_STATE_WALK);
-            if (moveZ) {
-                playerStatus->targetYaw = moveX;
-                playerStatus->animFlags &= ~0x80000000;
+        input_to_move_vector(&angle, &magnitude);
+        func_800E5150();
+        if (check_input_jump()) {
+            if (magnitude != 0.0f || playerStatus->targetYaw != angle) {
+                playerStatus->targetYaw = angle;
+            }
+        } else if (phi_s1 || !check_input_hammer()) {
+            if (magnitude == 0.0f) {
+                playerData->idleFrameCounter++;
+            } else {
+                playerStatus->framesOnGround = 0;
+                set_action_state(ACTION_STATE_WALK);
+                if (magnitude != 0.0f) {
+                    playerStatus->targetYaw = angle;
+                    playerStatus->animFlags &= ~0x80000000;
+                }
             }
         }
     }
 }
-#endif
+// #endif
 
 INCLUDE_ASM(void, "world/action/idle", func_802B61E4_E23444, void);
