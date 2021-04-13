d613fa6e7f25d9eb67539c79ca365237af8012c6
Alex Bates <hi@imalex.xyz>
2021-04-13 18:14:50 +0100

    jump

---
+++ src/world/action/jump.h
@@ -0,0 +1,20 @@
+#ifndef _WORLD_ACTION_JUMP_
+#define _WORLD_ACTION_JUMP_
+
+#include "common.h"
+
+void func_802B6000_E24040(void);
+
+void func_802B60B4_E240F4(void);
+
+void func_802B6198_E241D8(void);
+
+void func_802B6294_E242D4(void);
+
+void func_802B6348_E24388(void);
+
+void func_802B647C_E244BC(void);
+
+void func_802B6508_E24548(void);
+
+#endif
--- tools/mv_segment.py
+++ tools/mv_segment.py
@@ -14,20 +14,24 @@ Path(f"src/{args.dest}.c").parent.mkdir(parents=True, exist_ok=True)
 Path(f"ver/current/asm/nonmatchings/{args.dest}").parent.mkdir(parents=True, exist_ok=True)
 Path(f"ver/current/asm/data/{args.dest}").parent.mkdir(parents=True, exist_ok=True)
 
-# rename
-os.rename(f"src/{args.src}.c", f"src/{args.dest}.c")
+if os.path.exists(f"src/{args.src}.c"):
+    print("moving c file")
+    os.rename(f"src/{args.src}.c", f"src/{args.dest}.c")
 
-with open(f"src/{args.dest}.c") as f:
-    content = f.read()
+    with open(f"src/{args.dest}.c") as f:
+        content = f.read()
 
-with open(f"src/{args.dest}.c", "w") as f:
-    f.write(content.replace(f'"{args.src}"', f'"{args.dest}"'))
+    with open(f"src/{args.dest}.c", "w") as f:
+        f.write(content.replace(f'"{args.src}"', f'"{args.dest}"'))
 
-if os.exists(f"ver/current/asm/nonmatchings/{args.src}"):
+if os.path.exists(f"ver/current/asm/nonmatchings/{args.src}"):
+    print("moving asm/nonmatchings files")
     os.rename(f"ver/current/asm/nonmatchings/{args.src}", f"ver/current/asm/nonmatchings/{args.dest}")
 
-if os.exists(f"ver/current/asm/data/{args.src}.data.s"):
+if os.path.exists(f"ver/current/asm/data/{args.src}.data.s"):
+    print("moving data file")
     os.rename(f"ver/current/asm/data/{args.src}.data.s", f"ver/current/asm/data/{args.dest}.data.s")
 
-if os.exists(f"ver/current/asm/data/{args.src}.rodata.s"):
+if os.path.exists(f"ver/current/asm/data/{args.src}.rodata.s"):
+    print("moving rodata file")
     os.rename(f"ver/current/asm/data/{args.src}.rodata.s", f"ver/current/asm/data/{args.dest}.rodata.s")
