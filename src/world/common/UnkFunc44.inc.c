007302764fff2ca5e17a5fde1e580476105394b1
Maide <eeeedddccc@hotmail.co.uk>
2021-04-26 19:33:52 +0100

    UnkFunc44

--- src/world/common/UnkFunc44.inc.c
+++ src/world/common/UnkFunc44.inc.c
@@ -1,8 +1,7 @@
-#include "jan_06.h"
+#include "common.h"
+#include "map.h"
 
-INCLUDE_ASM(s32, "world/area_jan/jan_06/B52B40", func_80240310_B52B40);
-/*
-ApiStatus N(func_80240310_B52B40)(ScriptInstance *script, s32 isInitialCall) {
+ApiStatus N(UnkFunc44)(ScriptInstance *script, s32 isInitialCall) {
     Bytecode* args = script->ptrReadPos;
 
     script->functionTemp[0].s = get_variable(script, *args++);
@@ -11,4 +10,3 @@ ApiStatus N(func_80240310_B52B40)(ScriptInstance *script, s32 isInitialCall) {
     sfx_adjust_env_sound_pos(0x194, 0, script->functionTemp[0].s, script->functionTemp[1].s, script->functionTemp[2].s);
     return ApiStatus_DONE2;
 }
-*/
