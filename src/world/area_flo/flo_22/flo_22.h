d3eca5eee10cb86b54ae47529cbbda57ba254cd2
Maide <eeeedddccc@hotmail.co.uk>
2021-04-27 07:45:00 +0100

    flo_22

--- src/world/area_flo/flo_22/flo_22.h
+++ src/world/area_flo/flo_22/flo_22.h
@@ -2,3 +2,12 @@
 #include "map.h"
 
 #define NAMESPACE flo_22
+
+ApiStatus N(SomeMatrixOperation2)(ScriptInstance* script, s32 isInitialCall);
+ApiStatus N(func_80240000_CEC240)(ScriptInstance* script, s32 isInitialCall);
+
+extern NpcGroupList N(npcGroupList_80240DE4);
+extern Script N(80240E24);
+extern Script N(80241528);
+extern Script N(80241F6C);
+extern Script N(main);
