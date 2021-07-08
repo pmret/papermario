f92162822bd2a45f3544ad9c154ca929dcf33fe3
Alex Bates <hi@imalex.xyz>
2021-07-08 15:33:24 +0100

    rename 7340_len_b00 crash

---
+++ src/crash.c
@@ -0,0 +1,27 @@
+#include "common.h"
+
+INCLUDE_ASM(s32, "crash", crash_delay_msec);
+
+INCLUDE_ASM(s32, "crash", func_8002BFB0);
+
+INCLUDE_ASM(s32, "crash", func_8002C054);
+
+INCLUDE_ASM(s32, "crash", func_8002C1D4);
+
+INCLUDE_ASM(s32, "crash", crash_printf_string);
+
+INCLUDE_ASM(s32, "crash", crash_print_fpr);
+
+INCLUDE_ASM(s32, "crash", func_8002C324);
+
+INCLUDE_ASM(s32, "crash", crash_show_handler);
+
+INCLUDE_ASM(s32, "crash", func_8002C7B0);
+
+INCLUDE_ASM(s32, "crash", crash_monitor_thread);
+
+INCLUDE_ASM(s32, "crash", func_8002C890);
+
+INCLUDE_ASM(s32, "crash", crash_create_monitor);
+
+INCLUDE_ASM(s32, "crash", func_8002C94C);
