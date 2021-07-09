8ea70f2e4178583b248f6dc7b214ee676ad8e927
Ethan Roseman <ethteck@gmail.com>
2021-07-09 20:08:52 +0900

    tidying

--- src/intro_logos.c
+++ src/intro_logos.c
@@ -1,11 +1,5 @@
 #include "common.h"
 
-void intro_logos_set_fade_alpha(s16 alpha);
-void intro_logos_set_fade_color(s16 color);
-s16 intro_logos_fade_in(s16 subtractAlpha);
-s16 intro_logos_fade_out(s16 addAlpha);
-void intro_logos_update_fade(void);
-
 void intro_logos_set_fade_alpha(s16 alpha) {
     gGameStatusPtr->bootAlpha = alpha;
 }
