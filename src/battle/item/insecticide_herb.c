28af7909768f4379f103ef590b33196353da84e1
Ethan Roseman <ethteck@gmail.com>
2021-07-10 21:57:40 +0900

    Further yaml cleanup & battle item refactor

--- src/battle/item/insecticide_herb.c
+++ src/battle/item/insecticide_herb.c
@@ -1,4 +1,6 @@
 #include "insecticide_herb.h"
+#include "ld_addrs.h"
+#include "battle/item/insecticide_herb.png.h"
 
 static HudElement* D_802A21C0;
 
@@ -202,4 +204,103 @@ Script N(DrinkItem) = SCRIPT({
     }
     SetAnimation(ACTOR_PLAYER, 0, ANIM_DRINK);
     sleep 45;
+});
+
+static s32 _pad = 0; // XXX
+
+#include "battle/item/insecticide_herb.png.inc.c"
+#include "battle/item/insecticide_herb.pal.inc.c"
+
+Vtx N(model)[] = {
+    { .v = { -16, -16, 0, FALSE, 0,    0,    0, 0, 0, 255 } },
+    { .v = { 15,  -16, 0, FALSE, 1024, 0,    0, 0, 0, 255 } },
+    { .v = { 15,  15,  0, FALSE, 1024, 1024, 0, 0, 0, 255 } },
+    { .v = { -16, 15,  0, FALSE, 0,    1024, 0, 0, 0, 255 } },
+};
+
+Gfx N(displayList)[] = {
+    gsDPPipeSync(),
+    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
+    gsDPSetCombineMode(G_CC_DECALRGBA, G_CC_DECALRGBA),
+    gsDPSetTexturePersp(G_TP_PERSP),
+    gsDPSetTextureDetail(G_TD_CLAMP),
+    gsDPSetTextureLOD(G_TL_TILE),
+    gsDPSetTextureLUT(G_TT_NONE),
+    gsDPSetTextureFilter(G_TF_AVERAGE),
+    gsDPSetTextureConvert(G_TC_FILT),
+    gsDPSetTextureLUT(G_TT_RGBA16),
+    gsDPLoadTLUT_pal16(0, battle_item_insecticide_herb_pal),
+    gsDPLoadTextureTile_4b(battle_item_insecticide_herb_png, G_IM_FMT_CI, battle_item_insecticide_herb_png_width, battle_item_insecticide_herb_png_height, 0, 0, battle_item_insecticide_herb_png_width - 1, battle_item_insecticide_herb_png_height - 1, 0, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD),
+    gsSPClearGeometryMode(G_LIGHTING),
+    gsSPClearGeometryMode(G_SHADING_SMOOTH),
+    gsSPVertex(N(model), ARRAY_COUNT(N(model)), 0),
+    gsSP1Triangle(0, 1, 2, 0),
+    gsSP1Triangle(0, 2, 3, 0),
+    gsDPPipeSync(),
+    gsSPEndDisplayList(),
+};
+
+s32 N(modelCommandList)[] = {
+    0x00000004, 0x0000000D, 0x00000001, sizeof(N(displayList)) / sizeof(s32), &N(displayList), 0x00000002, 0x00000000,
+};
+
+Script script = SCRIPT({
+    SI_VAR(10) = (const) ITEM_INSECTICIDE_HERB;
+    await N(UseItemWithEffect);
+    UseBattleCamPreset(3);
+    MoveBattleCamOver(15);
+    SetAnimation(ACTOR_PLAYER, 0, ANIM_THROW);
+    PlaySound(SOUND_THROW);
+    sleep 3;
+    CreateVirtualEntity(SI_VAR(10), N(modelCommandList));
+    SI_VAR(0) = 1.0;
+    MultiplyByActorScale(SI_VAR(0));
+    SetVirtualEntityScale(SI_VAR(10), SI_VAR(0), SI_VAR(0), SI_VAR(0));
+    GetActorPos(ACTOR_PLAYER, SI_VAR(0), SI_VAR(1), SI_VAR(2));
+    SI_VAR(3) = 20;
+    SI_VAR(4) = 42;
+    SI_VAR(5) = 5;
+    MultiplyVec3ByActorScale(SI_VAR(3), SI_VAR(4), SI_VAR(5));
+    SI_VAR(0) += SI_VAR(3);
+    SI_VAR(1) += SI_VAR(4);
+    SI_VAR(2) += SI_VAR(5);
+    SetVirtualEntityPosition(SI_VAR(10), SI_VAR(0), SI_VAR(1), SI_VAR(2));
+    InitTargetIterator();
+    SetGoalToTarget(ACTOR_SELF);
+    GetGoalPos(ACTOR_SELF, SI_VAR(0), SI_VAR(1), SI_VAR(2));
+    spawn {
+        SI_VAR(0) = 0;
+        loop 18 {
+            SI_VAR(0) += 0xFFFFFFC4;
+            SetVirtualEntityRotation(SI_VAR(10), 0, 0, SI_VAR(0));
+            sleep 1;
+        }
+    }
+    SetVirtualEntityJumpGravity(SI_VAR(10), 0.80078125);
+    SI_VAR(2) += 5;
+    VirtualEntityJumpTo(SI_VAR(10), SI_VAR(0), SI_VAR(1), SI_VAR(2), 18);
+    PlayEffect(0x7, 3, SI_VAR(0), SI_VAR(1), SI_VAR(2), 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    SI_VAR(0) -= 10;
+    PlayEffect(0x7, 3, SI_VAR(0), SI_VAR(1), SI_VAR(2), 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    SI_VAR(0) += 20;
+    PlayEffect(0x7, 3, SI_VAR(0), SI_VAR(1), SI_VAR(2), 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    spawn {
+        N(func_802A12E0_72AA30)(SI_VAR(0), SI_VAR(1), SI_VAR(2));
+        sleep 3;
+        N(func_802A12E0_72AA30)(SI_VAR(0), SI_VAR(1), SI_VAR(2));
+        sleep 3;
+        N(func_802A12E0_72AA30)(SI_VAR(0), SI_VAR(1), SI_VAR(2));
+    }
+    InitTargetIterator();
+    SetGoalToTarget(ACTOR_SELF);
+    N(func_802A123C_72A98C)();
+    DeleteVirtualEntity(SI_VAR(10));
+    if (SI_VAR(9) == 0) {
+        ItemDamageEnemy(SI_VAR(0), 0, 0, 0, 32);
+    } else {
+        InitTargetIterator();
+        SetGoalToTarget(ACTOR_SELF);
+        N(func_802A1280_72A9D0)();
+    }
+    await N(PlayerGoHome);
 });
--- src/battle/item/pebble.c
+++ src/battle/item/pebble.c
@@ -1,6 +1,8 @@
-#include "egg_missile.h"
+#include "pebble.h"
+#include "ld_addrs.h"
+#include "battle/item/pebble.png.h"
 
-static HudElement* D_802A2890;
+static HudElement* D_802A1E80;
 
 ApiStatus N(GiveRefund)(ScriptInstance* script, s32 isInitialCall) {
     BattleStatus* battleStatus = &gBattleStatus;
@@ -32,8 +34,8 @@ ApiStatus N(GiveRefund)(ScriptInstance* script, s32 isInitialCall) {
         posY = player->currentPos.y;
         posZ = player->currentPos.z;
         get_screen_coords(gCurrentCameraID, posX, posY, posZ, &iconPosX, &iconPosY, &iconPosZ);
-        D_802A2890 = create_hud_element(&D_80108A64);
-        set_hud_element_render_pos(D_802A2890, iconPosX + 36, iconPosY - 63);
+        D_802A1E80 = create_hud_element(&D_80108A64);
+        set_hud_element_render_pos(D_802A1E80, iconPosX + 36, iconPosY - 63);
     }
 
     script->varTable[0] = sleepTime;
@@ -47,20 +49,8 @@ ApiStatus N(GiveRefundCleanup)(ScriptInstance* script, s32 isInitialCall) {
     s32 sellValue = gItemTable[battleStatus->selectedItemID].sellValue;
 
     if (player_team_is_ability_active(player, ABILITY_REFUND) && sellValue > 0) {
-        free_hud_element(D_802A2890);
+        free_hud_element(D_802A1E80);
     }
-
-    return ApiStatus_DONE2;
-}
-
-ApiStatus N(func_802A123C_71CF1C)(ScriptInstance* script, s32 isInitialCall) {
-    Bytecode* args = script->ptrReadPos;
-    s32 a = get_variable(script, *args++);
-    s32 b = get_variable(script, *args++);
-    s32 c = get_variable(script, *args++);
-
-    playFX_17(0, a, b, c);
-    playFX_19(0, a, b + 20, c);
 
     return ApiStatus_DONE2;
 }
@@ -166,4 +156,87 @@ Script N(DrinkItem) = SCRIPT({
     }
     SetAnimation(ACTOR_PLAYER, 0, ANIM_DRINK);
     sleep 45;
+});
+
+static s32 _pad = 0;
+
+#include "battle/item/pebble.png.inc.c"
+#include "battle/item/pebble.pal.inc.c"
+
+Vtx N(model)[] = {
+    { .v = { -16, -16, 0, FALSE, 0,    0,    0, 0, 0, 255 } },
+    { .v = { 15,  -16, 0, FALSE, 1024, 0,    0, 0, 0, 255 } },
+    { .v = { 15,  15,  0, FALSE, 1024, 1024, 0, 0, 0, 255 } },
+    { .v = { -16, 15,  0, FALSE, 0,    1024, 0, 0, 0, 255 } },
+};
+
+Gfx N(displayList)[] = {
+    gsDPPipeSync(),
+    gsSPTexture(0xFFFF, 0xFFFF, 0, G_TX_RENDERTILE, G_ON),
+    gsDPSetCombineMode(G_CC_DECALRGBA, G_CC_DECALRGBA),
+    gsDPSetTexturePersp(G_TP_PERSP),
+    gsDPSetTextureDetail(G_TD_CLAMP),
+    gsDPSetTextureLOD(G_TL_TILE),
+    gsDPSetTextureLUT(G_TT_NONE),
+    gsDPSetTextureFilter(G_TF_AVERAGE),
+    gsDPSetTextureConvert(G_TC_FILT),
+    gsDPSetTextureLUT(G_TT_RGBA16),
+    gsDPLoadTLUT_pal16(0, N(pal)),
+    gsDPLoadTextureTile_4b(N(png), G_IM_FMT_CI, battle_item_pebble_png_width, battle_item_pebble_png_height, 0, 0, battle_item_pebble_png_width - 1, battle_item_pebble_png_height - 1, 0, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMIRROR | G_TX_WRAP, G_TX_NOMASK, G_TX_NOMASK, G_TX_NOLOD, G_TX_NOLOD),
+    gsSPClearGeometryMode(G_LIGHTING),
+    gsSPClearGeometryMode(G_SHADING_SMOOTH),
+    gsSPVertex(N(model), ARRAY_COUNT(N(model)), 0),
+    gsSP1Triangle(0, 1, 2, 0),
+    gsSP1Triangle(0, 2, 3, 0),
+    gsDPPipeSync(),
+    gsSPEndDisplayList(),
+};
+
+s32 N(modelCommandList)[] = {
+    0x00000004, 0x0000000D, 0x00000001, sizeof(N(displayList)) / sizeof(s32), &N(displayList), 0x00000002, 0x00000000,
+};
+
+Script N(main) = SCRIPT({
+    SI_VAR(10) = (const) ITEM_PEBBLE;
+    await N(UseItemWithEffect);
+    UseBattleCamPreset(3);
+    MoveBattleCamOver(15);
+    SetAnimation(ACTOR_PLAYER, 0, ANIM_THROW);
+    PlaySound(SOUND_THROW);
+    sleep 3;
+    CreateVirtualEntity(SI_VAR(10), N(modelCommandList));
+    SI_VAR(0) = 1.0;
+    MultiplyByActorScale(SI_VAR(0));
+    SetVirtualEntityScale(SI_VAR(10), SI_VAR(0), SI_VAR(0), SI_VAR(0));
+    GetActorPos(ACTOR_PLAYER, SI_VAR(0), SI_VAR(1), SI_VAR(2));
+    SI_VAR(3) = 20;
+    SI_VAR(4) = 42;
+    SI_VAR(5) = 5;
+    MultiplyVec3ByActorScale(SI_VAR(3), SI_VAR(4), SI_VAR(5));
+    SI_VAR(0) += SI_VAR(3);
+    SI_VAR(1) += SI_VAR(4);
+    SI_VAR(2) += SI_VAR(5);
+    SetVirtualEntityPosition(SI_VAR(10), SI_VAR(0), SI_VAR(1), SI_VAR(2));
+    InitTargetIterator();
+    SetGoalToTarget(ACTOR_SELF);
+    GetGoalPos(ACTOR_SELF, SI_VAR(0), SI_VAR(1), SI_VAR(2));
+    spawn {
+        SI_VAR(0) = 0;
+        loop 18 {
+            SI_VAR(0) += 0xFFFFFFC4;
+            SetVirtualEntityRotation(SI_VAR(10), 0, 0, SI_VAR(0));
+            sleep 1;
+        }
+    }
+    SetVirtualEntityJumpGravity(SI_VAR(10), 0.80078125);
+    SI_VAR(2) += 5;
+    VirtualEntityJumpTo(SI_VAR(10), SI_VAR(0), SI_VAR(1), SI_VAR(2), 18);
+    GetItemPower(ITEM_PEBBLE, SI_VAR(3), SI_VAR(4));
+    ApplyShrinkFromOwner(SI_VAR(3));
+    ItemDamageEnemy(SI_VAR(9), 0x18000000, 0, SI_VAR(3), 32);
+    SI_VAR(0) += 60;
+    SI_VAR(1) += 0;
+    VirtualEntityJumpTo(SI_VAR(10), SI_VAR(0), SI_VAR(1), SI_VAR(2), 16);
+    DeleteVirtualEntity(SI_VAR(10));
+    await N(PlayerGoHome);
 });
