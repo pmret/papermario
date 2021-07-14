e84798a8022b808961f36badc74018fbeff9936c
Ethan Roseman <ethteck@gmail.com>
2021-07-14 17:33:28 +0900

    rodata padding

--- src/world/area_hos/hos_00/A0BD40.c
+++ src/world/area_hos/hos_00/A0BD40.c
@@ -12,3 +12,4 @@ static char* N(exit_str_0) = "osr_00";
 static char* N(exit_str_1) = "osr_01";
 static char* N(exit_str_2) = "hos_01";
 static char* N(exit_str_3) = "hos_06";
+static char* N(exit_str_4) = "\0\0\0\0";
