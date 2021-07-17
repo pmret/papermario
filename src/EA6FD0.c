bca5129f4cbb608f46ac0af5ff8d8477e88ff5c0
Rainchus <rainmachus@gmail.com>
2021-07-17 10:10:58 -0500

    Match battle functions and some unused/dead functions (#318)
    * decomp dead functions
    
    * decomp battle functions in 1A5830.c
    
    * delete matched functions .s files
    
    * fix variable names

--- src/EA6FD0.c
+++ src/EA6FD0.c
@@ -1,3 +1,11 @@
 #include "common.h"
 
-INCLUDE_ASM(s32, "EA6FD0", func_80240000_EA6FD0);
+s32 func_80240000_EA6FD0(void) {
+    s32 temp = dead_get_variable(NULL, SI_SAVE_VAR(0));
+
+    if (temp <= 0x26) {
+        return 0x19010F;
+    } else {
+        return 0x190110;
+    }
+}
