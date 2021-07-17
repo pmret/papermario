5ca050313906c65cc709c039f071de4b18cee194
Alex Bates <hi@imalex.xyz>
2021-07-17 11:31:49 +0100

    fix lakilester split

--- ver/us/asm/nonmatchings/battle/partner/lakilester/func_8023A1B0_70DEC0.s
+++ ver/us/asm/nonmatchings/battle/partner/lakilester/func_8023A1B0_70DEC0.s
@@ -6,3 +6,4 @@ glabel func_8023A1B0_70DEC0
 /* 70DEC4 8023A1B4 8C42D334 */  lw        $v0, %lo(D_8023D334)($v0)
 /* 70DEC8 8023A1B8 AC820084 */  sw        $v0, 0x84($a0)
 /* 70DECC 8023A1BC 03E00008 */  jr        $ra
+addiu $v0, $zero, 2
