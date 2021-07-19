fc1248342f21f40fa6146a7c341cb42779753fba
Rain <rainmachus@gmail.com>
2021-07-19 09:23:47 -0500

    fix style

--- src/d5a50_len_5fd0.c
+++ src/d5a50_len_5fd0.c
@@ -59,7 +59,7 @@ void func_80144258(s32 arg0) {
 }
 
 void func_80144278(s32 arg0) {
-    func_80143C48 (arg0, 0, 1);
+    func_80143C48(arg0, 0, 1);
 }
 
 INCLUDE_ASM(s32, "d5a50_len_5fd0", draw_hud_element);
@@ -127,9 +127,8 @@ INCLUDE_ASM(s32, "d5a50_len_5fd0", set_hud_element_size);
 INCLUDE_ASM(s32, "d5a50_len_5fd0", func_80144E4C);
 
 void func_80144E74(s32 iconIndex, s32 arg1) {
-    HudElement *hudElement;
+    HudElement* hudElement = gHudElementList[iconIndex & ~0x800];
 
-    hudElement = gHudElementList[iconIndex & ~0x800];
     hudElement->flags &= ~0xF000000;
     hudElement->flags |= arg1 << 24;
 }
