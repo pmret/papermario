b4694ce86062dc0136000eaf639431888c3e1715
ThreeMustyFears <87553242+ThreeMustyFears@users.noreply.github.com>
2021-08-02 13:27:10 -0300

    Some requested changes
    I couldn't figure out how to get Kelebek1's suggestions for the suggest_player_anim_clearUnkFlag to work, but I managed to do it for sfx_play_sound_at_player

--- src/world/action/jump.c
+++ src/world/action/jump.c
@@ -1,25 +1,17 @@
 #include "common.h"
 
-extern void phys_adjust_cam_on_landing();
-extern void sfx_play_sound_at_player(s32 soundID, s32 arg0);
-extern void suggest_player_anim_clearUnkFlag(s32 arg0);
-extern void phys_init_integrator_for_current_state();
-extern s32 check_input_jump();
-extern s32 player_raycast_below_cam_relative(PlayerStatus*, f32*, f32*, f32*, f32*, f32*, f32*, f32*, f32*);
-
 extern f32 D_8010C960;
 extern f32 D_8010C97C;
 
 void func_802B6000_E24040() {
-    s32 temp_v1;
-    s32 phi_a0;
     PlayerStatus* playerStatus = &gPlayerStatus;
     CollisionStatus* collisionStatus;
+    s32 temp_v1;
 
     playerStatus->fallState = 0;
     playerStatus->decorationList = 0;
     playerStatus->unk_C2 = 0;
-    playerStatus->flags &= 0x7FFFFFF7 ;
+    playerStatus->flags &= 0x7FFFFFF7;
     playerStatus->flags |= 2;
     playerStatus->unk_3C = playerStatus->position.x;
     playerStatus->unk_40 = playerStatus->position.z;
@@ -28,14 +20,14 @@ void func_802B6000_E24040() {
     phys_init_integrator_for_current_state();
     collisionStatus = &gCollisionStatus;
 
-    if ((playerStatus->animFlags & PLAYER_ANIM_FLAG_8BIT_MARIO)) {
+    if (playerStatus->animFlags & PLAYER_ANIM_FLAG_8BIT_MARIO) {
         temp_v1 = 0x90005;
     }
     else {
-        phi_a0 = (playerStatus->animFlags & PLAYER_ANIM_FLAG_3);
-        temp_v1 = 0x60009;
-        if (!phi_a0) {
+        if (!(playerStatus->animFlags & (PLAYER_ANIM_FLAG_HOLDING_ITEM | PLAYER_ANIM_FLAG_2))) {
             temp_v1 = 0x10007;
+        } else {
+            temp_v1 = 0x60009;
         }
     }
     suggest_player_anim_clearUnkFlag(temp_v1);
@@ -44,60 +36,56 @@ void func_802B6000_E24040() {
 }
 
 void func_802B60B4_E240F4() {
-    s32 temp_v1;
-    s32 phi_a0;
     PlayerStatus* playerStatus = &gPlayerStatus;
-    Camera* cam;
+    s32 phi_a0;
 
     if (playerStatus->flags < 0) {
-        playerStatus->flags &= 0x7FFFFFFF;
+        playerStatus->flags &= (~0x80000000);
         func_802B6000_E24040();
 
         if (playerStatus->actionState == ACTION_STATE_LAUNCH) {
             phys_adjust_cam_on_landing();
         } else {
-            cam = &gCameras;
-            cam->moveFlags |= 1;
+            gCameras[0].moveFlags |= 1;
         }
 
         if (playerStatus->actionState == ACTION_STATE_JUMP) {
-            phi_a0 = SOUND_JUMP_2081;
-            if ((playerStatus->animFlags & PLAYER_ANIM_FLAG_8BIT_MARIO)) {
-                phi_a0 = SOUND_JUMP_8BIT_MARIO;
+            if (playerStatus->animFlags & PLAYER_ANIM_FLAG_8BIT_MARIO) {
+                sfx_play_sound_at_player(SOUND_JUMP_8BIT_MARIO, 0);
             }
-            sfx_play_sound_at_player(phi_a0, 0);
+            else {
+                sfx_play_sound_at_player(SOUND_JUMP_2081, 0);
+            }
         }
     }
 
-    if ((playerStatus->animFlags & PLAYER_ANIM_FLAG_8BIT_MARIO)) {
+    if (playerStatus->animFlags & PLAYER_ANIM_FLAG_8BIT_MARIO) {
         phi_a0 = 0x90005;
     } else {
-        temp_v1 = playerStatus->animFlags & PLAYER_ANIM_FLAG_3;
-        phi_a0 = 0x60009;
-
-        if (!temp_v1) {
+        if (!(playerStatus->animFlags & (PLAYER_ANIM_FLAG_HOLDING_ITEM | PLAYER_ANIM_FLAG_2))) {
           phi_a0 = 0x10007;
         }
+        else {
+            phi_a0 = 0x60009;
+        }
     }
+
     suggest_player_anim_clearUnkFlag(phi_a0);
     playerStatus->decorationList++;
 }
 
 void func_802B6198_E241D8() {
-    f32 temp_f2;
-    s32 temp_s1;
-    s32 phi_a0;
-    Entity* temp_v0;
     PlayerStatus* playerStatus = &gPlayerStatus;
     CollisionStatus* collisionStatus = &gCollisionStatus;
-    Camera* cam;
+    Entity* entity;
+    s32 temp_s1;
+    s32 phi_a0;
 
     temp_s1 = 0x80000000;
     if (playerStatus->flags < 0) {
-        temp_v0 = get_entity_by_index(collisionStatus->currentFloor);
-        temp_f2 = temp_v0->position.z;
-        D_8010C960 = temp_v0->position.x;
-        D_8010C97C = temp_f2;
+        entity = get_entity_by_index(collisionStatus->currentFloor);
+        D_8010C960 = entity->position.x;
+        D_8010C97C = entity->position.z;
         func_802B6000_E24040();
         playerStatus->flags |= 0x880000;
         disable_player_input();
@@ -105,21 +93,20 @@ void func_802B6198_E241D8() {
 
     playerStatus->decorationList++;
     if (playerStatus->fallState == 1) {
-        if ((playerStatus->flags & 0x80000000)) {
+        if (playerStatus->flags & 0x80000000) {
 
           playerStatus->flags &= 0x7FFFFFF5;
           playerStatus->flags |= 4;
 
           phi_a0 = 0x6000A;
 
-          if (!(playerStatus->animFlags & PLAYER_ANIM_FLAG_3)) {
+          if (!(playerStatus->animFlags & (PLAYER_ANIM_FLAG_HOLDING_ITEM | PLAYER_ANIM_FLAG_2))) {
             phi_a0 = 0x10008;
           }
 
           temp_s1 = phi_a0;
           suggest_player_anim_clearUnkFlag(temp_s1);
-          cam = &gCameras;
-          cam->moveFlags |= 1;
+          gCameras[0].moveFlags |= 1;
         }
     } else {
         return;
@@ -128,17 +115,14 @@ void func_802B6198_E241D8() {
 }
 
 void func_802B6294_E242D4() {
-    s32 temp_v1;
-    s32 phi_a0;
     PlayerStatus* playerStatus = &gPlayerStatus;
-    Camera* cam;
+    s32 phi_a0;
 
-    if ((playerStatus->animFlags & PLAYER_ANIM_FLAG_PEACH_PHYSICS)) {
+    if (playerStatus->animFlags & PLAYER_ANIM_FLAG_PEACH_PHYSICS) {
         func_802B647C_E244BC();
         return;
     }
 
-    temp_v1 = playerStatus->flags;
     if ( playerStatus->flags < 0) {
         playerStatus->flags &= 0x7FFFFFF5;
         playerStatus->flags |= 4;
@@ -146,25 +130,21 @@ void func_802B6294_E242D4() {
         if (playerStatus->animFlags & PLAYER_ANIM_FLAG_8BIT_MARIO) {
             phi_a0 = 0x90005;
         } else {
-            temp_v1 = 0x6000A;
-            phi_a0 = temp_v1;
-
-            if (!(playerStatus->animFlags & PLAYER_ANIM_FLAG_3)) {
+            if (!(playerStatus->animFlags & (PLAYER_ANIM_FLAG_HOLDING_ITEM | PLAYER_ANIM_FLAG_2))) {
                 phi_a0 = 0x10008;
+            }
+            else {
+                phi_a0 = 0x6000A;
             }
         }
         suggest_player_anim_clearUnkFlag(phi_a0);
-        cam = &gCameras;
-        cam->moveFlags |= 1;
+        gCameras[0].moveFlags |= 1;
     }
     playerStatus->decorationList++;
 }
 
 void func_802B6348_E24388() {
-    f32* sub_arg1;
-    f32* sub_arg2;
-    f32* sub_arg3;
-    f32* sub_arg4;
+    PlayerStatus* playerStatus = &gPlayerStatus;
     f32 sub_arg5;
     f32 sub_arg6;
     f32 sub_arg7;
@@ -173,8 +153,6 @@ void func_802B6348_E24388() {
     f32 sp2C;
     f32 sp30;
     f32 sp34;
-    PlayerStatus* playerStatus = &gPlayerStatus;
-    Camera* cam;
 
     if (playerStatus->animFlags & PLAYER_ANIM_FLAG_PEACH_PHYSICS) {
         func_802B6508_E24548();
@@ -184,62 +162,54 @@ void func_802B6348_E24388() {
     if (playerStatus->flags < 0) {
         playerStatus->flags &= 0x7FFFFFF5;
         playerStatus->flags |= 4;
-        cam = &gCameras;
-        cam->moveFlags |= 1;
+        gCameras[0].moveFlags |= 1;
     }
 
     playerStatus->decorationList++;
     phys_update_interact_collider();
-    sub_arg1 = &sp28;
-    sub_arg2 = &sp2C;
-    sub_arg3 = &sp30;
-    sub_arg4 = &sp34;
-    sub_arg6 = sub_arg6;
     sp28 = playerStatus->position.x;
     sp2C = playerStatus->position.y;
     sp30 = playerStatus->position.z;
     sp34 = playerStatus->colliderHeight;
 
-    if (((get_collider_type_by_id(player_raycast_below_cam_relative(playerStatus, sub_arg1, sub_arg2, sub_arg3, sub_arg4,
-                                    &sub_arg5, &sub_arg6, &sub_arg7, &sub_arg8)) & 0xFF) - 2 >= 2u) && check_input_jump()) {
+    if (((get_collider_type_by_id(player_raycast_below_cam_relative(playerStatus, &sp28, &sp2C, &sp30, &sp34,
+                                    &sub_arg5, &sub_arg6, &sub_arg7, &sub_arg8)) & 0xFF) - 2 >= 2U) && check_input_jump()) {
 
-        set_action_state(3);
+        set_action_state(ACTION_STATE_JUMP);
         playerStatus->flags &= -0xF;
         func_802B60B4_E240F4();
     }
 }
 
 void func_802B647C_E244BC() {
-    s32 temp_v1;
     PlayerStatus* playerStatus = &gPlayerStatus;
-    Camera* cam;
+    s32 temp_v1;
 
     if (playerStatus->flags < 0) {
-        playerStatus->flags &= 0x7FFFFFFF;
+        playerStatus->flags &= ~0x80000000;
         playerStatus->flags &= ~0xA;
         playerStatus->flags |= 4;
-        temp_v1 = 0xA0006;
 
         if (!(playerStatus->animFlags & PLAYER_ANIM_FLAG_PEACH_PHYSICS)) {
             temp_v1 = 0x10008;
+        } else {
+            temp_v1 = 0xA0006;
         }
+
         suggest_player_anim_clearUnkFlag(temp_v1);
-        cam = &gCameras;
-        cam->moveFlags |= 1;
+        gCameras[0].moveFlags |= 1;
     }
     playerStatus->decorationList++;
 }
 
 void func_802B6508_E24548() {
     PlayerStatus* playerStatus = &gPlayerStatus;
-    Camera* cam;
 
     if (playerStatus->flags < 0) {
-        playerStatus->flags &= 0x7FFFFFFF;
+        playerStatus->flags &= ~0x80000000;
         playerStatus->flags &= ~0xA;
         playerStatus->flags |= 4;
-        cam = &gCameras;
-        cam->moveFlags |= 1;
+        gCameras[0].moveFlags |= 1;
 
         if (playerStatus->animFlags & PLAYER_ANIM_FLAG_PEACH_PHYSICS) {
             suggest_player_anim_clearUnkFlag(0xA0006);
