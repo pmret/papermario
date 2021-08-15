a5ceb0f0d20c18340aec41b782282a0e5ae744bc
ThreeMustyFears <87553242+ThreeMustyFears@users.noreply.github.com>
2021-08-14 21:44:20 -0300

    requested changes

--- src/world/action/walk.c
+++ src/world/action/walk.c
@@ -1,6 +1,7 @@
 #include "common.h"
 #include "world/actions.h"
 
+// TODO figure out these peach player animation IDs
 s32 D_802B6910_E23FF0[] = {0x000A0002, 0x000A002B, 0x000A002D, 0x000A002F, 0x000A0031, 0x000A0033, 0x000A0035, 0x000A0037, 0x000A0039, 0x000A003B, 0x000A003D, 0x000A003F,
                            0x000A0041, 0x000A0043, 0x000A0045, 0x000A0047, 0x000A0049, 0x000A004B, 0x000A004D, 0x00000000 };
 
@@ -11,14 +12,12 @@ INCLUDE_ASM(void, "world/action/walk", func_802B6000_E236E0, void);
 void func_802B6288_E23968() {
     PlayerStatus* playerStatus = &gPlayerStatus;
     PlayerData* playerData = &gPlayerData;
-    f32 sp14;
-    f32 sp10;
+    f32 moveX;
+    f32 moveY;
     s32 temp_v1;
     s32 phi_a0;
     f32 phi_f2;
     s32 phi_s3;
-    s32 xAxis;
-    s32 yAxis;
 
     phi_s3 = 0;
     if (playerStatus->animFlags & PLAYER_ANIM_FLAG_PEACH_PHYSICS) {
@@ -61,7 +60,7 @@ void func_802B6288_E23968() {
     }
 
     playerStatus->currentSpeed = playerStatus->runSpeed * phi_f2;
-    player_input_to_move_vector(&sp10, &sp14);
+    player_input_to_move_vector(&moveX, &moveY);
     phys_update_interact_collider();
     if (check_input_jump() == FALSE) {
         if (phi_s3 == 0) {
@@ -70,33 +69,30 @@ void func_802B6288_E23968() {
             }
         }
 
-        player_input_to_move_vector(&sp10, &sp14);
-        if (sp14 == 0.0f) {
+        player_input_to_move_vector(&moveX, &moveY);
+        if (moveY == 0.0f) {
             set_action_state(ACTION_STATE_IDLE);
             return;
         }
 
-        if (fabsf(D_800F7B40 - sp10) <= 90.0f) {
+        if (fabsf(D_800F7B40 - moveX) <= 90.0f) {
             temp_v1 = playerStatus->animFlags;
             if (temp_v1 >= 0) {
-                playerStatus->targetYaw = sp10;
+                playerStatus->targetYaw = moveX;
             }
            playerStatus->animFlags &= ~0x80000000;
         } else {
             temp_v1 = playerStatus->animFlags;
-            if (temp_v1 < 0) {
-                playerStatus->targetYaw = sp10;
+            temp_v1 = temp_v1 < 0;
+            if (temp_v1) {
+                playerStatus->targetYaw = moveX;
             } else {
-                xAxis = 0x80000000;
-                playerStatus->animFlags |= xAxis;
+                playerStatus->animFlags |= 0x80000000;
             }
-
         }
 
-        if (!is_ability_active(0xB)) {
-            xAxis = playerStatus->stickAxis[0] * playerStatus->stickAxis[0];
-            yAxis = playerStatus->stickAxis[1] * playerStatus->stickAxis[1];
-            if (55.0f >= sqrtf(xAxis + yAxis)) {
+        if (!is_ability_active(ABILITY_SLOW_GO)) {
+            if (sqrtf(SQ(playerStatus->stickAxis[0]) + SQ(playerStatus->stickAxis[1])) <= 55.0f) {
                 set_action_state(ACTION_STATE_WALK);
                 return;
             }
@@ -160,8 +156,8 @@ void func_802B65E8_E23CC8(void) {
 void func_802B6738_E23E18(void) {
     PlayerStatus* playerStatus = &gPlayerStatus;
     GameStatus* gameStatus;
-    f32 sp14;
-    f32 sp10;
+    f32 moveX;
+    f32 moveY;
 
     if (playerStatus->flags < 0) {
         playerStatus->flags &= ~0x80000000;
@@ -170,8 +166,7 @@ void func_802B6738_E23E18(void) {
             playerStatus->currentSpeed = playerStatus->runSpeed;
         }
 
-        if (!(playerStatus->animFlags & PLAYER_ANIM_FLAG_IN_DISGUISE))
-        {
+        if (!(playerStatus->animFlags & PLAYER_ANIM_FLAG_IN_DISGUISE)) {
             gameStatus = gGameStatusPtr;
             if (!(gameStatus->peachFlags & 0x10)) {
                 if (!gameStatus->peachAnimIdx) {
@@ -182,9 +177,8 @@ void func_802B6738_E23E18(void) {
             } else {
                 suggest_player_anim_clearUnkFlag(0xD000D);
             }
-
         } else {
-          peach_set_disguise_anim(world_actions_peachDisguises[playerStatus->peachDisguise].unk_08);
+            peach_set_disguise_anim(world_actions_peachDisguises[playerStatus->peachDisguise].unk_08);
         }
     }
 
@@ -195,17 +189,15 @@ void func_802B6738_E23E18(void) {
     }
 
     playerStatus->currentSpeed = playerStatus->runSpeed;
-    player_input_to_move_vector(&sp10, &sp14);
+    player_input_to_move_vector(&moveX, &moveY);
     phys_update_interact_collider();
-    if (sp14 == 0.0f) {
+    if (moveY == 0.0f) {
         set_action_state(ACTION_STATE_IDLE);
         return;
     }
 
-    playerStatus->targetYaw = sp10;
-
-    if (sqrtf(playerStatus->stickAxis[0] * playerStatus->stickAxis[0] +
-              playerStatus->stickAxis[1] * playerStatus->stickAxis[1]) < 55.0f) {
+    playerStatus->targetYaw = moveX;
+    if (sqrtf(SQ(playerStatus->stickAxis[0]) + SQ(playerStatus->stickAxis[1])) < 55.0f) {
         set_action_state(ACTION_STATE_WALK);
         return;
     } else {
