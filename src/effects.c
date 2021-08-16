9aebc26572f89b8805a7eec55b3e4d55bd8f13f3
Ethan Roseman <ethteck@gmail.com>
2021-08-17 05:28:08 +0900

    comments

--- src/effects.c
+++ src/effects.c
@@ -17,7 +17,6 @@ extern s32 D_801A6000;
 #define FX_MAIN(name) void name(s32 arg0, s32 arg1, s32 arg2, s32 arg3, f32 x, f32 y, f32 z);
 #define FX_ENTRY(name, gfx_name) { fx_##name##_main, effect_##name##_ROM_START, effect_##name##_ROM_END, \
                                    effect_##name##_VRAM, gfx_name##_ROM_START, gfx_name##_ROM_END }
-#define NULL_ENTRY { NULL, NULL, NULL, NULL, NULL, NULL }
 
 FX_MAIN(fx_1_main);
 FX_MAIN(fx_1_main);
@@ -149,141 +148,141 @@ FX_MAIN(fx_133_main);
 FX_MAIN(fx_134_main);
 
 EffectTableEntry gEffectTable[] = {
-    NULL_ENTRY, // 0
-    FX_ENTRY(1, _326410), // 1
-    NULL_ENTRY, // 2
-    NULL_ENTRY, // 3
-    NULL_ENTRY, // 4
-    NULL_ENTRY, // 5
-    FX_ENTRY(6, _328EA0), // 6
-    FX_ENTRY(7, _328EA0), // 7
-    FX_ENTRY(8, _32CEC0), // 8
-    FX_ENTRY(9, _32CEC0), // 9
-    FX_ENTRY(10, _32EC50), // 10
-    FX_ENTRY(11, _32EC50), // 11
-    FX_ENTRY(12, _32FB50), // 12
-    FX_ENTRY(13, _330460), // 13
-    FX_ENTRY(14, _330F00), // 14
-    FX_ENTRY(15, _3326A0), // 15
-    FX_ENTRY(16, _334C70), // 16
-    FX_ENTRY(17, _337FC0), // 17
-    FX_ENTRY(18, _339F60), // 18
-    FX_ENTRY(19, _33BBD0), // 19
-    FX_ENTRY(20, _33D610), // 20
-    FX_ENTRY(21, _33F000), // 21
-    FX_ENTRY(22, _340880), // 22
-    FX_ENTRY(23, _328EA0), // 23
-    FX_ENTRY(24, _343040), // 24
-    FX_ENTRY(25, _343F70), // 25
-    FX_ENTRY(26, _3451E0), // 26
-    FX_ENTRY(27, _345B40), // 27
-    FX_ENTRY(28, _34E770), // 28
-    FX_ENTRY(29, _328EA0), // 29
-    FX_ENTRY(30, _350220), // 30
-    FX_ENTRY(31, _352CE0), // 31
-    FX_ENTRY(32, _353BB0), // 32
-    FX_ENTRY(33, _343040), // 33
-    FX_ENTRY(34, _337FC0), // 34
-    FX_ENTRY(35, _356530), // 35
-    FX_ENTRY(36, _3573A0), // 36
-    FX_ENTRY(37, _3593B0), // 37
-    FX_ENTRY(38, _35A5E0), // 38
-    FX_ENTRY(39, _328EA0), // 39
-    FX_ENTRY(40, _35C550), // 40
-    FX_ENTRY(41, _35D510), // 41
-    NULL_ENTRY, // 42
-    FX_ENTRY(43, _33D610), // 43
-    FX_ENTRY(44, _35F0E0), // 44
-    FX_ENTRY(45, _360A30), // 45
-    FX_ENTRY(46, _361670), // 46
-    FX_ENTRY(47, _362C50), // 47
-    FX_ENTRY(48, _3638C0), // 48
-    FX_ENTRY(49, _364C00), // 49
-    FX_ENTRY(50, _3659B0), // 50
-    FX_ENTRY(51, _366D60), // 51
-    FX_ENTRY(52, _36AEE0), // 52
-    FX_ENTRY(53, _36DF90), // 53
-    FX_ENTRY(54, _36ED60), // 54
-    FX_ENTRY(55, _3733E0), // 55
-    FX_ENTRY(56, _337FC0), // 56
-    FX_ENTRY(57, _375510), // 57
-    FX_ENTRY(58, _337FC0), // 58
-    FX_ENTRY(59, _377F80), // 59
-    FX_ENTRY(60, _37ADD0), // 60
-    FX_ENTRY(61, _37D180), // 61
-    FX_ENTRY(62, _37D9D0), // 62
-    FX_ENTRY(63, _3803A0), // 63
-    FX_ENTRY(64, _381E00), // 64
-    FX_ENTRY(65, _3863B0), // 65
-    FX_ENTRY(66, _37D9D0), // 66
-    FX_ENTRY(67, _38A350), // 67
-    FX_ENTRY(68, _38BBA0), // 68
-    FX_ENTRY(69, _38D070), // 69
-    FX_ENTRY(70, _38E990), // 70
-    FX_ENTRY(71, _38F710), // 71
-    {fx_72_main, effect_72_ROM_START, effect_72_ROM_END, effect_72_VRAM, 0x003903D0, 0x00391D30 }, // 72
-    FX_ENTRY(73, _392440), // 73
-    FX_ENTRY(74, _3930A0), // 74
-    FX_ENTRY(75, _394670), // 75
-    FX_ENTRY(76, _3960F0), // 76
-    FX_ENTRY(77, _397040), // 77
-    FX_ENTRY(78, _398BC0), // 78
-    FX_ENTRY(79, _3A0D60), // 79
-    FX_ENTRY(80, _3A2440), // 80
-    FX_ENTRY(81, _3A33D0), // 81
-    FX_ENTRY(82, _3A4320), // 82
-    FX_ENTRY(83, _3A5BE0), // 83
-    FX_ENTRY(84, _3A77A0), // 84
-    FX_ENTRY(85, _3AB030), // 85
-    FX_ENTRY(86, _3AF700), // 86
-    FX_ENTRY(87, _3B2D90), // 87
-    FX_ENTRY(88, _3B46A0), // 88
-    FX_ENTRY(89, _3B5340), // 89
-    FX_ENTRY(90, _3B6BF0), // 90
-    FX_ENTRY(91, _3B78D0), // 91
-    FX_ENTRY(92, _3B8860), // 92
-    FX_ENTRY(93, _3B9A70), // 93
-    FX_ENTRY(94, _3BAEA0), // 94
-    FX_ENTRY(95, _3BCA90), // 95
-    FX_ENTRY(96, _3BD9A0), // 96
-    FX_ENTRY(97, _3C1BA0), // 97
-    FX_ENTRY(98, _3CB890), // 98
-    FX_ENTRY(99, _3CD6E0), // 99
-    FX_ENTRY(100, _3CFAF0), // 100
-    FX_ENTRY(101, _3D11E0), // 101
-    FX_ENTRY(102, _3D2580), // 102
-    FX_ENTRY(103, _3D3930), // 103
-    FX_ENTRY(104, _3593B0), // 104
-    FX_ENTRY(105, _3D4E90), // 105
-    NULL_ENTRY, // 106
-    FX_ENTRY(107, _3D5F30), // 107
-    FX_ENTRY(108, _3D7040), // 108
-    FX_ENTRY(109, _3D7770), // 109
-    FX_ENTRY(110, _3D80C0), // 110
-    FX_ENTRY(111, _3D9100), // 111
-    FX_ENTRY(112, _3DBF40), // 112
-    FX_ENTRY(113, _3DCD50), // 113
-    FX_ENTRY(114, _3DEE60), // 114
-    FX_ENTRY(115, _3E12D0), // 115
-    FX_ENTRY(116, _3E1CD0), // 116
-    FX_ENTRY(117, _3E2960), // 117
-    NULL_ENTRY, // 118
-    FX_ENTRY(119, _3E5350), // 119
-    FX_ENTRY(120, _3E5F30), // 120
-    { fx_121_main, effect_121_ROM_START, effect_121_ROM_END, effect_121_VRAM, 0x3EBE60, 0x3F83F0 }, // 121
-    FX_ENTRY(122, _3F8CC0), // 122
-    { fx_123_main, effect_123_ROM_START, effect_123_ROM_END, effect_123_VRAM, 0x3FA4B0, 0x3FEAE0 }, // 123
-    FX_ENTRY(124, _3FF250), // 124
-    { fx_125_main, effect_125_ROM_START, effect_125_ROM_END, effect_125_VRAM, 0x3FA4B0, 0x3FEAE0 }, // 125
-    FX_ENTRY(126, _403BF0), // 126
-    { fx_127_main, effect_127_ROM_START, effect_127_ROM_END, effect_127_VRAM, 0x404F40, 0x406B40 }, // 127
-    FX_ENTRY(128, _407080), // 128
-    FX_ENTRY(129, _40A1C0), // 129
-    FX_ENTRY(130, _40BBA0), // 130
-    FX_ENTRY(131, _40D290), // 131
-    FX_ENTRY(132, _337FC0), // 132
-    FX_ENTRY(133, _413FA0), // 133
-    FX_ENTRY(134, _415060), // 134
+    /* 0x00 */ {},
+    /* 0x01 */ FX_ENTRY(1, _326410),
+    /* 0x02 */ {},
+    /* 0x03 */ {},
+    /* 0x04 */ {},
+    /* 0x05 */ {},
+    /* 0x06 */ FX_ENTRY(6, _328EA0),
+    /* 0x07 */ FX_ENTRY(7, _328EA0),
+    /* 0x08 */ FX_ENTRY(8, _32CEC0),
+    /* 0x09 */ FX_ENTRY(9, _32CEC0),
+    /* 0x0A */ FX_ENTRY(10, _32EC50),
+    /* 0x0B */ FX_ENTRY(11, _32EC50),
+    /* 0x0C */ FX_ENTRY(12, _32FB50),
+    /* 0x0D */ FX_ENTRY(13, _330460),
+    /* 0x0E */ FX_ENTRY(14, _330F00),
+    /* 0x0F */ FX_ENTRY(15, _3326A0),
+    /* 0x10 */ FX_ENTRY(16, _334C70),
+    /* 0x11 */ FX_ENTRY(17, _337FC0),
+    /* 0x12 */ FX_ENTRY(18, _339F60),
+    /* 0x13 */ FX_ENTRY(19, _33BBD0),
+    /* 0x14 */ FX_ENTRY(20, _33D610),
+    /* 0x15 */ FX_ENTRY(21, _33F000),
+    /* 0x16 */ FX_ENTRY(22, _340880),
+    /* 0x17 */ FX_ENTRY(23, _328EA0),
+    /* 0x18 */ FX_ENTRY(24, _343040),
+    /* 0x19 */ FX_ENTRY(25, _343F70),
+    /* 0x1A */ FX_ENTRY(26, _3451E0),
+    /* 0x1B */ FX_ENTRY(27, _345B40),
+    /* 0x1C */ FX_ENTRY(28, _34E770),
+    /* 0x1D */ FX_ENTRY(29, _328EA0),
+    /* 0x1E */ FX_ENTRY(30, _350220),
+    /* 0x1F */ FX_ENTRY(31, _352CE0),
+    /* 0x20 */ FX_ENTRY(32, _353BB0),
+    /* 0x21 */ FX_ENTRY(33, _343040),
+    /* 0x22 */ FX_ENTRY(34, _337FC0),
+    /* 0x23 */ FX_ENTRY(35, _356530),
+    /* 0x24 */ FX_ENTRY(36, _3573A0),
+    /* 0x25 */ FX_ENTRY(37, _3593B0),
+    /* 0x26 */ FX_ENTRY(38, _35A5E0),
+    /* 0x27 */ FX_ENTRY(39, _328EA0),
+    /* 0x28 */ FX_ENTRY(40, _35C550),
+    /* 0x29 */ FX_ENTRY(41, _35D510),
+    /* 0x2A */ {},
+    /* 0x2B */ FX_ENTRY(43, _33D610),
+    /* 0x2C */ FX_ENTRY(44, _35F0E0),
+    /* 0x2D */ FX_ENTRY(45, _360A30),
+    /* 0x2E */ FX_ENTRY(46, _361670),
+    /* 0x2F */ FX_ENTRY(47, _362C50),
+    /* 0x30 */ FX_ENTRY(48, _3638C0),
+    /* 0x31 */ FX_ENTRY(49, _364C00),
+    /* 0x32 */ FX_ENTRY(50, _3659B0),
+    /* 0x33 */ FX_ENTRY(51, _366D60),
+    /* 0x34 */ FX_ENTRY(52, _36AEE0),
+    /* 0x35 */ FX_ENTRY(53, _36DF90),
+    /* 0x36 */ FX_ENTRY(54, _36ED60),
+    /* 0x37 */ FX_ENTRY(55, _3733E0),
+    /* 0x38 */ FX_ENTRY(56, _337FC0),
+    /* 0x39 */ FX_ENTRY(57, _375510),
+    /* 0x3A */ FX_ENTRY(58, _337FC0),
+    /* 0x3B */ FX_ENTRY(59, _377F80),
+    /* 0x3C */ FX_ENTRY(60, _37ADD0),
+    /* 0x3D */ FX_ENTRY(61, _37D180),
+    /* 0x3E */ FX_ENTRY(62, _37D9D0),
+    /* 0x3F */ FX_ENTRY(63, _3803A0),
+    /* 0x40 */ FX_ENTRY(64, _381E00),
+    /* 0x41 */ FX_ENTRY(65, _3863B0),
+    /* 0x42 */ FX_ENTRY(66, _37D9D0),
+    /* 0x43 */ FX_ENTRY(67, _38A350),
+    /* 0x44 */ FX_ENTRY(68, _38BBA0),
+    /* 0x45 */ FX_ENTRY(69, _38D070),
+    /* 0x46 */ FX_ENTRY(70, _38E990),
+    /* 0x47 */ FX_ENTRY(71, _38F710),
+    /* 0x48 */ {fx_72_main, effect_72_ROM_START, effect_72_ROM_END, effect_72_VRAM, 0x003903D0, 0x00391D30 },
+    /* 0x49 */ FX_ENTRY(73, _392440),
+    /* 0x4A */ FX_ENTRY(74, _3930A0),
+    /* 0x4B */ FX_ENTRY(75, _394670),
+    /* 0x4C */ FX_ENTRY(76, _3960F0),
+    /* 0x4D */ FX_ENTRY(77, _397040),
+    /* 0x4E */ FX_ENTRY(78, _398BC0),
+    /* 0x4F */ FX_ENTRY(79, _3A0D60),
+    /* 0x50 */ FX_ENTRY(80, _3A2440),
+    /* 0x51 */ FX_ENTRY(81, _3A33D0),
+    /* 0x52 */ FX_ENTRY(82, _3A4320),
+    /* 0x53 */ FX_ENTRY(83, _3A5BE0),
+    /* 0x54 */ FX_ENTRY(84, _3A77A0),
+    /* 0x55 */ FX_ENTRY(85, _3AB030),
+    /* 0x56 */ FX_ENTRY(86, _3AF700),
+    /* 0x57 */ FX_ENTRY(87, _3B2D90),
+    /* 0x58 */ FX_ENTRY(88, _3B46A0),
+    /* 0x59 */ FX_ENTRY(89, _3B5340),
+    /* 0x5A */ FX_ENTRY(90, _3B6BF0),
+    /* 0x5B */ FX_ENTRY(91, _3B78D0),
+    /* 0x5C */ FX_ENTRY(92, _3B8860),
+    /* 0x5D */ FX_ENTRY(93, _3B9A70),
+    /* 0x5E */ FX_ENTRY(94, _3BAEA0),
+    /* 0x5F */ FX_ENTRY(95, _3BCA90),
+    /* 0x60 */ FX_ENTRY(96, _3BD9A0),
+    /* 0x61 */ FX_ENTRY(97, _3C1BA0),
+    /* 0x62 */ FX_ENTRY(98, _3CB890),
+    /* 0x63 */ FX_ENTRY(99, _3CD6E0),
+    /* 0x64 */ FX_ENTRY(100, _3CFAF0),
+    /* 0x65 */ FX_ENTRY(101, _3D11E0),
+    /* 0x66 */ FX_ENTRY(102, _3D2580),
+    /* 0x67 */ FX_ENTRY(103, _3D3930),
+    /* 0x68 */ FX_ENTRY(104, _3593B0),
+    /* 0x69 */ FX_ENTRY(105, _3D4E90),
+    /* 0x6A */ {},
+    /* 0x6B */ FX_ENTRY(107, _3D5F30),
+    /* 0x6C */ FX_ENTRY(108, _3D7040),
+    /* 0x6D */ FX_ENTRY(109, _3D7770),
+    /* 0x6E */ FX_ENTRY(110, _3D80C0),
+    /* 0x6F */ FX_ENTRY(111, _3D9100),
+    /* 0x70 */ FX_ENTRY(112, _3DBF40),
+    /* 0x71 */ FX_ENTRY(113, _3DCD50),
+    /* 0x72 */ FX_ENTRY(114, _3DEE60),
+    /* 0x73 */ FX_ENTRY(115, _3E12D0),
+    /* 0x74 */ FX_ENTRY(116, _3E1CD0),
+    /* 0x75 */ FX_ENTRY(117, _3E2960),
+    /* 0x76 */ {},
+    /* 0x77 */ FX_ENTRY(119, _3E5350),
+    /* 0x78 */ FX_ENTRY(120, _3E5F30),
+    /* 0x79 */ { fx_121_main, effect_121_ROM_START, effect_121_ROM_END, effect_121_VRAM, 0x3EBE60, 0x3F83F0 },
+    /* 0x7A */ FX_ENTRY(122, _3F8CC0),
+    /* 0x7B */ { fx_123_main, effect_123_ROM_START, effect_123_ROM_END, effect_123_VRAM, 0x3FA4B0, 0x3FEAE0 },
+    /* 0x7C */ FX_ENTRY(124, _3FF250),
+    /* 0x7D */ { fx_125_main, effect_125_ROM_START, effect_125_ROM_END, effect_125_VRAM, 0x3FA4B0, 0x3FEAE0 },
+    /* 0x7E */ FX_ENTRY(126, _403BF0),
+    /* 0x7F */ { fx_127_main, effect_127_ROM_START, effect_127_ROM_END, effect_127_VRAM, 0x404F40, 0x406B40 },
+    /* 0x80 */ FX_ENTRY(128, _407080),
+    /* 0x81 */ FX_ENTRY(129, _40A1C0),
+    /* 0x82 */ FX_ENTRY(130, _40BBA0),
+    /* 0x83 */ FX_ENTRY(131, _40D290),
+    /* 0x84 */ FX_ENTRY(132, _337FC0),
+    /* 0x85 */ FX_ENTRY(133, _413FA0),
+    /* 0x86 */ FX_ENTRY(134, _415060),
 };
 
 s32 D_8007FEB8[] = {
