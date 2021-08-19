0a866d87cc6c9913bd150c00ff57f4b8db442076
ThreeMustyFears <87553242+ThreeMustyFears@users.noreply.github.com>
2021-08-19 20:30:29 -0300

    requested changes
    I changed gSpinHistoryPosY, and gSpinHistoryPosAngle to arrays based off of the declarations in 7E9D0.c

--- src/world/action/ultra_boots.c
+++ src/world/action/ultra_boots.c
@@ -1,8 +1,8 @@
 #include "common.h"
 
 extern s32 gSpinHistoryBufferPos;
-extern s32 gSpinHistoryPosY;
-extern s16 gSpinHistoryPosAngle;
+extern s32 gSpinHistoryPosY[5];
+extern s16 gSpinHistoryPosAngle[5];
 extern s32 D_8015A578;
 
 typedef struct struct8015A578 {
@@ -19,19 +19,17 @@ void func_802B6000_E26710(void) {
     Entity* currentFloorEntity;
     f32 temp_f0;
     f32 temp_f20;
-    s32 temp_a0;
     s32 temp_v0_2;
-    s32 temp_v1_2;
-    u32 temp_v0;
+    u8 temp_v1_2;
     f32 phi_f4;
     s32 sp12;
     s32 sp10;
+    u32 entityType;
 
-    if (playerStatus->flags < 0) {
-        temp_a0 = playerStatus->flags;
+    if (playerStatus->flags & (1 << 31)) {
         playerStatus->flags &= ~0x80000000;
         playerStatus->flags |= 0x2000A;
-        phys_clear_spin_history(temp_a0);
+        phys_clear_spin_history();
         playerStatus->fallState = 0;
         playerStatus->currentSpeed = 0.0f;
         playerStatus->gravityIntegrator[0] = 16.0f;
@@ -41,7 +39,7 @@ void func_802B6000_E26710(void) {
         suggest_player_anim_setUnkFlag(0x80000);
         disable_player_input();
         playerStatus->flags |= 0x200;
-        gCameras->moveFlags = gCameras->moveFlags | 1;
+        gCameras[0].moveFlags |= 1;
         temp_f0 = clamp_angle(playerStatus->targetYaw - gCameras[gCurrentCameraID].currentYaw);
         phi_f4 = -60.0f;
         if (temp_f0 <= 180.0f) {
@@ -63,25 +61,29 @@ void func_802B6000_E26710(void) {
         }
     }
     if (playerStatus->gravityIntegrator[0] <= 0.0f) {
-        *(&gSpinHistoryPosY + (gSpinHistoryBufferPos)) = playerStatus->position.y;
+        gSpinHistoryPosY[gSpinHistoryBufferPos] = playerStatus->position.y;
     }
 
-    *(&gSpinHistoryPosAngle + gSpinHistoryBufferPos++) = playerStatus->spriteFacingAngle;
+    gSpinHistoryPosAngle[gSpinHistoryBufferPos++] = playerStatus->spriteFacingAngle;
 
     if (gSpinHistoryBufferPos >= 6) {
         gSpinHistoryBufferPos = 0;
     }
 
     switch (playerStatus->fallState) {
-    case 0:
-        temp_f20 = integrate_gravity();
-        playerStatus->position.y = player_check_collision_below(temp_f20, &sp10);
-        if (sp10 >= 0 && collisionStatus->currentFloor & 0x4000 && (get_entity_type(collisionStatus->currentFloor) - 7 < 2)) {
-            currentFloorEntity = get_entity_by_index(collisionStatus->currentFloor);
-            currentFloorEntity->collisionFlags |= 1;
-            disable_player_input();
-            playerStatus->fallState = 0xB;
-        } else {
+        case 0:
+            temp_f20 = integrate_gravity();
+            playerStatus->position.y = player_check_collision_below(temp_f20, &sp10);
+            if (sp10 >= 0 && collisionStatus->currentFloor & 0x4000 ) {
+                entityType = get_entity_type(collisionStatus->currentFloor);
+                if(entityType == 7 || entityType == 8) {
+                    currentFloorEntity = get_entity_by_index(collisionStatus->currentFloor);
+                    currentFloorEntity->collisionFlags |= 1;
+                    disable_player_input();
+                    playerStatus->fallState = 0xB;
+                    break;
+                }
+            }
             if (temp_f20 <= 0.0f) {
                 record_jump_apex();
                 playerStatus->framesOnGround = 3;
@@ -93,83 +95,84 @@ void func_802B6000_E26710(void) {
                 playerStatus->flags &= ~0x00020008;
                 set_action_state(ACTION_STATE_LAND);
             }
-        }
-        break;
-    case 1:
-        if (--playerStatus->framesOnGround <= 0) {
-            playerStatus->fallState++;
-        }
-        break;
-    case 2:
-        temp_f20 = integrate_gravity();
-        playerStatus->position.y = player_check_collision_below(temp_f20, &sp10);
-        if (temp_f20 < -100.0f) {
-            playerStatus->gravityIntegrator[3] = 0.0f;
-            playerStatus->gravityIntegrator[2] = 0.0f;
-            playerStatus->gravityIntegrator[1] = 0.0f;
-            playerStatus->gravityIntegrator[0] = -100.0f;
-        }
-        if (sp10 >= 0) {
-            if (collisionStatus->currentFloor & 0x4000) {
-                temp_v0 = get_entity_type(collisionStatus->currentFloor);
-                if (temp_v0 - 0x2E < 2) {
+            break;
+        case 1:
+            if (--playerStatus->framesOnGround <= 0) {
+                playerStatus->fallState++;
+            }
+            break;
+        case 2:
+            temp_f20 = integrate_gravity();
+            playerStatus->position.y = player_check_collision_below(temp_f20, &sp10);
+            if (temp_f20 < -100.0f) {
+                playerStatus->gravityIntegrator[3] = 0.0f;
+                playerStatus->gravityIntegrator[2] = 0.0f;
+                playerStatus->gravityIntegrator[1] = 0.0f;
+                playerStatus->gravityIntegrator[0] = -100.0f;
+            }
+            if (sp10 >= 0) {
+                if (collisionStatus->currentFloor & 0x4000) {
+                    entityType = get_entity_type(collisionStatus->currentFloor);
+                    if (entityType == 0x2E || entityType == 0x2F) {
+                        playerStatus->flags &= ~0x00020008;
+                        set_action_state(ACTION_STATE_LAND);
+                        return;
+                    } else if (entityType == 7 || entityType == 8) {
+                        playerStatus->flags &= ~0x00020008;
+                        phys_player_land();
+                        exec_ShakeCam1(0, 0, 4);
+                        sfx_play_sound_at_player(0x14A, 0);
+                        start_rumble(0x100, 0x32);
+
+                        tempStruct = &D_8015A578;
+                        tempStruct->unk_00 = 1;
+                        tempStruct->unk_08 = playerStatus->position.y;
+                        playerStatus->flags |= 0x400;
+                        return;
+                    }
+                }
+
+                temp_v1_2 = get_collider_type_by_id(sp10);
+                if (temp_v1_2 == 3) {
+                    playerStatus->unk_BF = 1;
                     playerStatus->flags &= ~0x00020008;
-                    set_action_state(ACTION_STATE_LAND);
+                    set_action_state(ACTION_STATE_HIT_LAVA);
+                    playerStatus->flags |= 0x800;
+                    return;
+                } else if (temp_v1_2 == 2) {
+                    set_action_state(ACTION_STATE_HIT_LAVA);
+                    playerStatus->flags &= ~0x00020008;
                     return;
                 }
-                if (temp_v0 - 7 < 2) {
-                    playerStatus->flags &= ~0x00020008;
-                    phys_player_land();
-                    exec_ShakeCam1(0, 0, 4);
-                    sfx_play_sound_at_player(0x14A, 0);
-                    start_rumble(0x100, 0x32);
 
-                    tempStruct = &D_8015A578;
-                    tempStruct->unk_00 = 1;
-                    tempStruct->unk_08 = playerStatus->position.y;
-                    playerStatus->flags |= 0x400;
-                    return;
-                }
-            }
-            temp_v1_2 = get_collider_type_by_id(sp10) & 0xFF;
-            if (temp_v1_2 == 3) {
-                playerStatus->unk_BF = 1;
-                playerStatus->flags &= ~0x00020008;
-                set_action_state(ACTION_STATE_HIT_LAVA);
-                playerStatus->flags |= 0x800;
-                return;
-            }
-            if (temp_v1_2 == 2) {
-                set_action_state(ACTION_STATE_HIT_LAVA);
-                playerStatus->flags &= ~0x00020008;
-                return;
-            }
-            playerStatus->framesOnGround = 8;
-            playerStatus->decorationList = 0;
-            playerStatus->actionState = 0x10;
-            playerStatus->fallState++;
-            exec_ShakeCam1(0, 0, 4);
-            sfx_play_sound_at_player(0x14A, 0);
-            start_rumble(0x100, 0x32);
+                playerStatus->framesOnGround = 8;
+                playerStatus->decorationList = 0;
+                playerStatus->actionState = 0x10;
+                playerStatus->fallState++;
+                exec_ShakeCam1(0, 0, 4);
+                sfx_play_sound_at_player(0x14A, 0);
+                start_rumble(0x100, 0x32);
 
-            tempStruct2 = &D_8015A578;
-            tempStruct2->unk_00= 1;
-            tempStruct2->unk_08 = playerStatus->position.y;
-            playerStatus->flags |= 0x400;
-        }
-        break;
-    case 3:
-        if (--playerStatus->framesOnGround == 0) {
+                tempStruct2 = &D_8015A578;
+                tempStruct2->unk_00 = 1;
+                tempStruct2->unk_08 = playerStatus->position.y;
+                playerStatus->flags |= 0x400;
+            }
+            break;
+        case 3:
+            if (--playerStatus->framesOnGround == 0) {
+                playerStatus->fallState++;
+                playerStatus->flags &= ~0x00020008;
+                set_action_state(ACTION_STATE_LAND);
+            }
+            break;
+        case 11:
+            set_action_state(ACTION_STATE_LAND_ON_SWITCH);
             playerStatus->fallState++;
-            playerStatus->flags &= ~0x00020008;
-            set_action_state(ACTION_STATE_LAND);
-        }
-        break;
-    case 11:
-        set_action_state(ACTION_STATE_LAND_ON_SWITCH);
-        playerStatus->fallState++;
-        enable_player_input();
-    case 12:
+            enable_player_input();
+            break;
+        case 12:
+            break;
     }
 
     if (playerStatus->gravityIntegrator[0] < 0.0f) {
