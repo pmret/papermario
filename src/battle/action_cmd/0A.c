c58b4098f546fe2759a478799ef9a823435ab30a
Muskellunge <37986958+Muskellunge@users.noreply.github.com>
2021-08-27 07:44:37 +0200

    decomped AC function (#401)
    * rename func_80242178_7ECE88
    
    * matched tiny function
    
    * decomp AC func
    
    * renamed some vars
    
    * made requested changes
    
    * named temp_s0

--- src/battle/action_cmd/0A.c
+++ src/battle/action_cmd/0A.c
@@ -8,6 +8,22 @@ INCLUDE_ASM(s32, "battle/action_cmd/0A", func_802A9000_4273B0);
 
 INCLUDE_ASM(s32, "battle/action_cmd/0A", func_802A928C_42763C);
 
-INCLUDE_ASM(s32, "battle/action_cmd/0A", func_802A955C_42790C);
+void func_802A955C_42790C(void) {
+    ActionCommandStatus* actionCommandStatus = &gActionCommandStatus;
+    s32 renderPosX;
+    s32 renderPosY;
+    s32 hudElementID;
+
+    draw_hud_element_clipped(actionCommandStatus->hudElements[0]);
+    hudElementID = actionCommandStatus->hudElements[1];
+    draw_hud_element_clipped(hudElementID);
+    get_hud_element_render_pos(hudElementID, &renderPosX, &renderPosY);
+    if (actionCommandStatus->unk_68 == 0) {
+        func_80268770(renderPosX, renderPosY, actionCommandStatus->unk_44 / 100);
+    } else {
+        func_8026880C(renderPosX, renderPosY, actionCommandStatus->unk_44 / 100);
+    }
+    draw_hud_element_clipped(actionCommandStatus->hudElements[2]);
+}
 
 #include "common/free_hud_elements.inc.c"
