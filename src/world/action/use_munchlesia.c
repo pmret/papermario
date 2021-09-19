9c220576c02598d34443db194cbad60fcdeda7dd
ThreeMustyFears <>
2021-09-19 16:16:24 -0300

    match tweester and munchlesia

--- src/world/action/use_munchlesia.c
+++ src/world/action/use_munchlesia.c
@@ -1,3 +1,79 @@
 #include "common.h"
 
-INCLUDE_ASM(s32, "world/action/use_munchlesia", func_802B6000_E291A0);
+extern f32 D_802B62D0;
+extern f32 D_802B62D4;
+extern f32 D_802B62D8;
+extern f32 D_802B62DC;
+extern s32 D_802B62E0;
+
+void func_802B6000_E291A0(void) {
+    PlayerStatus* playerStatus = &gPlayerStatus;
+    s32 sp10;
+    f32 tempCollision;
+
+    if (playerStatus->flags & (1 << 31)) {
+        playerStatus->flags &= ~0x80000000;
+        playerStatus->fallState = 0;
+        gOverrideFlags |= 0x40;
+        disable_player_static_collisions();
+        disable_player_input();
+    }
+
+    switch (playerStatus->fallState) {
+        case 0:
+            suggest_player_anim_clearUnkFlag(0x1002E);
+            D_802B62E0 = 0x20;
+            D_802B62D0 = 16.0f;
+            D_802B62D4 = 4.0f;
+            D_802B62D8 = 1.0f;
+            D_802B62D4 = 1.875f;
+            playerStatus->fallState++;
+            D_802B62DC = playerStatus->targetYaw;
+            break;
+        case 1:
+            playerStatus->position.y += D_802B62D0;
+            D_802B62D0 = D_802B62D0 - D_802B62D8;
+            add_vec2D_polar(&playerStatus->position, &playerStatus->position.z, D_802B62D4, D_802B62DC);
+            gCameras[0].targetPos.x = playerStatus->position.x;
+            gCameras[0].targetPos.y = playerStatus->position.y;
+            gCameras[0].targetPos.z = playerStatus->position.z;
+            if (D_802B62D0 <= 0.0f) {
+                playerStatus->fallState++;
+            }
+            break;
+        case 2:
+            playerStatus->position.y += D_802B62D0;
+            D_802B62D0 = D_802B62D0 - D_802B62D8;
+            add_vec2D_polar(&playerStatus->position, &playerStatus->position.z, D_802B62D4, D_802B62DC);
+            gCameras[0].targetPos.x = playerStatus->position.x;
+            gCameras[0].targetPos.y = playerStatus->position.y;
+            gCameras[0].targetPos.z = playerStatus->position.z;
+            tempCollision = player_check_collision_below(D_802B62D0, &sp10);
+            if (sp10 >= 0) {
+                sfx_play_sound_at_player(SOUND_UNKNOWN_162, 0);
+                suggest_player_anim_setUnkFlag(0x80003);
+                playerStatus->position.y = tempCollision;
+                D_802B62E0 = 0xA;
+                playerStatus->fallState++;
+            }
+            break;
+        case 3:
+            if (playerStatus->unk_BC != 0) {
+                suggest_player_anim_setUnkFlag(0x10030);
+                playerStatus->fallState = 5;
+                playerStatus->framesOnGround = 15;
+                break;
+            }
+            break;
+        case 5:
+            if (playerStatus->framesOnGround != 0) {
+                playerStatus->framesOnGround--;
+                break;
+            }
+            enable_player_static_collisions();
+            enable_player_input();
+            set_action_state(ACTION_STATE_IDLE);
+            gOverrideFlags &= ~0x40;
+            break;
+    }
+}
--- src/world/action/use_tweester.c
+++ src/world/action/use_tweester.c
@@ -1,3 +1,84 @@
 #include "common.h"
+#include "../partners.h"
+#include "../../npc.h"
 
-INCLUDE_ASM(s32, "world/action/use_tweester", func_802B6000_E2A340);
+typedef struct unkTweesterStruct{
+    /* 0x000 */ s32 unk_00;
+    /* 0x004 */ char unk_04[8];
+    /* 0x00C */ f32 unk_0C;
+    /* 0x010 */ f32 unk_10;
+    /* 0x014 */ f32 unk_14;
+    /* 0x018 */ f32 unk_18;
+}unkTweesterStruct;
+
+extern s8* D_8010C934;
+extern struct unkTweesterStruct* D_802B6350_E2A690;
+
+void func_802B6000_E2A340(void) {
+    PlayerStatus* playerStatus = &gPlayerStatus;
+    Entity* entity;
+    f32 sp10;
+    f32 sp14;
+    f32 tempY;
+
+    entity = D_8010C934;
+    if (playerStatus->flags & (1 << 31)) {
+        playerStatus->flags &= ~0x80000000;
+        disable_player_static_collisions();
+        disable_player_input();
+        playerStatus->flags |= 0x100008;
+        suggest_player_anim_clearUnkFlag(0x8001F);
+        playerStatus->fallState = 0;
+        mem_clear(D_802B6350_E2A690, sizeof(*D_802B6350_E2A690));
+        D_802B6350_E2A690->unk_0C = fabsf(dist2D(playerStatus->position.x, playerStatus->position.z, entity->position.x, entity->position.z));
+        D_802B6350_E2A690->unk_10 = atan2(entity->position.x, entity->position.z, playerStatus->position.x, playerStatus->position.z);
+        D_802B6350_E2A690->unk_14 = 6.0f;
+        D_802B6350_E2A690->unk_18 = 50.0f;
+        D_802B6350_E2A690->unk_00 = 0x78;
+        sfx_play_sound_at_player(SOUND_UNKNOWN_2F6, 0);
+    }
+
+    switch (playerStatus->fallState) {
+        case 0:
+            sin_cos_rad((D_802B6350_E2A690->unk_10 * TAU) / 360.0f, &sp10, &sp14);
+
+            playerStatus->position.x = entity->position.x + (sp10 * D_802B6350_E2A690->unk_0C);
+            playerStatus->position.z = entity->position.z - (sp14 * D_802B6350_E2A690->unk_0C);
+
+            D_802B6350_E2A690->unk_10 = clamp_angle(D_802B6350_E2A690->unk_10 - D_802B6350_E2A690->unk_14);
+
+            if (D_802B6350_E2A690->unk_0C > 20.0f) {
+                D_802B6350_E2A690->unk_0C--;
+            } else if (D_802B6350_E2A690->unk_0C < 19.0f) {
+                D_802B6350_E2A690->unk_0C++;
+            }
+
+            tempY = sin_rad((D_802B6350_E2A690->unk_18 * TAU) / 360.0f)  * 3.0f;
+            D_802B6350_E2A690->unk_18 += 3.0f;
+            if (D_802B6350_E2A690->unk_18 > 150.0f) {
+                D_802B6350_E2A690->unk_18 = 150.0f;
+            }
+
+            playerStatus->position.y += tempY;
+            playerStatus->spriteFacingAngle = clamp_angle(360.0f - D_802B6350_E2A690->unk_10);
+            D_802B6350_E2A690->unk_14 += 0.6;
+            if (D_802B6350_E2A690->unk_14 > 40.0f) {
+                D_802B6350_E2A690->unk_14 = 40.0f;
+            }
+            if (--D_802B6350_E2A690->unk_00 == 0) {
+                playerStatus->fallState++;
+                entity_start_script(entity);
+            }
+            break;
+        case 1:
+            disable_player_shadow();
+            disable_npc_shadow(wPartnerNpc);
+            playerStatus->unk_10 = 0x32;
+            enable_player_static_collisions();
+            enable_player_input();
+            playerStatus->flags &= ~0x100008;
+            set_action_state(ACTION_STATE_IDLE);
+            break;
+    }
+}
+
