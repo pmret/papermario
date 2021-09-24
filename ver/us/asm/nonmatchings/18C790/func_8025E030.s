7f51368c9a716e01953c167d63aeb44b240fe688
JaThePlayer <janmalenta@gmail.com>
2021-09-24 23:23:56 +0200

    decomp parts of 18C790.c

--- ver/us/asm/nonmatchings/18C790/func_8025E030.s
+++
@@ -1,9 +0,0 @@
-.set noat      # allow manual use of $at
-.set noreorder # don't insert nops after branches
-
-glabel func_8025E030
-/* 18C910 8025E030 3C02802A */  lui       $v0, %hi(D_8029FB78)
-/* 18C914 8025E034 8C42FB78 */  lw        $v0, %lo(D_8029FB78)($v0)
-/* 18C918 8025E038 0002102B */  sltu      $v0, $zero, $v0
-/* 18C91C 8025E03C 03E00008 */  jr        $ra
-/* 18C920 8025E040 00021040 */   sll      $v0, $v0, 1
