210777fedb8bcef8ab5f83eb6424e97a381116aa
JoshDuMan <40190173+JoshDuMan@users.noreply.github.com>
2021-09-26 01:24:53 -0400

    Match func_802A10E4_743ED4

--- src/battle/move/power_bounce/743DF0.c
+++ src/battle/move/power_bounce/743DF0.c
@@ -3,12 +3,11 @@
 
 #define NAMESPACE battle_move_power_bounce
 
-extern s32* D_802A2730_745520;
+extern s32 D_802A2730_745520;
 extern s32 D_802A4020;
 
 #include "world/common/UnkMoveFunc1.inc.c"
 
-#ifdef NON_MATCHING
 ApiStatus func_802A10E4_743ED4(Evt* script, s32 isInitialCall) {
     Actor* targetActor = get_actor(get_actor(script->owner1.actorID)->targetActorID);
     s32 powerBounceChance;
@@ -18,16 +17,14 @@ ApiStatus func_802A10E4_743ED4(Evt* script, s32 isInitialCall) {
     if (is_ability_active(ABILITY_DODGE_MASTER)) {
         powerBounceChance += 7;
     }
+    D_802A2730_745520 = (D_802A2730_745520 * powerBounceChance / 100);
 
-    if ((*D_802A2730_745520 * powerBounceChance) / 100 < rand_int(100)) {
+    if (D_802A2730_745520 < rand_int(100)) {
         script->varTable[0] = 0;
     }
 
     return ApiStatus_DONE2;
 }
-#else
-INCLUDE_ASM(s32, "battle/move/power_bounce/743DF0", func_802A10E4_743ED4);
-#endif
 
 ApiStatus func_802A1194_743F84(Evt* script, s32 isInitialCall) {
     D_802A4020 = 0;
