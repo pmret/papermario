686d539af13db76c1f2650366a5bc97eb3f33493
ThreeMustyFears <>
2021-09-26 19:38:14 -0300

    matches

--- src/entity/Chest.c
+++ src/entity/Chest.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "effects.h"
 
 // size unknown
 typedef struct Chest {
@@ -19,6 +20,8 @@ typedef struct Chest {
     /* 0x28 */ f32 unk_28;
     /* 0x2C */ f32 unk_2C;
     /* 0x30 */ s8 unk_30;
+    /* 0x31 */ char unk_31[3];
+    /* 0x34 */ s32 unk_34;
 } Chest;
 
 extern s32 D_802EAD7C;
@@ -145,7 +148,97 @@ void entity_Chest_close(Entity* entity) {
 
 INCLUDE_ASM(s32, "entity/Chest", entity_GiantChest_hide_effect);
 
-INCLUDE_ASM(s32, "entity/Chest", entity_GiantChest_open);
+void entity_GiantChest_open(Entity* entity) {
+    PlayerStatus* playerStatus = &gPlayerStatus;
+    Chest* chest = entity->dataBuf;
+    f32 sp24;
+    f32 sp20;
+    f32 sp1C;
+    f32 sp18;
+    f32 theta;
+    f32 delta;
+    f32 zero;
+    f32 temp_f4;
+    f32 four;
+    f32 intermediateTheta;
+    f32 phi_f22;
+
+    if (chest->unk_10 != 0) {
+        set_item_entity_position(chest->unk_14, chest->unk_18, chest->unk_1C, chest->unk_20);
+    }
+    entity_Chest_open(entity);
+
+    switch (chest->unk_04) {
+        case 0:
+            chest->unk_02--;
+            if (chest->unk_02 == 0) {
+                chest->unk_24 = 0.0f;
+                chest->unk_04++;
+                chest->unk_28 = 20.0f;
+                chest->unk_2C = 8.0f;
+            }
+            break;
+        case 1:
+            chest->unk_1C += chest->unk_2C;
+            chest->unk_2C -= 0.7;
+            if (chest->unk_2C <= 2.0f) {
+                chest->unk_2C = 6.0f;
+                chest->unk_04++;
+            }
+            break;
+        case 2:
+            if (chest->unk_28 < 140.0f) {
+                chest->unk_1C += cos_rad(chest->unk_28 * TAU / 360.0f) * 3.0f;
+            } else {
+                temp_f4 = (chest->unk_1C - playerStatus->position.y - 30.0f) * 0.25f;
+                if (temp_f4 <= 0.4) {
+                    temp_f4 = 0.4f;
+                }
+                chest->unk_1C -= temp_f4;
+            }
+            delta = sin_rad(chest->unk_24 * TAU / 360.0f);
+            theta = intermediateTheta = clamp_angle(atan2(entity->position.x, entity->position.z,
+                                                          playerStatus->position.x, playerStatus->position.z));
+
+            if (gGameStatusPtr->areaID == 0x12) {
+                phi_f22 = 3.0f;
+            } else phi_f22 = 4.0f;
+
+            theta *= TAU;
+            theta = theta / 360.0f;
+            chest->unk_18 += (phi_f22 * sin_rad(theta) * delta);
+            chest->unk_20 -= (phi_f22 * cos_rad(theta) * delta);
+            chest->unk_24 += 8.0f;
+            if (chest->unk_24 >= 180.0f) {
+                chest->unk_24 = 180.0f;
+            }
+            chest->unk_28 += 13.0f;
+            if (chest->unk_28 >= 180.0f){
+                chest->unk_02 = 7;
+                chest->unk_28 = 180.0f;
+                chest->unk_04++;
+                if (chest->unk_10 != 0) {
+                    suggest_player_anim_setUnkFlag(0x6000C);
+                    sin_cos_rad((90.0f - gCameras->currentYaw) * TAU / 360.0f, &sp18, &sp1C);
+                    sin_cos_rad((180.0f - gCameras->currentYaw) * TAU / 360.0f, &sp20, &sp24);
+                    zero = 0.0f;
+                    four = 4.0f;
+                    playFX_33(0, chest->unk_18 + zero * sp18 - sp20 * four,
+                              chest->unk_1C + 12.0f, chest->unk_20 - sp1C * zero + sp24 * four, 1.0f, 0x64);
+                    playFX_1B(0, chest->unk_18 + zero * sp18 - sp20 * four,
+                              chest->unk_1C + 12.0f, chest->unk_20 - sp1C * zero + sp24 * four, 1.0f, &chest->unk_34);
+                }
+            }
+            break;
+        case 3:
+            chest->unk_02--;
+            if (chest->unk_02 == 0) {
+                exec_entity_commandlist(entity);
+                chest->unk_04 = 0;
+            }
+            break;
+    }
+}
 
 // the function itself matches, but somehow it breaks other completely unrelated files
 #ifdef NON_MATCHING
--- src/world/partner/bow.c
+++ src/world/partner/bow.c
@@ -1,9 +1,17 @@
 #include "common.h"
 #include "../partners.h"
+#include "../../npc.h"
 
 extern s32 D_802BE0C0;
 extern s32 D_802BE0C4;
+extern s32 D_802BE0E4;
+extern s32 D_802BE0E8;
+extern s32 D_802BE0EC;
+
 extern struct unkPartnerStruct* D_802BDFFC_32494C;
+
+void func_802BDDF0_324740(Npc* partner);
+s32 func_802BD540_323E90(void);
 
 void func_802BD100_323A50(Npc* partner) {
     partner->collisionHeight = 26;
@@ -97,14 +105,246 @@ s32 func_802BD168_323AB8(Evt* evt, s32 arg1) {
     return 0;
 }
 
-INCLUDE_ASM(s32, "world/partner/bow", func_802BD4FC_323E4C);
+void func_802BD4FC_323E4C(Npc* partner) {
+    if (D_8010C954 != 0) {
+        D_8010C954 = 0;
+        partner->flags = D_802BDFFC_32494C->unk_08;
+        D_802BDFFC_32494C->unk_04 = 0;
+        partner_clear_player_tracking(partner);
+    }
+}
 
 INCLUDE_ASM(s32, "world/partner/bow", func_802BD540_323E90);
 
-INCLUDE_ASM(s32, "world/partner/bow", func_802BD694_323FE4);
+s32 func_802BD694_323FE4(Evt* arg0, s32 arg1) {
+    PlayerStatus* playerStatus = &gPlayerStatus;
+    PartnerActionStatus* partnerActionStatus = &gPartnerActionStatus;
+    CollisionStatus* collisionStatus = &gCollisionStatus;
+    Npc* npc;
+    f32 distance;
 
-INCLUDE_ASM(s32, "world/partner/bow", func_802BDDF0_324740);
+    npc = arg0->owner2.npc;
+    if (arg1 != 0) {
+        func_802BD4FC_323E4C(npc);
+        if (!(playerStatus->animFlags & 0x100000)) {
+            if (func_800EA52C(9) != 0) {
+                if (playerStatus->animFlags & 0x200000) {
+                    playerStatus->animFlags &= ~0x200000;
+                    arg0->functionTemp[2] = disable_player_input();
+                    D_802BE0C4 = 1;
+                    arg0->functionTemp[0] = 20;
+                } else {
+                    arg0->functionTemp[0] = 40;
+                }
+            } else {
+                return 2;
+            }
+        } else {
+            return 2;
+        }
+    }
 
-INCLUDE_ASM(s32, "world/partner/bow", func_802BDF08_324858);
+    switch (arg0->functionTemp[0]) {
+        case 40:
+            if (playerStatus->statusMenuCounterinputEnabledCounter != 0) {
+                return 2;
+            }
+            playerStatus->flags |= 0x100;
+            arg0->functionTemp[1] = 3;
+            arg0->functionTemp[2] = disable_player_input();
+            D_802BE0C4 = 1;
+            arg0->functionTemp[0]++;
+            break;
+        case 41:
+            if ((func_800EA52C(9) == 0 || is_starting_conversation()) &&
+                 arg0->functionTemp[2] < playerStatus->statusMenuCounterinputEnabledCounter
+                 && D_802BE0C4 != 0) {
 
-INCLUDE_ASM(s32, "world/partner/bow", func_802BDF64_3248B4);
+                enable_player_input();
+                D_802BE0C4 = 0;
+                playerStatus->flags &= ~0x100;
+                return 2;
+            }
+            arg0->functionTemp[1]--;
+            if (arg0->functionTemp[1] == 0) {
+                if (arg0->functionTemp[2] < playerStatus->statusMenuCounterinputEnabledCounter) {
+                    if (D_802BE0C4 != 0) {
+                        enable_player_input();
+                        D_802BE0C4 = 0;
+                    }
+                    playerStatus->flags &= ~0x100;
+                    return 2;
+                }
+                arg0->functionTemp[0] = 20;
+            }
+            break;
+    }
+
+    switch (arg0->functionTemp[0]) {
+        case 20:
+            if ((playerStatus->flags & 0x800) != 0) {
+                playerStatus->flags &= ~0x100;
+                if (D_802BE0C4 != 0) {
+                    enable_player_input();
+                    D_802BE0C4 = 0;
+                }
+                return 2;
+            }
+            if (arg0->functionTemp[2] != 0) {
+                D_802BE0C4 = 1;
+            }
+
+            D_802BE0C0 = 1;
+            npc->flags &= ~0xA00;
+            partnerActionStatus->actionState.b[0] = 1;
+            partnerActionStatus->actionState.b[3] = 9;
+            playerStatus->flags |= 0x8000;
+            func_800EF4E0();
+            npc->moveToPos.x = playerStatus->position.x;
+            npc->moveToPos.y = playerStatus->position.y + (playerStatus->colliderHeight * 0.5f);
+            npc->moveToPos.z = playerStatus->position.z;
+            npc->currentAnim.w = 0x50002;
+            npc->yaw = playerStatus->targetYaw;
+            add_vec2D_polar(&npc->moveToPos.x, &npc->moveToPos.z, -2.0f, gCameras[gCurrentCameraID].currentYaw);
+            add_vec2D_polar(&npc->moveToPos.x, &npc->moveToPos.z,
+                            playerStatus->colliderDiameter * 0.5f, npc->yaw);
+            npc->duration = 5;
+            npc->yaw = atan2(npc->pos.x, npc->pos.z, playerStatus->position.x, playerStatus->position.z);
+            set_action_state(ACTION_STATE_RIDE);
+            suggest_player_anim_clearUnkFlag(0x10002);
+            arg0->functionTemp[0]++;
+            break;
+        case 21:
+            if (collisionStatus->currentFloor >= 0 && (playerStatus->animFlags & 0x100000) == 0) {
+                npc->moveToPos.x = playerStatus->position.x;
+                npc->moveToPos.y = playerStatus->position.y + (playerStatus->colliderHeight * 0.5f);
+                npc->moveToPos.z = playerStatus->position.z;
+                npc->pos.x += ((npc->moveToPos.x - npc->pos.x) / npc->duration);
+                npc->pos.y += ((npc->moveToPos.y - npc->pos.y) / npc->duration);
+                npc->pos.z += ((npc->moveToPos.z - npc->pos.z) / npc->duration);
+                D_802BE0E4 = playerStatus->position.x - npc->pos.x;
+                D_802BE0E8 = playerStatus->position.y - npc->pos.y;
+                D_802BE0EC = playerStatus->position.z - npc->pos.z;
+                npc->duration--;
+                if (npc->duration == 0) {
+                    npc->yaw = playerStatus->targetYaw;
+                    func_8003D624(npc, 7, playerStatus->alpha1, 0, 0, 0, 0);
+                    suggest_player_anim_setUnkFlag(0x10014);
+                    sfx_play_sound_at_npc(SOUND_BOW_VANISH, 0, -4);
+                    arg0->functionTemp[0] = 1;
+                }
+                break;
+            }
+            func_802BDDF0_324740(npc);
+            return 2;
+        case 1:
+            if (collisionStatus->currentFloor >= 0) {
+                playerStatus->alpha1 -= 8;
+                if (playerStatus->alpha1 < 0x81) {
+                    playerStatus->alpha1 = 0x80;
+                    npc->renderMode = 0x16;
+                    arg0->functionTemp[0]++;
+                    playerStatus->flags &= ~0x100;
+                    npc->flags |= 0x40;
+                }
+                get_shadow_by_index(npc->shadowIndex)->unk_05 = playerStatus->alpha1 >> 1;
+                func_8003D624(npc, 7, playerStatus->alpha1, 0, 0, 0, 0);
+                npc->pos.x = playerStatus->position.x - D_802BE0E4;
+                npc->pos.y = playerStatus->position.y - D_802BE0E8;
+                npc->pos.z = playerStatus->position.z - D_802BE0EC;
+                break;
+            }
+            func_802BDDF0_324740(npc);
+            return 2;
+        case 2:
+            if (collisionStatus->currentFloor < 0) {
+                func_802BDDF0_324740(npc);
+                return 2;
+            }
+            npc->pos.x = playerStatus->position.x - D_802BE0E4;
+            npc->pos.y = playerStatus->position.y - D_802BE0E8;
+            npc->pos.z = playerStatus->position.z - D_802BE0EC;
+
+            distance = dist2D(0.0f, 0.0f, partnerActionStatus->stickX, partnerActionStatus->stickY);
+            if ((collisionStatus->currentFloor < 0) || distance > 10.0f ||
+                 partnerActionStatus->pressedButtons & (BUTTON_B | BUTTON_C_DOWN) || playerStatus->flags & 0x800) {
+                if (func_802BD540_323E90() < 0) {
+                    arg0->functionTemp[0]++;
+                    arg0->functionTemp[1] = 3;
+                    arg0->functionTemp[2] = playerStatus->statusMenuCounterinputEnabledCounter;
+                }
+            }
+            break;
+        case 3:
+            if (arg0->functionTemp[1] == 0) {
+                if (arg0->functionTemp[2] < playerStatus->statusMenuCounterinputEnabledCounter) {
+                    arg0->functionTemp[0] = 2;
+                    break;
+                }
+            } else {
+                arg0->functionTemp[1]--;
+                break;
+            }
+
+            sfx_play_sound_at_npc(SOUND_BOW_APPEAR, 0, -4);
+            func_802BDDF0_324740(npc);
+            return 1;
+    }
+    return 0;
+}
+
+void func_802BDDF0_324740(Npc* partner) {
+    PlayerStatus* playerStatus = &gPlayerStatus;
+    PartnerActionStatus* partnerActionStatus = &gPartnerActionStatus;
+    s32 actionState;
+
+    playerStatus->alpha1 = 0xFF;
+    func_8003D624(partner, 0, 0, 0, 0, 0, 0);
+    partner->renderMode = 0x11;
+    get_shadow_by_index(partner->shadowIndex)->unk_05 = playerStatus->alpha1 >> 1;
+
+    if (D_802BE0C4 != 0) {
+        enable_player_input();
+    }
+
+    playerStatus->flags &= ~0x8002;
+    partner->flags &= ~0x42;
+    D_802BE0C4 = 0;
+    actionState = ACTION_STATE_IDLE;
+    if ((playerStatus->flags & 0x800) != 0) {
+        actionState = ACTION_STATE_HIT_LAVA;
+    }
+
+    set_action_state(actionState);
+    partnerActionStatus->actionState.b[0] = 0;
+    partnerActionStatus->actionState.b[3] = 0;
+    playerStatus->flags &= ~0x100;
+    partner_clear_player_tracking(partner);
+    D_802BE0C0 = 0;
+}
+
+s32 func_802BDF08_324858(Evt* arg0, s32 arg1) {
+    Npc* partner = arg0->owner2.npc;
+    if (arg1 != 0) {
+        partner_init_put_away(partner);
+        if (D_802BE0C0 != 0) {
+            sfx_play_sound_at_npc(SOUND_BOW_APPEAR, 0, -4);
+        }
+        func_802BDDF0_324740(partner);
+    }
+    return partner_put_away(partner) != 0;
+}
+
+void func_802BDF64_3248B4(Npc* arg0) {
+    PartnerActionStatus* partnerActionStatus = &gPartnerActionStatus;
+
+    if (D_802BE0C0 != 0) {
+        enable_player_input();
+        set_action_state(ACTION_STATE_IDLE);
+        partner_clear_player_tracking(arg0);
+        partnerActionStatus->actionState.b[0] = 0;
+        partnerActionStatus->actionState.b[3] = 0;
+        D_802BE0C0 = 0;
+        arg0->flags &= ~0x2;
+    }
+}
