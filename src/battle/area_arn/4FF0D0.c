f9357a6ce169b6e215199c13c406771e5108f6bf
eperdew <eperdew17@gmail.com>
2021-09-27 20:46:20 -0700

    Decompile area_arn/4550D0.c and area_dig/6DE1F0.c.

--- src/battle/area_arn/4FF0D0.c
+++ src/battle/area_arn/4FF0D0.c
@@ -2,4 +2,25 @@
 
 #define NAMESPACE b_area_arn
 
-INCLUDE_ASM(s32, "battle/area_arn/4FF0D0", func_80218680_4FF0D0);
+ApiStatus func_80218680_4FF0D0(Evt* script, s32 isInitialCall) {
+    Bytecode *readPos;
+    s32 var1;
+    s32 var2;
+    s32 var3;
+    f32 var4;
+    Temp8025D160* temp8025D160;
+
+    readPos = script->ptrReadPos;
+    temp8025D160 = (Temp8025D160*)evt_get_variable(script, *readPos++);
+    var1 = evt_get_variable(script, *readPos++);
+    var2 = evt_get_variable(script, *readPos++);
+    var3 = evt_get_variable(script, *readPos++);
+    var4 = evt_get_float_variable(script, *readPos++);
+
+    temp8025D160->unk_0C->unk_04 = var1;
+    temp8025D160->unk_0C->unk_08 = var2;
+    temp8025D160->unk_0C->unk_0C = var3;
+    temp8025D160->unk_0C->unk_34 = var4;
+
+    return ApiStatus_DONE2;
+}
