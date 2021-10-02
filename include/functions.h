c1427d1cce462a3a829baf8fa5ca76b650e419e4
Ethan Roseman <ethteck@gmail.com>
2021-10-02 11:14:48 +0900

    mdl_get_next_texture_address

--- include/functions.h
+++ include/functions.h
@@ -492,7 +492,7 @@ void* load_asset_by_name(const char* assetName, u32* decompressedSize);
 void mdl_draw_hidden_panel_surface(Gfx** arg0, u16 treeIndex);
 s32 func_8011CFBC(void);
 s32 set_screen_overlay_center_worldpos(void);
-s32 mdl_get_next_texture_address(void);
+s32 mdl_get_next_texture_address(s32);
 void draw_msg(s32 msgID, s32 posX, s32 posY, s32 opacity, s32 palette, s32 style);
 void get_background_color_blend(u8* r, u8* g, u8* b, u8* a);
 
