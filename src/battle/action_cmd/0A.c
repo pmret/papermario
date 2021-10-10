7a79145202372604d92f2a046abab2d9732d07aa
eperdew <eperdew17@gmail.com>
2021-10-09 18:05:35 -0700

    Decompile small functions in 0A.c and air_raid.c

--- src/battle/action_cmd/0A.c
+++ src/battle/action_cmd/0A.c
@@ -2,7 +2,58 @@
 
 #define NAMESPACE action_command_0A
 
-INCLUDE_ASM(s32, "battle/action_cmd/0A", func_802A9000_4273B0);
+extern s32 D_802942E0;
+extern HudElementAnim D_80292974;
+extern HudElementAnim D_80108AFC;
+extern HudElementAnim D_80108B28;
+
+ApiStatus func_802A9000_4273B0(Evt* script, s32 isInitialCall) {
+    ActionCommandStatus* actionCommandStatus = &gActionCommandStatus;
+    BattleStatus* battleStatus = &gBattleStatus;
+    s32 hudElement;
+
+    battleStatus->unk_82 = 100;
+    battleStatus->unk_434 = &D_802942E0;
+    battleStatus->unk_86 = 127;
+
+    if (battleStatus->unk_83 == 0) {
+        battleStatus->actionSuccess = 0;
+        return ApiStatus_DONE2;
+    }
+
+    func_80268858();
+
+    actionCommandStatus->actionCommandID = 10;
+    actionCommandStatus->unk_61 = 1;
+    actionCommandStatus->state = 0;
+    actionCommandStatus->unk_60 = 0;
+    actionCommandStatus->barFillLevel = 0;
+    actionCommandStatus->unk_48 = 0;
+    actionCommandStatus->unk_68 = 0;
+    battleStatus->actionSuccess = 0;
+    actionCommandStatus->hudElementX = -48;
+    actionCommandStatus->hudElementY = 80;
+
+    hudElement = create_hud_element(&D_80108B28);
+    actionCommandStatus->hudElements[0] = hudElement;
+    set_hud_element_render_pos(hudElement, actionCommandStatus->hudElementX, actionCommandStatus->hudElementY);
+    set_hud_element_render_depth(hudElement, 0);
+    set_hud_element_flags(hudElement, 0x82);
+
+    hudElement = create_hud_element(&D_80108AFC);
+    actionCommandStatus->hudElements[1] = hudElement;
+    set_hud_element_render_pos(hudElement, actionCommandStatus->hudElementX, actionCommandStatus->hudElementY + 28);
+    set_hud_element_render_depth(hudElement, 0);
+    set_hud_element_flags(hudElement, 0x82);
+
+    hudElement = create_hud_element(&D_80292974);
+    actionCommandStatus->hudElements[2] = hudElement;
+    set_hud_element_render_pos(hudElement, actionCommandStatus->hudElementX, actionCommandStatus->hudElementY + 28);
+    set_hud_element_render_depth(hudElement, 0);
+    set_hud_element_flags(hudElement, 0x82);
+
+    return ApiStatus_DONE2;
+}
 
 #include "common/MashActionCommandInit.inc.c"
 
