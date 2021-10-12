4a10225fd8759555dbca503a495b376516b584d4
JoshDuMan <40190173+JoshDuMan@users.noreply.github.com>
2021-10-12 02:00:20 -0400

    Address Ethan's wants

--- src/world/area_sam/sam_04/D13810.c
+++ src/world/area_sam/sam_04/D13810.c
@@ -3,17 +3,12 @@
 ApiStatus func_80240240_D13810 (Evt* script, s32 isInitialCall) {
     Bytecode* args = script->ptrReadPos;
     s32 var = *args++;
-    f32 x1, x2;
-    f32 z1, z2;
-    f32 angle;
+    f32 x1 = evt_get_float_variable(script, *args++);
+    f32 z1 = evt_get_float_variable(script, *args++);
+    f32 x2 = evt_get_float_variable(script, *args++);
+    f32 z2 = evt_get_float_variable(script, *args++);    
+    f32 angle = clamp_angle(atan2(x1, z1, x2, z2));
 
-    x1 = evt_get_float_variable(script, *args++);
-    z1 = evt_get_float_variable(script, *args++);
-    x2 = evt_get_float_variable(script, *args++);
-    z2 = evt_get_float_variable(script, *args++);
-
-    angle = clamp_angle(atan2(x1, z1, x2, z2));
     evt_set_float_variable(script, var, angle);
-
     return ApiStatus_DONE2;
 }
