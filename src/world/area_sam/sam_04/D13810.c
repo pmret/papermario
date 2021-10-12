0a23fb6beec622dbd3cfa1c05aa008c7e7545feb
JoshDuMan <40190173+JoshDuMan@users.noreply.github.com>
2021-10-12 01:51:16 -0400

    Match func_80240240_D13810

--- src/world/area_sam/sam_04/D13810.c
+++ src/world/area_sam/sam_04/D13810.c
@@ -1,3 +1,19 @@
 #include "sam_04.h"
 
-INCLUDE_ASM(s32, "world/area_sam/sam_04/D13810", func_80240240_D13810);
+ApiStatus func_80240240_D13810 (Evt* script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
+    s32 var = *args++;
+    f32 x1, x2;
+    f32 z1, z2;
+    f32 angle;
+
+    x1 = evt_get_float_variable(script, *args++);
+    z1 = evt_get_float_variable(script, *args++);
+    x2 = evt_get_float_variable(script, *args++);
+    z2 = evt_get_float_variable(script, *args++);
+
+    angle = clamp_angle(atan2(x1, z1, x2, z2));
+    evt_set_float_variable(script, var, angle);
+
+    return ApiStatus_DONE2;
+}
