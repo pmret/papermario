fa9b81280e97a3a3f0a5a771d2e4cf58c1c1fe0c
Lightning <LightningTH@GMail.com>
2021-10-25 08:06:32 -0700

    Add comment detailing purpose of genobjcopy

--- tools/build/genobjcopy.py
+++ tools/build/genobjcopy.py
@@ -1,6 +1,15 @@
 #! /usr/bin/python3
 import sys, os
 
+#Under normal compilation we rely on splat to use a discard option in the ldscript
+#to not include sections in the elf then just output all sections, however under debug we want
+#to have debug sections.
+#In debugging mode splat is told to output a list of sections it is custom creating, which are
+#all of the sections we export to the z64 file with an objcopy. The below chunk of code is
+#responsible for adding -j to each of the names and outputting a file for objcopy to use
+#so we can still generate a elf file with all the extra debugging sections and still output
+# the required sections to the .z64 without outputting everything.
+
 if __name__ == "__main__":
 	infile, outfile = sys.argv[1:]
 
