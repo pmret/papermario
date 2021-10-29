cddf97a2a2cb563550cb48485b4f10656b52d6ef
ponmander <ponmander@gmail.com>
2021-10-29 13:13:23 -0500

    FINAL final fix.

---
+++ src/os/osEPiLinkHandle.c
@@ -0,0 +1,14 @@
+#include "common.h"
+#include "include_asm_libultra.h"
+
+#ifndef KMC_ASM
+s32 osEPiLinkHandle(OSPiHandle* EPiHandle) {
+    u32 saveMask = __osDisableInt();
+    EPiHandle->next = __osPiTable;
+    __osPiTable = EPiHandle;
+    __osRestoreInt(saveMask);
+    return 0;
+}
+#else
+INCLUDE_ASM_LIBULTRA("osEPiLinkHandle", osEPiLinkHandle);
+#endif
---
+++ src/os/osEPiWriteIo.c
@@ -0,0 +1,15 @@
+#ifdef KMC_ASM
+#define MOVE_ADDU
+#endif
+
+#include "common.h"
+
+s32 osEPiWriteIo(OSPiHandle* handle, u32 devAddr, u32 data) {
+    register s32 ret;
+
+    osPiGetAccess();
+    ret = osEPiRawWriteIo(handle, devAddr, data);
+    osPiRelAccess();
+
+    return ret;
+}
