37a71f55337f1f9743358cf39478932d7e5636ea
Ethan Roseman <ethteck@gmail.com>
2021-11-10 20:51:24 +0900

    oopth

--- progress.py
+++ progress.py
@@ -91,8 +91,8 @@ def main(args):
 
     matching_size, nonmatching_size = get_funcs_sizes(func_sizes, matching_funcs, nonmatching_funcs)
 
-    print("\nMatching funcs:\n" + matching_funcs)
-    print("\nNonmatching funcs:\n" + nonmatching_funcs)
+    print("\nMatching funcs:\n" + str(matching_funcs))
+    print("\nNonmatching funcs:\n" + str(nonmatching_funcs))
 
     if len(all_funcs) == 0:
         funcs_matching_ratio = 0.0
