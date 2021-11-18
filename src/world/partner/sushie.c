da0882faa96d835626b58f7a4c1e3a9814fbb1fc
Rain <rainmachus@gmail.com>
2021-11-18 10:29:34 -0600

    decomp world sushie/watt files

--- src/world/partner/sushie.c
+++ src/world/partner/sushie.c
@@ -1,24 +1,138 @@
 #include "common.h"
+#include "../src/world/partners.h"
+#include "npc.h"
 
 extern unkPartnerStruct* D_802BFDF8_320B68;
+extern s32 D_802BFEEC;
+extern f32 D_802BFEE0;
+extern void func_802BD368_31E0D8(s32, f32, f32, f32, f32, f32);
+extern void playFX_23(s32, f32, f32, f32, s32);
+extern s32 partner_use_ability(void);
+extern void partner_kill_ability_script(void);
+extern s32 D_802BFEE4;
+extern s32 D_802BFEE8;
+extern s32 D_802BFEF0;
+extern s32 D_802BFEF4;
+extern f32 D_802BFDB0_320B20;
+extern f32 D_802BFDB4_320B24;
+extern s16 D_8010C97A;
 
-INCLUDE_ASM(s32, "world/partner/sushie", func_802BD100_31DE70);
 
-INCLUDE_ASM(s32, "world/partner/sushie", func_802BD20C_31DF7C);
+void func_802BD100_31DE70(void) {
+    Npc* partnerNPC = get_npc_unsafe(NPC_PARTNER);
+    PlayerStatus* playerStatus = &gPlayerStatus;
+    Camera* cam =  gCameras;
+    s32 phi_v1;
 
-INCLUDE_ASM(s32, "world/partner/sushie", func_802BD368_31E0D8);
+    playerStatus->position.x = partnerNPC->pos.x;
+    playerStatus->position.y = partnerNPC->pos.y + 16.0f;
+    playerStatus->position.z = partnerNPC->pos.z;
+    playerStatus->targetYaw = partnerNPC->yaw;
+
+    if (playerStatus->spriteFacingAngle < 90.0f || playerStatus->spriteFacingAngle > 270.0f) {
+        phi_v1 = -8;
+    } else {
+        phi_v1 = 8;
+    }
+
+    playerStatus->position.z -= cos_rad(((((cam->currentYaw + playerStatus->spriteFacingAngle) - 90.0f) + phi_v1) * TAU) / 360.0f) * -4.0f;
+}
+
+void func_802BD20C_31DF7C(f32* arg0, f32* arg1) {
+    f32 temp_f0;
+    f32 temp_f0_2;
+    f32 temp_f22;
+    f32 temp_f2;
+    f32 temp_f2_2;
+    f32 phi_f20;
+
+    temp_f2 = gPartnerActionStatus.stickY;
+    temp_f0 = gPartnerActionStatus.stickX;
+    D_802BFDB0_320B20 = temp_f0;
+    D_802BFDB4_320B24 = temp_f2;
+    temp_f22 = clamp_angle(atan2(0.0f, 0.0f, temp_f0, -temp_f2) + gCameras->currentYaw);
+    phi_f20 = 0.0f;
+
+    //TODO make not garbage to read
+    if ((dist2D(0.0f, 0.0f, D_802BFDB0_320B20, -D_802BFDB4_320B24) >= 1.0) && ((temp_f0_2 = D_802BFDB0_320B20, temp_f2_2 = D_802BFDB4_320B24, !(((temp_f0_2 * temp_f0_2) + (temp_f2_2 * temp_f2_2)) > 3025.0f)) || (phi_f20 = 4.0f, (D_802BFEE4 != 0)))) {
+        phi_f20 = 2.0f;
+    }
+
+/*
+    if (dist2D(0.0f, 0.0f, D_802BFDB0_320B20, -D_802BFDB4_320B24) >= 1.0) {
+        phi_f20 = 4.0f;
+        if ( !((SQ(D_802BFDB0_320B20)) + (SQ(D_802BFDB4_320B24)) > 3025.0f) || D_802BFEE4 != 0) {
+            phi_f20 = 2.0f;
+        }
+    }
+*/
+
+    *arg0 = temp_f22;
+    *arg1 = phi_f20;
+}
+
+void func_802BD368_31E0D8(s32 arg0, f32 arg1, f32 arg2, f32 arg3, f32 arg4, f32 arg5) {
+    CollisionStatus* collisionStatus = &gCollisionStatus;
+    f32 sp20;
+
+    arg2 += 100.0f;
+    sp20 = 200.0f;
+    
+    if (npc_raycast_down_ahead(arg0, &arg1, &arg2, &arg3, &sp20, arg4, arg5) == 0) {
+        collisionStatus->currentFloor = -1;
+        return;
+    }
+
+    collisionStatus->currentFloor = D_8010C97A;
+    D_802BFEE0 = arg2;
+}
 
 INCLUDE_ASM(s32, "world/partner/sushie", func_802BD414_31E184);
 
-INCLUDE_ASM(s32, "world/partner/sushie", func_802BE280_31EFF0);
+s32 func_802BE280_31EFF0(s32 arg0, f32* arg1, f32* arg2, f32* arg3, f32 arg4, f32 arg5, f32* arg6) {
+    f32 sp38, sp3C, sp40, sp44, sp48, sp4C, sp50, sp54, sp58;
+    s32 colliderRayResult;
+
+    sin_cos_rad((*arg6 * TAU) / 360.0f, &sp38, &sp3C);
+    sp3C = -sp3C;
+    sp4C = arg5 + arg4;
+    colliderRayResult = test_ray_colliders(0x10000, *arg1, *arg2, *arg3, sp38, 0.0f, sp3C, &sp40, &sp44, &sp48, &sp4C, &sp50, &sp54, &sp58);
+
+    if (colliderRayResult >= 0) {
+        *arg6 = atan2(0.0f, 0.0f, sp50, sp58);
+    }
+
+    return colliderRayResult;
+}
 
 INCLUDE_ASM(s32, "world/partner/sushie", func_802BE3A4_31F114);
 
-INCLUDE_ASM(s32, "world/partner/sushie", func_802BF520_320290);
+void func_802BF520_320290(Npc* npc) {
+    npc->collisionHeight = 0x18;
+    npc->collisionRadius = 0x24;
+    npc->unk_80 = 0x10000;
+    D_802BFEEC = 0;
+    D_802BFEE4 = 0;
+    D_802BFEE8 = 0;
+    D_802BFEF0 = 0;
+    D_802BFEF4 = 0;
+}
 
-INCLUDE_ASM(s32, "world/partner/sushie", func_802BF568_3202D8);
+s32 func_802BF568_3202D8(Evt* script, s32 isInitialCall) {
+    Npc* npc = script->owner2.npc;
+ 
+    if (isInitialCall != 0) {
+        partner_init_get_out(npc);
+    }
 
-s32 func_802BF5A0_320310(Evt* evt, s32 arg1) {
+    if (partner_get_out(npc)) {
+        return ApiStatus_DONE1;
+    } else {
+        return ApiStatus_BLOCK;
+    }
+}
+
+ApiStatus func_802BF5A0_320310(Evt* evt, s32 arg1) {
     Npc* npc;
     Entity* entity;
     f32 sp10;
@@ -36,7 +150,7 @@ s32 func_802BF5A0_320310(Evt* evt, s32 arg1) {
     if (entity == NULL) {
         partner_walking_update_player_tracking(npc);
         partner_walking_update_motion(npc);
-        return 0;
+        return ApiStatus_BLOCK;
     }
 
     switch (D_802BFDF8_320B68->unk_04) {
@@ -97,15 +211,127 @@ s32 func_802BF5A0_320310(Evt* evt, s32 arg1) {
             }
             break;
     }
-    return 0;
+
+    return ApiStatus_BLOCK;
 }
 
-INCLUDE_ASM(s32, "world/partner/sushie", func_802BF920_320690);
+void func_802BF920_320690(Npc* npc) {
+    if (D_8010C954 != 0) {
+        D_8010C954 = NULL;
+        npc->flags = D_802BFDF8_320B68->unk_08;
+        D_802BFDF8_320B68->unk_04 = 0;
+        partner_clear_player_tracking(npc);
+    }
+}
 
-INCLUDE_ASM(s32, "world/partner/sushie", func_802BF964_3206D4);
+s32 func_802BF964_3206D4(Evt* script, s32 isInitialCall) {
+    Npc* npc = script->owner2.npc;
 
-INCLUDE_ASM(s32, "world/partner/sushie", func_802BF9B8_320728);
+    if (isInitialCall != 0) {
+        partner_init_put_away(npc);
+        gPlayerStatusPtr->animFlags &= 0xFFBFFFFF;
+    }
 
-INCLUDE_ASM(s32, "world/partner/sushie", func_802BFA58_3207C8);
+    if (partner_put_away(npc)) {
+        return ApiStatus_DONE1;
+    } else {
+        return ApiStatus_BLOCK;
+    }
 
-INCLUDE_ASM(s32, "world/partner/sushie", func_802BFAB8_320828);
+}
+
+void func_802BF9B8_320728(Npc* npc) {
+    PartnerActionStatus* partnerActionStatus = &gPartnerActionStatus;
+
+    if (D_802BFEEC != 0) {
+        partnerActionStatus->npc = *npc;
+        partnerActionStatus->actionState.b[1] = 1;
+        enable_player_static_collisions();
+        enable_player_input();
+        set_action_state(0);
+        partner_clear_player_tracking(npc);
+    }
+
+    partnerActionStatus->actionState.b[3] = 7;
+}
+
+void func_802BFA58_3207C8(Npc* npc) {
+    if (gPartnerActionStatus.actionState.b[1] != 0) {
+        *npc = gPartnerActionStatus.npc;
+        partner_use_ability();
+    }
+}
+
+s32 func_802BFAB8_320828(Evt* script, s32 isInitialCall) {
+    Npc* partnerNPC = get_npc_unsafe(NPC_PARTNER);
+    PlayerStatus* playerStatus = &gPlayerStatus;
+    f32 temp_f0;
+    s32 funcTemp0;
+
+    if (isInitialCall != 0) {
+        script->functionTemp[0] = 0;
+        D_802BFEE0 = playerStatus->position.y;
+    }
+    
+    funcTemp0 = script->functionTemp[0];
+
+    switch (funcTemp0) {
+        case 0:
+            gGameStatusPtr->unk_7D = 1;
+            disable_player_static_collisions();
+            disable_player_input();
+            partnerNPC->pos.x = playerStatus->position.x;
+            partnerNPC->pos.z = playerStatus->position.z;
+            partnerNPC->pos.y = playerStatus->position.y;
+            func_802BD368_31E0D8(partnerNPC->unk_80, partnerNPC->pos.x, partnerNPC->pos.y, partnerNPC->pos.z, partnerNPC->yaw, partnerNPC->collisionRadius * 0.5f);
+            partnerNPC->pos.y = D_802BFEE0 - (partnerNPC->collisionHeight * 0.5f);
+            temp_f0 = atan2(partnerNPC->pos.x, partnerNPC->pos.z, script->varTable[1], script->varTable[3]);
+            partnerNPC->currentAnim.w = 0x7000A;
+            partnerNPC->yaw = temp_f0;
+            partnerNPC->jumpScale = 0.0f;
+            partnerNPC->moveSpeed = 3.0f;
+            partnerNPC->moveToPos.x = partnerNPC->pos.x;
+            partnerNPC->moveToPos.y = partnerNPC->pos.y;
+            partnerNPC->moveToPos.z = partnerNPC->pos.z;
+            partnerNPC->flags = (partnerNPC->flags | 0x108) & ~0x200;
+            disable_npc_shadow(partnerNPC);
+            disable_player_shadow();
+
+            if (script->varTable[0xC] == 0) {
+                partner_kill_ability_script();
+            } else {
+                suggest_player_anim_setUnkFlag(0x8000F);
+                if ((partnerNPC->yaw >= 0.0f) && (partnerNPC->yaw <= 180.0f)) {
+                    partnerNPC->yawCamOffset = partnerNPC->yaw;
+                    partnerNPC->isFacingAway = 1;
+                }
+            }
+
+            script->functionTemp[1] = 0x19;
+            script->functionTemp[0] = 1;
+            break;
+        case 1:
+            npc_move_heading(partnerNPC, partnerNPC->moveSpeed, partnerNPC->yaw);
+            func_802BD100_31DE70();
+            if (!(script->functionTemp[1] & 3)) {
+                playFX_23(0, partnerNPC->pos.x, partnerNPC->moveToPos.y + (partnerNPC->collisionHeight * 0.5f), partnerNPC->pos.z, 0);
+            }
+
+            script->functionTemp[1]--;
+            if (script->functionTemp[1] == 0) {
+                if (script->varTable[0xC] == funcTemp0) {
+                    set_action_state(0x21);
+                    partner_use_ability();
+                    return ApiStatus_DONE2;
+                }
+
+                enable_player_static_collisions();
+                enable_player_input();
+                return ApiStatus_DONE2;
+            }
+
+            break;
+    }
+
+    return ApiStatus_BLOCK;
+}
--- src/world/partner/watt.c
+++ src/world/partner/watt.c
@@ -7,23 +7,39 @@ void force_player_anim(s32 arg0);
 void func_802BE070_31DBE0(void);
 void partner_kill_ability_script(void);
 s32 partner_use_ability(void);
+extern s32 D_802BE278_31DDE8;
+extern f64 D_802BE2F0_31DE60;
+extern s32 D_802BE250_31DDC0;
 
 typedef struct unk_802BE310_C {
-    /* 0x00 */ char unk_00[0x28];
+    /* 0x00 */ s32 unk_00;
+    /* 0x04 */ f32 unk_04;
+    /* 0x08 */ f32 unk_08;
+    /* 0x0C */ f32 unk_0C;
+    /* 0x10 */ char unk_10[0x10];
+    /* 0x20 */ s32 unk_20;
+    /* 0x24 */ s32 unk_24;
     /* 0x28 */ s32 unk_28;
+    /* 0x2C */ char unk_2C[0x0C];
 } unk_802BE310_C;
 
 typedef struct unk_802BE310 {
-    /* 0x00 */ char unk_00[0x0C];
+    /* 0x00 */ s32 unk_00;
+    /* 0x04 */ s32 unk_04;
+    /* 0x08 */ s32 unk_08;
     /* 0x0C */ unk_802BE310_C* unk_0C;
     /* 0x10 */
 } unk_802BE310;
 
 typedef struct unk_802BE274_31DDE4 {
-    /* 0x00 */ char unk_00[4];
+    /* 0x00 */ s32 unk_00;
     /* 0x04 */ s32 unk_04;
     /* 0x08 */ s32 unk_08;
-} unk_802BE274_31DDE4;
+    /* 0x0C */ f32 unk_0C;
+    /* 0x10 */ f32 unk_10;
+    /* 0x14 */ f32 unk_14;
+    /* 0x18 */ f32 unk_18;
+} unk_802BE274_31DDE4; //sizeof 0x1C
 
 extern s32 D_802BE300;
 extern s32 D_802BE304;
@@ -74,12 +90,125 @@ ApiStatus func_802BD27C_31CDEC(Evt* script, s32 isInitialCall) {
     }
 }
 
+ApiStatus func_802BD2B4_31CE24(Evt *script, s32 isInitialCall) {
+    PlayerData* playerData = &gPlayerData;
+    Entity* entity;
+    f32 sp10, sp14;
+    f32 new_var;
+    f32 temp_f0;
+    Npc* npc = script->owner2.npcID;
+    
+    if (gPartnerActionStatus.actionState.b[1] == 0) {
+        if (isInitialCall != 0) {
+            partner_flying_enable(npc, 1);
+            mem_clear(D_802BE274_31DDE4, sizeof(unk_802BE274_31DDE4));
+            D_8010C954 = NULL;
+        }
 
-INCLUDE_ASM(s32, "world/partner/watt", func_802BD2B4_31CE24);
+        entity = D_8010C954;
+        playerData->unk_2F4[6]++;
+
+        if (entity == NULL) {
+            func_802BD100_31CC70(npc);
+            partner_flying_update_player_tracking(npc);
+            partner_flying_update_motion(npc);
+            if (npc->moveSpeed != 0.0f) {
+                if (D_802BE278_31DDE8 == 0) {
+                    D_802BE278_31DDE8 = 1;
+                    func_802BD1AC_31CD1C(D_802BE278_31DDE8);
+                    npc->currentAnim.w = 0x60003;
+                }
+            } else if (D_802BE278_31DDE8 != 0) {
+                D_802BE278_31DDE8 = 0;
+                func_802BD1AC_31CD1C(D_802BE278_31DDE8);
+                npc->currentAnim.w = 0x60001;
+            }
+
+            if (D_802BE310 != 0) {
+                D_802BE310->unk_0C->unk_04 = npc->pos.x;
+                D_802BE310->unk_0C->unk_08 = npc->pos.y + 13.0f;
+                D_802BE310->unk_0C->unk_0C = npc->pos.z;
+            }
+
+            return 0;
+
+        } else {
+                switch (D_802BE274_31DDE4->unk_04) {
+                    case 0:
+                        D_802BE274_31DDE4->unk_04 = 1;
+                        D_802BE274_31DDE4->unk_08 = npc->flags;
+                        D_802BE274_31DDE4->unk_0C = fabsf(dist2D(npc->pos.x, npc->pos.z, entity->position.x, entity->position.z));
+                        D_802BE274_31DDE4->unk_10 = atan2(entity->position.x, entity->position.z, npc->pos.x, npc->pos.z);
+                        D_802BE274_31DDE4->unk_14 = 6.0f;
+                        D_802BE274_31DDE4->unk_18 = 50.0f;
+                        D_802BE274_31DDE4->unk_00 = 0x78;
+                        npc->flags |= 0x40148;
+                        npc->flags &= ~0x200;
+                    case 1:
+                        sin_cos_rad((D_802BE274_31DDE4->unk_10 * TAU) / 360.0f, &sp10, &sp14);
+                        npc->pos.x = (entity->position.x + (sp10 * D_802BE274_31DDE4->unk_0C));
+                        npc->pos.z = (entity->position.z - (sp14 * D_802BE274_31DDE4->unk_0C));
+                        D_802BE274_31DDE4->unk_10 = clamp_angle(D_802BE274_31DDE4->unk_10 - D_802BE274_31DDE4->unk_14);
+
+                        if (D_802BE274_31DDE4->unk_0C > 20.0f) {
+                            D_802BE274_31DDE4->unk_0C--;
+                        } else if (D_802BE274_31DDE4->unk_0C < 19.0f) {
+                            D_802BE274_31DDE4->unk_0C++;
+                        }
+
+                        temp_f0 = sin_rad((D_802BE274_31DDE4->unk_18 * TAU) / 360.0f);
+                        D_802BE274_31DDE4->unk_18 += 3.0f;
+
+                        new_var = temp_f0 * 3.0f;
+
+                        if (D_802BE274_31DDE4->unk_18 > 150.0f) {
+                            D_802BE274_31DDE4->unk_18 = 150.0f;
+                        }
+                        
+                        npc->pos.y += new_var;
+                        npc->renderYaw = clamp_angle(360.0f - D_802BE274_31DDE4->unk_10);
+                        D_802BE274_31DDE4->unk_14 += D_802BE2F0_31DE60;
+
+                        if (D_802BE274_31DDE4->unk_14 > 40.0f) {
+                            D_802BE274_31DDE4->unk_14 = 40.0f;
+                        }
+
+                        if (--D_802BE274_31DDE4->unk_00 == 0) {
+                            D_802BE274_31DDE4->unk_04++;
+                        }
+                        break;
+
+                    case 2:
+                        npc->flags = D_802BE274_31DDE4->unk_08;
+                        D_802BE274_31DDE4->unk_00 = 0x1E;
+                        D_802BE274_31DDE4->unk_04++;
+                        break;
+                    case 3:
+                        partner_flying_update_player_tracking(npc);
+                        partner_flying_update_motion(npc);
+                        if (--D_802BE274_31DDE4->unk_00 == 0) {
+                            D_802BE274_31DDE4->unk_04 = 0;
+                            D_8010C954 = NULL;
+                        }
+
+                        break;
+                }
+            }
+
+        if (D_802BE310 != 0) {
+            D_802BE310->unk_0C->unk_04 = npc->pos.x;
+            D_802BE310->unk_0C->unk_08 = npc->pos.y + 13.0f;
+            D_802BE310->unk_0C->unk_0C = npc->pos.z;
+        }
+
+        return 0;
+    }
+    return 0;
+}
 
 void func_802BD710_31D280(Npc* npc) {
-    if (D_8010C954 != 0) {
-        D_8010C954 = 0;
+    if (D_8010C954 != NULL) {
+        D_8010C954 = NULL;
         npc->flags = D_802BE274_31DDE4->unk_08;
         D_802BE274_31DDE4->unk_04 = 0;
         partner_clear_player_tracking(npc);
@@ -192,4 +321,48 @@ void func_802BE014_31DB84(void) {
     suggest_player_anim_clearUnkFlag(animationID);
 }
 
-INCLUDE_ASM(s32, "world/partner/watt", func_802BE070_31DBE0);
+void func_802BE070_31DBE0(void) {
+    Npc* partnerNPC;
+    Npc* new_var2;
+    Camera* camera = gCameras;
+    PlayerStatus* playerStatus;
+    f32 temp;
+    f32 temp_f20;
+    f32 spriteFacingAngle;
+    s32 phi_v1;
+    
+    if (gPartnerActionStatus.actionState.b[0] != 0) {
+        spriteFacingAngle = gPlayerStatusPtr->spriteFacingAngle;
+        if ((spriteFacingAngle < 90.0f) || (spriteFacingAngle > 270.0f)) {
+            if (!(gPlayerStatusPtr->trueAnimation & 0x1000000)) {
+                phi_v1 = -5;
+                temp = 0.6f;
+            } else {
+                phi_v1 = 5;
+                temp = 0.6f;
+            }
+        } else {
+            if (!(gPlayerStatusPtr->trueAnimation & 0x1000000)) {
+                phi_v1 = 5;
+                temp = 0.6f;
+            } else {
+                phi_v1 = -5;
+                temp = 0.6f;
+            }
+        }
+
+        temp_f20 = ((((camera->currentYaw + 270.0f) - gPlayerStatusPtr->spriteFacingAngle) + phi_v1) * TAU) / 360.0f;
+
+        playerStatus = gPlayerStatusPtr;
+        partnerNPC = wPartnerNpc;
+        partnerNPC->pos.x = playerStatus->position.x + ((sin_rad(temp_f20) * gPlayerStatusPtr->colliderDiameter) * temp);
+
+        new_var2 = wPartnerNpc;
+        playerStatus = gPlayerStatusPtr;
+        partnerNPC = new_var2;
+        partnerNPC->pos.z = playerStatus->position.z - ((cos_rad(temp_f20) * gPlayerStatusPtr->colliderDiameter) * temp);
+        
+        wPartnerNpc->yaw = gPlayerStatusPtr->targetYaw;
+        wPartnerNpc->pos.y = gPlayerStatusPtr->position.y + 5.0f;
+    }
+}
