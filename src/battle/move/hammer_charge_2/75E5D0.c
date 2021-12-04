26679ce499c042fdedd27b362db4cce23ee6002d
Alexander Faßbender <31699028+Brotenko@users.noreply.github.com>
2021-12-04 15:00:24 +0100

    Cleanup charge moves (#542)

--- src/battle/move/hammer_charge_2/75E5D0.c
+++ src/battle/move/hammer_charge_2/75E5D0.c
@@ -14,7 +14,6 @@ extern s32 D_802A4350;
 ApiStatus func_802A10C8_75E698(Evt* script, s32 isInitialCall) {
     Bytecode* args = script->ptrReadPos;
     BattleStatus* battleStatus = &gBattleStatus;
-    BattleStatus* battleStatus2 = battleStatus; // TODO: macro?
     s32 var1 = evt_get_variable(script, *args++);
     s32 var2 = evt_get_variable(script, *args++);
     s32 var3 = evt_get_variable(script, *args++);
@@ -23,14 +22,14 @@ ApiStatus func_802A10C8_75E698(Evt* script, s32 isInitialCall) {
     playFX_56(2, var1, var2, var3, 1.0f, 60);
     var4 = &D_802A4350;
     *var4 = 0;
-    if (battleStatus2->hammerCharge > 0) {
+    if (battleStatus->hammerCharge > 0) {
         *var4 = 1;
     }
 
-    battleStatus2->hammerCharge += 3;
+    battleStatus->hammerCharge += 3;
 
-    if (battleStatus2->hammerCharge > 99) {
-        (* &battleStatus)->hammerCharge = 99; // TODO: macro?
+    if (battleStatus->hammerCharge > 99) {
+        battleStatus->hammerCharge = 99;
     }
 
     battleStatus->jumpCharge = 0;
