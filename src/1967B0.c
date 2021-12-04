72b2e593f66bfd1a9237fb6d3defcd813fd31e55
JoshDuMan <40190173+JoshDuMan@users.noreply.github.com>
2021-12-03 23:34:49 -0500

    Match func_8026EBF8, LoadFreeItemScript, and LoadItemScript (#543)
    * Match func_8026EBF8
    
    * Delete func_8026EBF8.s
    
    * Match LoadItemScript
    
    * Delete LoadItemScript.s
    
    * Document more of DecorationTable
    
    * Fix warning.
    
    * Comments
    
    * Comments
    
    * Match LoadFreeItemScript
    
    * Delete LoadFreeItemScript.s
    
    * doublke
    
    * Alex GOAT
    
    Co-authored-by: Alex Bates <hi@imalex.xyz>
    
    Co-authored-by: Alex Bates <hi@imalex.xyz>

--- src/1967B0.c
+++ src/1967B0.c
@@ -1,8 +1,6 @@
 #include "common.h"
 
-INCLUDE_ASM(s32, "1967B0", LoadItemScript);
-
-extern s32 D_80293B80;
+extern s32 D_80293B80[];
 
 typedef struct BattleItemTableEntry {
     /* 0x0 */ u8* romStart;
@@ -13,7 +11,95 @@ typedef struct BattleItemTableEntry {
 
 extern BattleItemTableEntry gBattleItemTable[];
 
-INCLUDE_ASM(s32, "1967B0", LoadFreeItemScript);
+ApiStatus LoadItemScript(Evt* script, s32 isInitialCall) {
+    PlayerData* playerData = &gPlayerData;
+    BattleStatus* battleStatus = &gBattleStatus;
+    s16 itemID = battleStatus->selectedItemID;
+    StaticItem* item = &gItemTable[itemID];
+    s32 i = 0;
+    s32* itemPtr;
+
+    while (1) {
+        if (playerData->invItems[i] == itemID) {
+            playerData->invItems[i] = 0;
+            break;
+        }
+        i++;
+    }
+    sort_items();
+
+    if (battleStatus->selectedMoveID == MOVE_DOUBLE_DIP) {
+        if (battleStatus->itemUsesLeft == 2) {
+            deduct_current_move_fp();
+        }
+    }
+
+    if (battleStatus->selectedMoveID == MOVE_TRIPLE_DIP) {
+        if (battleStatus->itemUsesLeft == 3) {
+            deduct_current_move_fp();
+        }
+    }
+    
+    itemPtr = &D_80293B80[0];
+    for (i = 0; *itemPtr != ITEM_NONE; i++, itemPtr++) {
+        if (*itemPtr == battleStatus->selectedItemID) {
+            break;
+        }
+    }
+
+    if (*itemPtr == ITEM_NONE) {
+        if (item->typeFlags & 0x80) {
+            i = 0;
+        } else {
+            i = 1;
+        }
+    }
+
+    dma_copy(gBattleItemTable[i].romStart, gBattleItemTable[i].romEnd, gBattleItemTable[i].vramStart);
+
+    script->varTable[0] = (s32) gBattleItemTable[i].vramEnd;
+    script->varTable[1] = 0;
+
+    return ApiStatus_DONE2;
+}
+
+ApiStatus LoadFreeItemScript(Evt* script, s32 isInitialCall) {
+    BattleStatus* battleStatus = &gBattleStatus;
+    Actor* actor = battleStatus->playerActor;
+    StaticItem* item = &gItemTable[battleStatus->selectedItemID];
+    SelectableTarget* target;
+    s32* itemPtr;
+    s32 i;
+
+    battleStatus->currentTargetListFlags = item->targetFlags | 0x8000;
+    battleStatus->currentAttackElement = 0;
+
+    player_create_target_list(actor);
+    target = &actor->targetData[actor->targetIndexList[0]];
+
+    battleStatus->currentTargetID = target->actorID;
+    battleStatus->currentTargetPart = target->partID;
+
+    itemPtr = &D_80293B80[0];
+    for (i = 0; *itemPtr != ITEM_NONE; i++, itemPtr++) {
+        if (*itemPtr == battleStatus->selectedItemID){
+            break;
+        }
+    }
+
+    if (*itemPtr == ITEM_NONE) {
+        if (item->typeFlags & 0x80) {
+            i = 0;
+        } else {
+            i = 1;
+        }
+    }
+
+    dma_copy(gBattleItemTable[i].romStart, gBattleItemTable[i].romEnd, gBattleItemTable[i].vramStart);
+    script->varTable[0] = (s32) gBattleItemTable[i].vramEnd;
+    script->varTable[1] = 1;
+    return ApiStatus_DONE2;
+}
 
 ApiStatus LoadMoveScript(Evt* script, s32 isInitialCall) {
     BattleStatus* battleStatus = &gBattleStatus;
