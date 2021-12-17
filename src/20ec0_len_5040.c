bb6d206c61b267fbe111f2cf1a278f0dfcba1beb
JoshDuMan <40190173+JoshDuMan@users.noreply.github.com>
2021-12-17 01:34:04 -0500

    Start working matching 20ec0_len_5040.c

--- src/20ec0_len_5040.c
+++ src/20ec0_len_5040.c
@@ -186,58 +186,98 @@ INCLUDE_ASM(s32, "20ec0_len_5040", func_80047820);
 
 INCLUDE_ASM(s32, "20ec0_len_5040", remove_status_debuff);
 
-INCLUDE_ASM(s32, "20ec0_len_5040", enable_status_1);
+void enable_status_1(s32 iconID) {
+    HudStatusIcon* statusIcon = &D_800A0F44[iconID];
+
+    statusIcon->flags |= 0x4;
+}
 
 INCLUDE_ASM(s32, "20ec0_len_5040", func_80047928);
 
 INCLUDE_ASM(s32, "20ec0_len_5040", remove_status_static);
 
-INCLUDE_ASM(s32, "20ec0_len_5040", enable_status_2);
+void enable_status_2(s32 iconID) {
+    HudStatusIcon* statusIcon = &D_800A0F44[iconID];
+
+    statusIcon->flags |= 0x8;
+}
 
 INCLUDE_ASM(s32, "20ec0_len_5040", func_80047A30);
 
 INCLUDE_ASM(s32, "20ec0_len_5040", remove_status_transparent);
 
-INCLUDE_ASM(s32, "20ec0_len_5040", enable_status_3);
+void enable_status_3(s32 iconID) {
+    HudStatusIcon* statusIcon = &D_800A0F44[iconID];
+
+    statusIcon->flags |= 0x10;
+}
 
 INCLUDE_ASM(s32, "20ec0_len_5040", func_80047B38);
 
 INCLUDE_ASM(s32, "20ec0_len_5040", remove_status_4);
 
-INCLUDE_ASM(s32, "20ec0_len_5040", enable_status_4);
+void enable_status_4(s32 iconID) {
+    HudStatusIcon* statusIcon = &D_800A0F44[iconID];
+
+    statusIcon->flags |= 0x20;
+}
 
 INCLUDE_ASM(s32, "20ec0_len_5040", create_status_icon_boost_jump);
 
 INCLUDE_ASM(s32, "20ec0_len_5040", remove_status_icon_boost_jump);
 
-INCLUDE_ASM(s32, "20ec0_len_5040", enable_status_icon_boost_jump);
+void enable_status_icon_boost_jump(s32 iconID) {
+    HudStatusIcon* statusIcon = &D_800A0F44[iconID];
+
+    statusIcon->flags |= 0x40;
+}
 
 INCLUDE_ASM(s32, "20ec0_len_5040", create_status_icon_boost_hammer);
 
 INCLUDE_ASM(s32, "20ec0_len_5040", remove_status_icon_boost_hammer);
 
-INCLUDE_ASM(s32, "20ec0_len_5040", enable_status_icon_boost_hammer);
+void enable_status_icon_boost_hammer(s32 iconID) {
+    HudStatusIcon* statusIcon = &D_800A0F44[iconID];
+
+    statusIcon->flags |= 0x80;
+}
 
 INCLUDE_ASM(s32, "20ec0_len_5040", create_status_icon_boost_partner);
 
 INCLUDE_ASM(s32, "20ec0_len_5040", remove_status_icon_boost_partner);
 
-INCLUDE_ASM(s32, "20ec0_len_5040", enable_status_icon_boost_partner);
+void enable_status_icon_boost_partner(s32 iconID) {
+    HudStatusIcon* statusIcon = &D_800A0F44[iconID];
+
+    statusIcon->flags |= 0x100;
+}
 
 INCLUDE_ASM(s32, "20ec0_len_5040", create_status_icon_surprise);
 
 INCLUDE_ASM(s32, "20ec0_len_5040", remove_status_icon_surprise);
 
-INCLUDE_ASM(s32, "20ec0_len_5040", enable_status_icon_surprise);
+void enable_status_icon_surprise(s32 iconID) {
+    HudStatusIcon* statusIcon = &D_800A0F44[iconID];
+
+    statusIcon->flags |= 0x200;
+}
 
 INCLUDE_ASM(s32, "20ec0_len_5040", create_status_icon_peril);
 
 INCLUDE_ASM(s32, "20ec0_len_5040", remove_status_icon_peril);
 
-INCLUDE_ASM(s32, "20ec0_len_5040", enable_status_icon_peril);
+void enable_status_icon_peril(s32 iconID) {
+    HudStatusIcon* statusIcon = &D_800A0F44[iconID];
+
+    statusIcon->flags |= 0x400;
+}
 
 INCLUDE_ASM(s32, "20ec0_len_5040", create_status_icon_danger);
 
 INCLUDE_ASM(s32, "20ec0_len_5040", remove_status_icon_danger);
 
-INCLUDE_ASM(s32, "20ec0_len_5040", enable_status_icon_danger);
+void enable_status_icon_danger(s32 iconID) {
+    HudStatusIcon* statusIcon = &D_800A0F44[iconID];
+
+    statusIcon->flags |= 0x800;
+}
