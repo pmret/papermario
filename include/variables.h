2b6323a6b28a32eeeaba82003bae75c530d7df91
JoshDuMan <40190173+JoshDuMan@users.noreply.github.com>
2021-12-17 23:04:54 -0500

    Match check_for_interactables (#553)
    * Match check_for_interactables
    
    * Delete check_for_interactables.s
    
    * Update enums.h
    
    * unk_C8 -> Npc*
    
    * Update 77480.c
    
    * Update 77480.c
    
    * Update 77480.c
    
    * Update 77480.c
    
    * Update 77480.c (#7)
    
    * Update functions.h
    
    * Merge branch 'master' of https://github.com/pmret/papermario into arn
    
    * Fix build.
    
    * Update src/77480.c
    
    Co-authored-by: Alex Bates <hi@imalex.xyz>
    
    * Comments
    
    Co-authored-by: Alex Bates <hi@imalex.xyz>

--- include/variables.h
+++ include/variables.h
@@ -282,7 +282,7 @@ extern s32 D_80104BEC;
 extern UNK_FUN_PTR(D_8010C920);
 extern s32 D_8010C940;
 extern Entity* D_8010C954;
-extern s32 D_8010C958;
+extern UNK_FUN_PTR(D_8010C958);
 extern s32 D_8010C95C;
 extern s32 D_8010C96C;
 extern s32 D_8010C980;
