2f23f46775044c09d770e713818169a4f1b0eeb5
JoshDuMan <40190173+JoshDuMan@users.noreply.github.com>
2021-12-26 06:29:29 -0500

    Match func_802549F4 (#571)
    * Match func_802549F4.
    
    * Delete func_802549F4.s
    
    * Match func_80254C50
    
    * s8
    
    * Delete func_80254C50.s
    
    * Update functions.h
    
    * s8 again
    
    * Trying out github.dev
    
    * Fix build?
    
    * Match func_802550BC

--- include/functions.h
+++ include/functions.h
@@ -89,7 +89,7 @@ void func_80257B48(s32);
 void func_8025599C(s32);
 void func_80257B88(void);
 void func_80257DA4(void);
-void func_80254C50(void);
+void func_80254C50(Actor*);
 void func_80258E14(void);
 
 f32 func_800E34D8(void);
@@ -97,6 +97,7 @@ void func_800E4AD8(s32 arg0);
 f32 player_check_collision_below(f32, s32* colliderID);
 s32 can_trigger_loading_zone(void);
 void func_802667F0(s32, Actor*, f32, f32, f32);
+void func_802591EC(s32, ActorPart*, s32, Matrix4f*, s32);
 
 void* general_heap_malloc(s32 size);
 s32 general_heap_free(void* data);
