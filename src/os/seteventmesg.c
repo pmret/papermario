1536220e3d5b282c909015bad085040727e11c2e
JoshDuMan <Joshua.Shoup.1996@gmail.com>
2021-12-29 10:42:56 -0500

    fix

--- src/os/seteventmesg.c
+++ src/os/seteventmesg.c
@@ -1,4 +1,4 @@
 #include "common.h"
 #include <PR/osint.h>
 
-INCLUDE_ASM(void, "os/40c20_len_1f0", osSetEventMesg, OSEvent event, OSMesgQueue* queue, OSMesg mesg);
+INCLUDE_ASM(void, "os/seteventmesg", osSetEventMesg, OSEvent event, OSMesgQueue* queue, OSMesg mesg);
