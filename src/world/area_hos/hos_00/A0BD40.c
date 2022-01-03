25bdf5f2479e02a2a5fea4d17af5e868f66d1b33
Ethan Roseman <ethteck@gmail.com>
2022-01-03 23:47:36 +0900

    UnkEffect0FFuncs

--- src/world/area_hos/hos_00/A0BD40.c
+++ src/world/area_hos/hos_00/A0BD40.c
@@ -1,13 +1,7 @@
 #include "hos_00.h"
 #include "effects.h"
 
-#include "common/UnkEffect0FFunc2.inc.c"
-
-#include "common/UnkEffect0FFunc.inc.c"
-
-INCLUDE_ASM(s32, "world/area_hos/hos_00/A0BD40", func_8024036C_A0C05C);
-
-INCLUDE_ASM(s32, "world/area_hos/hos_00/A0BD40", func_80240584_A0C274);
+#include "common/UnkEffect0FFuncs.inc.c"
 
 static char* N(exit_str_0) = "osr_00";
 static char* N(exit_str_1) = "osr_01";
