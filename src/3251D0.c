f459012f31e5086ad97e7a8e0779b6d766a666cc
JoshDuMan <joshua.shoup.1996@gmail.com>
2022-01-08 05:25:43 -0500

    Pogress

--- src/3251D0.c
+++ src/3251D0.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "effects.h"
+#include "script_api/battle.h"
 
 ApiStatus func_802BD100_3251D0(Evt* script, s32 isInitialCall) {
     Bytecode* args = script->ptrReadPos;
@@ -104,3 +105,105 @@ ApiStatus func_802BD4E8(Evt* script, s32 isInitialCall) {
     remove_consumable();
     return ApiStatus_DONE2;
 }
+
+extern EvtSource D_802C0000;
+
+EvtSource D_802BD510_3255E0 = {
+    EVT_SET_GROUP(0)
+    EVT_CALL(SetPlayerAnimation, ANIM_GOT_ITEM)
+    EVT_CALL(GetPlayerPos, LW(0), LW(1), LW(2))
+    EVT_ADD(LW(1), 45)
+    EVT_CALL(MakeItemEntity, LW(10), LW(0), LW(1), LW(2), 1, 0)
+    EVT_SET(LW(10), LW(0))
+    EVT_WAIT_FRAMES(15)
+    EVT_CALL(GetPlayerPos, LW(0), LW(1), LW(2))
+    EVT_ADD(LW(1), 20)
+    EVT_CALL(D_802C0000, LW(0), LW(1), LW(2))
+    EVT_CALL(RemoveItemEntity, LW(10))
+    EVT_RETURN
+    EVT_END
+};
+
+EvtSource D_802BD5F4 = {
+    EVT_THREAD
+        EVT_LOOP(4)
+            EVT_CALL(PlaySound, SOUND_2095)
+            EVT_WAIT_FRAMES(10)
+        EVT_END_LOOP
+    EVT_END_THREAD
+    EVT_CALL(SetPlayerAnimation, ANIM_EAT)
+    EVT_WAIT_FRAMES(45)
+    EVT_RETURN
+    EVT_END
+};
+
+EvtSource D_802BD660 = {
+    EVT_THREAD
+        EVT_LOOP(4)
+            EVT_CALL(PlaySound, SOUND_2095)
+            EVT_WAIT_FRAMES(10)
+        EVT_END_LOOP
+    EVT_END_THREAD
+    EVT_CALL(SetPlayerAnimation, ANIM_DRINK)
+    EVT_WAIT_FRAMES(45)
+    EVT_RETURN
+    EVT_END
+};
+
+EvtSource D_802BD6CC = {
+    EVT_SET_GROUP(0)
+    EVT_CALL(DisablePlayerInput, TRUE)
+    EVT_CALL(GetCurrentPartner, LW(0))
+    EVT_IF_NE(LW(0), 0)
+        EVT_CALL(func_802D2B6C)
+        EVT_WAIT_FRAMES(20)
+    EVT_END_IF
+    EVT_CALL(0x802C02E4)
+    EVT_CALL(0x802C0358, LW(10))
+    EVT_EXEC_WAIT(0x802C0410)
+    EVT_IF_EQ(LW(13), 0)
+        EVT_EXEC_WAIT(0x802C04F4)
+    EVT_ELSE
+        EVT_EXEC_WAIT(0x802C0560)
+    EVT_END_IF
+    EVT_IF_EQ(LW(15), 1)
+        EVT_CALL(SetPlayerAnimation, ANIM_STICK_OUT_TONGUE)
+    EVT_END_IF
+    EVT_IF_GT(LW(11), 0)
+        EVT_CALL(GetPlayerPos, LW(0), LW(1), LW(2))
+        EVT_ADD(LW(0), 0)
+        EVT_ADD(LW(1), 35)
+        EVT_CALL(0x802C00AC, LW(0), LW(1), LW(2), LW(11))
+    EVT_END_IF
+    EVT_IF_GT(LW(12), 0)
+        EVT_CALL(GetPlayerPos, LW(0), LW(1), LW(2))
+        EVT_ADD(LW(0), 20)
+        EVT_ADD(LW(1), 25)
+        EVT_CALL(0x802C016C, LW(0), LW(1), LW(2), LW(12))
+    EVT_END_IF
+    EVT_CALL(GetPlayerPos, LW(0), LW(1), LW(2))
+    EVT_ADD(LW(1), 25)
+    EVT_CALL(func_802D7520, LW(0), LW(1), LW(2), LW(11))
+    EVT_IF_NE(LW(11), 0)
+        EVT_CALL(0x802C022C, LW(11))
+    EVT_END_IF
+    EVT_IF_NE(LW(12), 0)
+        EVT_CALL(0x802C0288, LW(12))
+    EVT_END_IF
+    EVT_IF_EQ(LW(15), 0)
+        EVT_WAIT_FRAMES(10)
+        EVT_CALL(SetPlayerAnimation, ANIM_THUMBS_UP)
+        EVT_WAIT_FRAMES(30)
+    EVT_ELSE
+        EVT_WAIT_FRAMES(30)
+    EVT_END_IF
+    EVT_CALL(GetPlayerPos, LW(0), LW(1), LW(2))
+    EVT_CALL(func_802D75D8, LW(0), LW(1), LW(2), LW(11))
+    EVT_CALL(SetPlayerAnimation, ANIM_10002)
+    EVT_WAIT_FRAMES(20)
+    EVT_CALL(0x802C03E8)
+    EVT_CALL(DisablePlayerInput, FALSE)
+    EVT_CALL(0x802C031C)
+    EVT_RETURN
+    EVT_END
+};
