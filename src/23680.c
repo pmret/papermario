c287477355042a79247425ea49552b2000522cbc
JoshDuMan <40190173+JoshDuMan@users.noreply.github.com>
2022-01-13 23:59:00 -0500

    Match DoBasicAI

--- src/23680.c
+++ src/23680.c
@@ -285,4 +285,84 @@ void func_8004A3E8(Evt* script, NpcAISettings* npcAISettings, EnemyTerritoryThin
     }
 }
 
-INCLUDE_ASM(s32, "23680", DoBasicAI, Evt* script, s32 isInitialCall);
+ApiStatus DoBasicAI(Evt* script, s32 isInitialCall) {
+    Enemy* enemy = script->owner1.enemy;
+    Npc* npc = get_npc_unsafe(enemy->npcID);
+    Bytecode* args = script->ptrReadPos;
+    EnemyTerritoryThing territory;
+    EnemyTerritoryThing* pTerritory = &territory;
+    NpcAISettings* aiSettings = (NpcAISettings*) evt_get_variable(script, *args++);
+
+    
+    territory.unk_00 = 0;
+    territory.shape = enemy->territory->wander.detectShape;
+    territory.pointX = enemy->territory->wander.detect.x;
+    territory.pointZ = enemy->territory->wander.detect.z;
+    territory.sizeX = enemy->territory->wander.detectSizeX;
+    territory.sizeZ = enemy->territory->wander.detectSizeZ;
+    territory.unk_18 = 65.0f;
+    territory.unk_1C = 0;
+
+
+    if (isInitialCall || enemy->unk_B0 & 4) {
+        script->functionTemp[0] = 0;
+        npc->duration = 0;
+
+        npc->currentAnim.w = enemy->animList[0];
+
+        npc->flags &= ~NPC_FLAG_NO_Y_MOVEMENT;
+        if (!enemy->territory->wander.isFlying) {
+            npc->flags |= NPC_FLAG_GRAVITY;
+            npc->flags &= ~NPC_FLAG_ENABLE_HIT_SCRIPT;
+        } else {
+            npc->flags &= ~NPC_FLAG_GRAVITY;
+            npc->flags |= NPC_FLAG_ENABLE_HIT_SCRIPT;
+        }
+
+        if (enemy->unk_B0 & 4) {
+            script->functionTemp[0] = 99;
+            script->functionTemp[1] = 0;
+        } else if (enemy->flags & ENEMY_FLAGS_40000000) {
+            script->functionTemp[0] = 12;
+        }
+
+        enemy->unk_B0 &= ~4;
+        enemy->flags &= ~ENEMY_FLAGS_40000000;
+    }
+
+    switch (script->functionTemp[0]) {
+        case 0x0:
+            func_800495A0(script, aiSettings, pTerritory);
+            /* fallthrough */
+        case 0x1:
+            func_800496B8(script, aiSettings, pTerritory);
+            break;
+        case 0x2:
+            base_UnkNpcAIFunc1(script, aiSettings, pTerritory);
+            /* fallthrough */
+        case 0x3:
+            func_80049C04(script, aiSettings, pTerritory);
+            break;
+        case 0xA:
+            func_80049E3C(script, aiSettings, pTerritory);
+            /* fallthrough */
+        case 0xB:
+            func_80049ECC(script, aiSettings, pTerritory);
+            break;
+        case 0xC:
+            func_80049F7C(script, aiSettings, pTerritory);
+            /* fallthrough */
+        case 0xD:
+            func_8004A124(script, aiSettings, pTerritory);
+            if (script->functionTemp[0] != 0xE) {
+                break;
+            }
+        case 0xE:
+            func_8004A3E8(script, aiSettings, pTerritory);
+            break;
+        case 0x63:
+            func_8004A73C(script);
+            break;
+    }
+    return ApiStatus_BLOCK;
+}
