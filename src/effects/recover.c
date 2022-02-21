fb828bf8dac308fdeedfe9acd444c4f1ac669166
JaThePlayer <50085307+JaThePlayer@users.noreply.github.com>
2022-02-21 16:53:26 +0100

    music_note.c, shape_spell.c, stat_change.c, tattle_window.c, recover.c data migration (#652)
    * music_note.c, shape_spell.c, stat_change.c, tattle_window.c, recover.c data migration
    
    * implement requested changes
    
    * Recover.c data fixes, style changes
    
    * The one newline lol
    
    * f

--- src/effects/recover.c
+++ src/effects/recover.c
@@ -1,6 +1,27 @@
 #include "common.h"
 #include "effects_internal.h"
 
+void func_E0080448(EffectInstance* effect);
+
+extern Gfx D_09003540[];
+extern Gfx D_09003580[];
+extern Gfx D_090035C0[];
+extern Gfx D_09003600[];
+extern Gfx D_09003640[];
+extern Gfx D_09003680[];
+extern Gfx D_090036C0[];
+extern Gfx D_09003700[];
+extern Gfx D_09003740[];
+extern Gfx D_09003780[];
+extern Gfx D_090037C0[];
+extern Gfx D_09003800[];
+
+Gfx* D_E0080AC0[] = { D_09003540, D_09003580, D_090035C0, D_09003600, D_09003640, D_09003680, D_090036C0, D_09003700, D_09003740, D_09003780, D_090037C0, D_09003800, };
+
+f32 D_E0080AF0[] = { 0.25f, 0.5f, 0.75f, 1.0f, 0.9f, 1.1f, 1.0f };
+
+f32 D_E0080B0C[] = { 0.25f, 0.5f, 0.75f, 1.0f, 1.2f, 0.9f, 1.0f };
+
 INCLUDE_ASM(s32, "effects/recover", recover_main);
 
 void recover_init(void) {
@@ -11,6 +32,8 @@ INCLUDE_ASM(s32, "effects/recover", recover_update);
 void recover_render(void) {
 }
 
-INCLUDE_ASM(s32, "effects/recover", func_E008042C);
+void func_E008042C(EffectInstance* effect) {
+    func_E0080448(effect);
+}
 
 INCLUDE_ASM(s32, "effects/recover", func_E0080448);
--- src/effects/tattle_window.c
+++ src/effects/tattle_window.c
@@ -1,17 +1,154 @@
 #include "common.h"
 #include "effects_internal.h"
 
-INCLUDE_ASM(s32, "effects/tattle_window", tattle_window_main);
+void tattle_window_init(EffectInstance* effect);
+void tattle_window_update(EffectInstance* effect);
+void tattle_window_render(EffectInstance* effect);
+void func_E00D8264(EffectInstance* effect);
+void func_E00D8630(EffectInstance* effect);
 
-void tattle_window_init(void) {
+typedef struct TattleWindowFXData {
+    /* 0x00 */ s32 unk_00;
+    /* 0x04 */ Vec3f pos;
+    /* 0x10 */ s32 unk_10;
+    /* 0x14 */ s32 unk_14;
+    /* 0x18 */ f32 unk_18;
+    /* 0x1C */ f32 unk_1C;
+    /* 0x20 */ s32 unk_20;
+    /* 0x24 */ s32 unk_24;
+    /* 0x28 */ f32 unk_28;
+    /* 0x2C */ f32 unk_2C;
+    /* 0x30 */ f32 unk_30;
+    /* 0x34 */ u8 unk_34;
+    /* 0x35 */ u8 unk_35;
+} TattleWindowFXData; // size = 0x38
+
+typedef struct D_E00D8818_Entry {
+    s16 unk_00;
+    u8 unk_02;
+    u8 unk_03;
+} D_E00D8818_Entry; // size = 0x4
+
+s32 D_E00D87E0[] = { 0x00000000, 0x00000000, 0x00000000, 0x09000000, 0x31100810, 0x08100810, 0x08000000, 0x00000000, 0xFC317FFF, 0x5FFEF438, 0xFC3135FF, 0x5FFEFE38, 0x3D4ABCFF, 0x00191CFF };
+
+D_E00D8818_Entry D_E00D8818[] = {
+    { .unk_00 = 0xFF38, .unk_02 = 0xFF, .unk_03 = 0xFF },
+    { .unk_00 = 0xFF60, .unk_02 = 0xFF, .unk_03 = 0xFF },
+    { .unk_00 = 0xFF88, .unk_02 = 0xFF, .unk_03 = 0xFF },
+    { .unk_00 = 0xFFAB, .unk_02 = 0xFF, .unk_03 = 0xFF },
+    { .unk_00 = 0xFFC9, .unk_02 = 0xFF, .unk_03 = 0xFF },
+    { .unk_00 = 0xFFE2, .unk_02 = 0xFF, .unk_03 = 0xFF },
+    { .unk_00 = 0xFFF0, .unk_02 = 0xFF, .unk_03 = 0xFF },
+    { .unk_00 = 0xFFF8, .unk_02 = 0xFF, .unk_03 = 0xFF },
+    { .unk_00 = 0xFFFC, .unk_02 = 0xFF, .unk_03 = 0xFF },
+    { .unk_00 = 0xFFFE, .unk_02 = 0xFF, .unk_03 = 0xFF },
+    { .unk_00 = 0xFFFF, .unk_02 = 0xFF, .unk_03 = 0xFF },
+    { .unk_00 = 0x0000, .unk_02 = 0xFF, .unk_03 = 0xFF },
+    { .unk_00 = 0x0000, .unk_02 = 0xFF, .unk_03 = 0xFF },
+    { .unk_00 = 0x0000, .unk_02 = 0xFF, .unk_03 = 0xFF },
+    { .unk_00 = 0x0000, .unk_02 = 0xFF, .unk_03 = 0xFF },
+    { .unk_00 = 0x0000, .unk_02 = 0xE6, .unk_03 = 0xFA },
+    { .unk_00 = 0x0000, .unk_02 = 0x78, .unk_03 = 0xF5 },
+    { .unk_00 = 0x0000, .unk_02 = 0x00, .unk_03 = 0xF0 },
+    { .unk_00 = 0x0000, .unk_02 = 0x00, .unk_03 = 0xD2 },
+    { .unk_00 = 0x0000, .unk_02 = 0x00, .unk_03 = 0xAA },
+    { .unk_00 = 0x0000, .unk_02 = 0x00, .unk_03 = 0x78 },
+    { .unk_00 = 0x0000, .unk_02 = 0x00, .unk_03 = 0x3C },
+};
+
+EffectInstance* tattle_window_main(s32 arg0, f32 x, f32 y, f32 z, f32 arg4, s32 arg5) {
+    EffectBlueprint bp;
+    EffectBlueprint* bpPtr = &bp;
+    EffectInstance* effect;
+    TattleWindowFXData* part;
+    s32 numParts = 1;
+
+    bp.init = tattle_window_init;
+    bp.update = tattle_window_update;
+    bp.renderWorld = tattle_window_render;
+    bp.unk_14 = func_E00D8264;
+    bp.unk_00 = 0;
+    bp.effectID = EFFECT_TATTLE_WINDOW;
+
+    effect = shim_create_effect_instance(bpPtr);
+    effect->numParts = numParts;
+    part = effect->data = shim_general_heap_malloc(numParts * sizeof(*part));
+
+    ASSERT(effect->data != NULL);
+
+    part->unk_00 = arg0;
+    part->unk_14 = 0;
+    if (arg5 <= 0) {
+        part->unk_10 = 0x3E8;
+    } else {
+        part->unk_10 = arg5;
+    }
+    part->pos.x = x;
+    part->pos.y = y;
+    part->pos.z = z;
+    part->unk_1C = 0;
+    part->unk_18 = arg4;
+    part->unk_28 = 0.0f;
+    part->unk_2C = part->unk_30 = part->unk_28;
+    part->unk_20 = 0;
+    part->unk_24 = 0;
+    part->unk_34 = 0xFF;
+    part->unk_35 = 0xFF;
+
+    return effect;
 }
 
-INCLUDE_ASM(s32, "effects/tattle_window", tattle_window_update);
-
-void tattle_window_render(void) {
+void tattle_window_init(EffectInstance* effect) {
 }
 
-INCLUDE_ASM(s32, "effects/tattle_window", func_E00D8264);
+void tattle_window_update(EffectInstance* effect) {
+    s32 unk_10, unk_10_2;
+    s32 old_unk_14;
+    TattleWindowFXData* part = effect->data;
+
+    if (effect->flags & 0x10) {
+        effect->flags &= ~0x10;
+        part->unk_10 = 23;
+    }
+
+    if (part->unk_10 < 1000) {
+        part->unk_10--;
+    }
+
+    old_unk_14 = part->unk_14++;
+    unk_10 = part->unk_10;
+
+    if (unk_10 < 0) {
+        shim_remove_effect(effect);
+        return;
+    }
+    unk_10_2 = unk_10;
+    part->unk_28 = 0;
+    part->unk_2C = 0;
+    part->unk_1C = part->unk_18;
+    if (old_unk_14 < 23) {
+        part->unk_30 = D_E00D8818[old_unk_14].unk_00;
+        part->unk_34 = D_E00D8818[old_unk_14].unk_02;
+        part->unk_35 = D_E00D8818[old_unk_14].unk_03;
+    } else {
+        part->unk_30 = D_E00D8818[0x16].unk_00;
+        part->unk_34 = D_E00D8818[0x16].unk_02;
+        part->unk_35 = D_E00D8818[0x16].unk_03;
+        part->unk_14 = 0x18;
+    }
+    if (unk_10_2 < 23) {
+        part->unk_30 = D_E00D8818[unk_10_2].unk_00;
+        part->unk_34 = D_E00D8818[unk_10_2].unk_02;
+        part->unk_35 = D_E00D8818[unk_10_2].unk_03;
+    }
+}
+
+void tattle_window_render(EffectInstance* effect) {
+}
+
+void func_E00D8264(EffectInstance* effect) {
+    func_E00D8630(effect);
+}
 
 void func_E00D8280(void) {
 }
