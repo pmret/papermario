b878794389263a6ee345852d0bf07712db94c4b2
Nasina7 <nasina987@gmail.com>
2022-03-16 17:13:03 -0400

    Decompiled and Matched unknown_move

--- src/battle/move/unknown_move/779C90.c
+++ src/battle/move/unknown_move/779C90.c
@@ -1,7 +1,34 @@
 #include "common.h"
 
+#include "battle/battle.h"
+
 #define NAMESPACE battle_move_unknown_move
 
 #include "world/common/UnkMoveFunc1.inc.c"
 
-INCLUDE_ASM(s32, "battle/move/unknown_move/779C90", func_802A10E4_779D74);
+extern s32 D_802A26D0_77B360;
+
+ApiStatus func_802A10E4_779D74(Evt* script, s32 isInitialCall) {
+    Actor *enemyTargetActor;
+    u32 enemyTargetPowerBounceChance;
+
+    enemyTargetActor = get_actor((s32) get_actor(script->owner1.enemyID)->targetActorID);
+
+    script->varTable[0] = 99;
+
+    enemyTargetPowerBounceChance = enemyTargetActor->actorBlueprint->powerBounceChance;
+
+    if (is_ability_active(0) != 0) {
+        enemyTargetPowerBounceChance += 7;
+    }
+
+    D_802A26D0_77B360 *= enemyTargetPowerBounceChance;
+    D_802A26D0_77B360 /= 100;
+
+    if (D_802A26D0_77B360 < rand_int(100)) {
+        script->varTable[0] = 0;
+    }
+
+    return ApiStatus_DONE2;
+}
+
