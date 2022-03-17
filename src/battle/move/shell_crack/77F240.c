83717471f9020f84932c7ab80c879ffad2f21f81
Nasina <nasina987@gmail.com>
2022-03-16 23:05:16 -0400

    Decompiled and Matched func_802A10A4_77F2E4 (#662)
    * Decompiled func_802A10A4_77F2E4
    
    * Resolved Suggestions
    
    * Fixed Formatting Error
    
    * Fixed Formatting Errors
    
    * oops
    
    * Move playerActorState Assignment

--- src/battle/move/shell_crack/77F240.c
+++ src/battle/move/shell_crack/77F240.c
@@ -1,9 +1,23 @@
 #include "common.h"
 
+#include "effects.h"
+
 #define NAMESPACE battle_move_shell_crack
 
 #include "world/common/IsBerserkerEquipped.inc.c"
 
 #include "world/common/IsRightOnEquipped.inc.c"
 
-INCLUDE_ASM(s32, "battle/move/shell_crack/77F240", func_802A10A4_77F2E4);
+ApiStatus func_802A10A4_77F2E4(Evt* script, s32 isInitialCall) {
+    ActorState* playerActorState = &gBattleStatus.playerActor->state;
+    s32 i;
+
+    for (i = 0; i < 20; i++) {
+        fx_floating_cloud_puff(0, 
+                               (playerActorState->goalPos.x + (f32) rand_int(30)) - 15.0f, 
+                               (playerActorState->goalPos.y + (f32) rand_int(20)) - 15.0f, 
+                               playerActorState->goalPos.z + 5.0f, 1.0f, 25);   
+    }
+
+    return ApiStatus_DONE2;
+}
