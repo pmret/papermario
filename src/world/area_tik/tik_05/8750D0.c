e7e92e860c74fa7412d7ef26ff7722b375aa15c2
Ethan Roseman <ethteck@gmail.com>
2022-04-22 01:04:37 +0900

    match

--- src/world/area_tik/tik_05/8750D0.c
+++ src/world/area_tik/tik_05/8750D0.c
@@ -13,12 +13,10 @@
 
 #include "world/common/CheckItemFlags40.inc.c"
 
-#ifdef NON_MATCHING
-
 extern s32 D_000001E4;
 
 ApiStatus func_8024093C_8756FC(Evt* script, s32 isInitialCall) {
-    Bytecode* args = script->ptrReadPosition;
+    Bytecode* args = script->ptrReadPos;
     Entity* entity = get_entity_by_index(evt_get_variable(script, *args++));
 
     if (entity == NULL) {
@@ -28,8 +26,5 @@ ApiStatus func_8024093C_8756FC(Evt* script, s32 isInitialCall) {
     play_model_animation(entity->virtualModelIndex, &D_000001E4);
     return ApiStatus_DONE2;
 }
-#else
-INCLUDE_ASM(s32, "world/area_tik/tik_05/8750D0", func_8024093C_8756FC);
-#endif
 
 #include "world/common/SetEntityPosition.inc.c"
