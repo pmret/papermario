87d4084947784d6387b6879c4bc102b756c2180f
HailSanta <Hail2Santa@gmail.com>
2022-04-22 01:22:15 -0400

    resolved some bad dead syms

--- src/EA0C10.c
+++ src/EA0C10.c
@@ -21,44 +21,7 @@ void func_80240DC4_EA16C4(Evt* script, NpcAISettings* aiSettings, EnemyTerritory
 
 #include "world/common/enemy/MeleeHitbox_33.inc.c"
 
-//Basically MeleeHitbox_CanSeePlayer.inc.c, but gPartnerActionStatus.partnerActionState access 0, instead of 3
-s32 N(MeleeHitbox_CanSeePlayer)(Evt* script) {
-    Enemy* enemy = script->owner1.enemy;
-    Npc* npc = get_npc_unsafe(enemy->npcID);
-    Camera* camera = &gCameras[gCurrentCamID];
-    Enemy* enemy2 = get_enemy(enemy->npcID + 1);
-    f32 phi_f20;
-    s32 ret = TRUE;
-
-    if (dist2D(npc->pos.x, npc->pos.z, gPlayerStatusPtr->position.x, gPlayerStatusPtr->position.z) > enemy2->varTable[2]) {
-        ret = FALSE;
-    }
-
-    if (clamp_angle(get_clamped_angle_diff(camera->currentYaw, npc->yaw)) < 180.0) {
-        phi_f20 = 90.0f;
-    } else {
-        phi_f20 = 270.0f;
-    }
-
-    if (fabsf(get_clamped_angle_diff(phi_f20, atan2(npc->pos.x, npc->pos.z, gPlayerStatusPtr->position.x,
-                                     gPlayerStatusPtr->position.z))) > enemy2->varTable[3]) {
-        ret = FALSE;
-    }
-
-    if ((2.0 * npc->collisionHeight) <= fabsf(npc->pos.y - gPlayerStatusPtr->position.y)) {
-        ret = FALSE;
-    }
-
-    if (gPartnerActionStatus.partnerActionState == PARTNER_BOW) {
-        ret = FALSE;
-    }
-
-    if (gPartnerActionStatus.partnerActionState == PARTNER_SUSHIE) {
-        ret = FALSE;
-    }
-
-    return ret;
-}
+#include "world/common/enemy/MeleeHitbox_CanSeePlayer.inc.c"
 
 // Similar to MeleeHitbox_Control
 ApiStatus func_802406CC_EA0FCC(Evt* script, s32 isInitialCall) {
