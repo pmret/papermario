002fcdb9f25031cd695d3fdb91c102251f1d9786
Kevin Klions <kklions1@gmail.com>
2022-04-24 13:46:32 -0400

    deleted commented out INCLUDE_ASM call

--- src/world/area_kkj/kkj_12/AD4100.c
+++ src/world/area_kkj/kkj_12/AD4100.c
@@ -8,8 +8,6 @@ INCLUDE_ASM(s32, "world/area_kkj/kkj_12/AD4100", func_80240714_AD47A4);
 
 #include "world/common/UnkGooberFunc.inc.c"
 
-// INCLUDE_ASM(s32, "world/area_kkj/kkj_12/AD4100", func_8024089C_AD492C);
-
 ApiStatus func_8024089C_AD492C() {
     D_80151328->sources[1].flags = 0;
     return ApiStatus_DONE2;
