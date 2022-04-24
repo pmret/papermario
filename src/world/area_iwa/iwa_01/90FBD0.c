70bd83fba501ddff840d99c6875b027100a80333
HailSanta <Hail2Santa@gmail.com>
2022-04-24 18:52:18 -0400

    iwa_01

--- src/world/area_iwa/iwa_01/90FBD0.c
+++ src/world/area_iwa/iwa_01/90FBD0.c
@@ -22,36 +22,4 @@
 
 #include "world/common/GetEncounterEnemyIsOwner.inc.c"
 
-INCLUDE_ASM(s32, "world/area_iwa/iwa_01/90FBD0", func_802422C4_911974);
-
-#include "world/common/enemy/state/CleftAI_00.inc.c"
-
-INCLUDE_ASM(s32, "world/area_iwa/iwa_01/90FBD0", func_80242498_911B48);
-
-#include "world/common/enemy/state/CleftAI_02.inc.c"
-
-#include "world/common/enemy/state/CleftAI_03.inc.c"
-
-#include "world/common/enemy/state/CleftAI_04.inc.c"
-
-#include "world/common/enemy/state/CleftAI_05.inc.c"
-
-#include "world/common/enemy/state/CleftAI_20.inc.c"
-
-INCLUDE_ASM(s32, "world/area_iwa/iwa_01/90FBD0", func_80242958_912008);
-
-#include "world/common/enemy/state/CleftAI_22.inc.c"
-
-INCLUDE_ASM(s32, "world/area_iwa/iwa_01/90FBD0", func_80242C48_9122F8);
-
-#include "world/common/enemy/state/CleftAI_41.inc.c"
-
-#include "world/common/enemy/state/CleftAI_50.inc.c"
-
-#include "world/common/enemy/state/CleftAI_51.inc.c"
-
-#define NAMESPACE dup_iwa_01
-#include "world/common/enemy/state/CleftAI_52.inc.c"
-#define NAMESPACE iwa_01
-
-INCLUDE_ASM(s32, "world/area_iwa/iwa_01/90FBD0", func_80242F54_912604);
+#include "world/common/enemy/CleftAI.inc.c"
