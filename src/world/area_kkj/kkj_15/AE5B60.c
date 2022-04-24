760c081d8378541e724ea22082525db6bb3dae3e
Kevin Klions <kklions1@gmail.com>
2022-04-24 13:38:52 -0400

    matched func_802405EC_AE5B7C

--- src/world/area_kkj/kkj_15/AE5B60.c
+++ src/world/area_kkj/kkj_15/AE5B60.c
@@ -2,4 +2,11 @@
 
 INCLUDE_ASM(s32, "world/area_kkj/kkj_15/AE5B60", func_802405D0_AE5B60);
 
-INCLUDE_ASM(s32, "world/area_kkj/kkj_15/AE5B60", func_802405EC_AE5B7C);
+// INCLUDE_ASM(s32, "world/area_kkj/kkj_15/AE5B60", func_802405EC_AE5B7C);
+
+ApiStatus func_802405EC_AE5B7C() {
+    GameStatus *temp_v1 = gGameStatusPtr;
+
+    temp_v1->peachFlags &= 0xEF;
+    return ApiStatus_DONE2;
+}
