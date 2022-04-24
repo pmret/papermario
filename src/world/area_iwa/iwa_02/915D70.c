a619827f2ad245dc0719f15e6b15607663e10a83
HailSanta <Hail2Santa@gmail.com>
2022-04-24 18:57:25 -0400

    iwa_02

--- src/world/area_iwa/iwa_02/915D70.c
+++ src/world/area_iwa/iwa_02/915D70.c
@@ -3,37 +3,7 @@
 static char* N(exit_str_0) = "iwa_01";
 static char* N(exit_str_1) = "iwa_04";
 
-INCLUDE_ASM(s32, "world/area_iwa/iwa_02/915D70", func_80240000_915D70);
-
-#include "world/common/enemy/state/CleftAI_00.inc.c"
-
-INCLUDE_ASM(s32, "world/area_iwa/iwa_02/915D70", func_802401D4_915F44);
-
-#include "world/common/enemy/state/CleftAI_02.inc.c"
-
-#include "world/common/enemy/state/CleftAI_03.inc.c"
-
-#include "world/common/enemy/state/CleftAI_04.inc.c"
-
-#include "world/common/enemy/state/CleftAI_05.inc.c"
-
-#include "world/common/enemy/state/CleftAI_20.inc.c"
-
-INCLUDE_ASM(s32, "world/area_iwa/iwa_02/915D70", func_80240694_916404);
-
-#include "world/common/enemy/state/CleftAI_22.inc.c"
-
-INCLUDE_ASM(s32, "world/area_iwa/iwa_02/915D70", func_80240984_9166F4);
-
-#include "world/common/enemy/state/CleftAI_41.inc.c"
-
-#include "world/common/enemy/state/CleftAI_50.inc.c"
-
-#include "world/common/enemy/state/CleftAI_51.inc.c"
-
-#include "world/common/enemy/state/CleftAI_52.inc.c"
-
-INCLUDE_ASM(s32, "world/area_iwa/iwa_02/915D70", func_80240C90_916A00);
+#include "world/common/enemy/CleftAI.inc.c"
 
 #include "world/common/StashVars.inc.c"
 
