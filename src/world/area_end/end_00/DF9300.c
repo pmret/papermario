046672284f8ccd7971247669767436693d583ffe
Kevin Klions <kklions1@gmail.com>
2022-04-24 23:36:35 -0400

    matched func_80242AD4_DF94D4, was a big brain moment

--- src/world/area_end/end_00/DF9300.c
+++ src/world/area_end/end_00/DF9300.c
@@ -6,14 +6,18 @@ INCLUDE_ASM(s32, "world/area_end/end_00/DF9300", func_80242A30_DF9430);
 
 
 
-#ifdef NON_MATCHING
-ApiStatus func_80242AD4_DF94D4(Evt* script, s32 isInitialCall) {
-    Bytecode* args = script->ptrReadPos;
-    EffectInstance* var0 = (EffectInstance*) evt_get_variable(script, *args++);
+// #ifdef NON_MATCHING
+typedef struct unk_DF94D4 {
+    /* 0x00 */ s32 unk00;
+    /* 0x04 */ s32 unk04;
+} unk_DF94D4;
 
-    var0->data->index = 1;
+ApiStatus func_80242AD4_DF94D4(Evt *script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
+    EffectInstance* effect = (EffectInstance*) evt_get_variable(script, *args++);
+    unk_DF94D4* unk = ((unk_DF94D4*) effect->data)->unk04 = 1;
     return ApiStatus_DONE2;
 }
-#else
-INCLUDE_ASM(s32, "world/area_end/end_00/DF9300", func_80242AD4_DF94D4);
-#endif
+// #else
+// INCLUDE_ASM(s32, "world/area_end/end_00/DF9300", func_80242AD4_DF94D4);
+// #endif
