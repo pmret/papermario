f3c988aafcb7fca94c9c4a242082c7f2967e72aa
Kevin Klions <kklions1@gmail.com>
2022-04-25 11:09:57 -0400

    small changes for cleanup on a function

--- src/world/area_end/end_00/DF9300.c
+++ src/world/area_end/end_00/DF9300.c
@@ -4,20 +4,15 @@
 
 INCLUDE_ASM(s32, "world/area_end/end_00/DF9300", func_80242A30_DF9430);
 
-
-
-// #ifdef NON_MATCHING
 typedef struct unk_DF94D4 {
-    /* 0x00 */ s32 unk00;
+    /* 0x00 */ char unk00[0x04];
     /* 0x04 */ s32 unk04;
 } unk_DF94D4;
 
 ApiStatus func_80242AD4_DF94D4(Evt *script, s32 isInitialCall) {
     Bytecode* args = script->ptrReadPos;
     EffectInstance* effect = (EffectInstance*) evt_get_variable(script, *args++);
-    unk_DF94D4* unk = ((unk_DF94D4*) effect->data)->unk04 = 1;
+
+    ((unk_DF94D4*) effect->data)->unk04 = 1;
     return ApiStatus_DONE2;
 }
-// #else
-// INCLUDE_ASM(s32, "world/area_end/end_00/DF9300", func_80242AD4_DF94D4);
-// #endif
