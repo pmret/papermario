df09155a2fe340a879a0e67ac98d6d98c25b4a24
Ethan Roseman <ethteck@gmail.com>
2022-04-29 13:28:34 +0900

    smalls

--- src/world/area_kpa/kpa_03/A4A410.c
+++ src/world/area_kpa/kpa_03/A4A410.c
@@ -1,11 +1,12 @@
 #include "kpa_03.h"
 
 ApiStatus func_80241E50_A4A410(Evt* script) {
-    PlayerStatus* p = &gPlayerStatus;
-    if (p->lastGoodPosition.y >= 0) {
+    PlayerStatus* playerStatus = &gPlayerStatus;
+
+    if (playerStatus->lastGoodPosition.y >= 0) {
         evt_set_variable(script, GW(0), FALSE);
     }
-    if (p->lastGoodPosition.y <= -280) {
+    if (playerStatus->lastGoodPosition.y <= -280) {
         evt_set_variable(script, GW(0), TRUE);
     }
     return ApiStatus_BLOCK;
