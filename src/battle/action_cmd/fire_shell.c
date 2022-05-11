83c0b232f61f3d7c0fefa0eca089f9b112cdc576
Ethan Roseman <ethteck@gmail.com>
2022-05-12 00:13:56 +0900

    fire_shell

--- src/battle/action_cmd/fire_shell.c
+++ src/battle/action_cmd/fire_shell.c
@@ -1,12 +1,19 @@
 #include "common.h"
 #include "hud_element.h"
+#include "battle/battle.h"
 
 #define NAMESPACE action_command_fire_shell
 
 extern s32 D_802942C0;
+extern HudScript HudScript_StickMashLeft[];
 extern HudScript HudScript_StickNeutral[];
 extern HudScript HudScript_BlueMeter[];
 extern HudScript HudScript_100pct[];
+
+s32 D_802A9950_427320[5] = { 0, 25, 50, 75, 75 };
+s32 D_802A9964_427334[4] = { 40, 70, 99, 200 };
+s32 D_802A9974_427344[5] = { 35, 60, 80, 99, 200 };
+s32 D_802A9988_427358[6] = { 35, 35, 60, 80, 99, 200 };
 
 ApiStatus func_802A9000_4269D0(void) {
     ActionCommandStatus* actionCommandStatus = &gActionCommandStatus;
@@ -59,7 +66,155 @@ ApiStatus func_802A9000_4269D0(void) {
 
 #include "common/MashActionCommandInit.inc.c"
 
-INCLUDE_ASM(s32, "battle/action_cmd/fire_shell", func_802A9294_426C64);
+void func_802A9294_426C64(void) {
+    ActionCommandStatus* actionCommandStatus = &gActionCommandStatus;
+    BattleStatus* battleStatus = &gBattleStatus;
+    Actor* partner = battleStatus->partnerActor;
+    s32 id;
+    s32 cutoff;
+    s32 fillLevel;
+
+    switch (actionCommandStatus->state) {
+        case 0:
+            btl_set_popup_duration(99);
+
+            id = actionCommandStatus->hudElements[0];
+            hud_element_set_alpha(id, 255);
+            if (actionCommandStatus->unk_61 != 0) {
+                hud_element_clear_flags(id, 2);
+            }
+
+            id = actionCommandStatus->hudElements[1];
+            hud_element_set_alpha(id, 255);
+            if (actionCommandStatus->unk_61 != 0) {
+                hud_element_clear_flags(id, 2);
+            }
+
+            actionCommandStatus->state = 1;
+            break;
+        case 1:
+            btl_set_popup_duration(99);
+            actionCommandStatus->hudElementX += 20;
+            if (actionCommandStatus->hudElementX > 50) {
+                actionCommandStatus->hudElementX = 50;
+            }
+            hud_element_set_render_pos(actionCommandStatus->hudElements[0], actionCommandStatus->hudElementX, actionCommandStatus->hudElementY);
+            hud_element_set_render_pos(actionCommandStatus->hudElements[1], actionCommandStatus->hudElementX, actionCommandStatus->hudElementY + 28);
+            break;
+        case 10:
+            btl_set_popup_duration(99);
+            if (actionCommandStatus->unk_4E != 0) {
+                actionCommandStatus->unk_4E--;
+                return;
+            }
+            hud_element_set_script(actionCommandStatus->hudElements[0], HudScript_StickMashLeft);
+            actionCommandStatus->barFillLevel = 0;
+            battleStatus->unk_85 = 0;
+            actionCommandStatus->unk_5C = 0;
+            actionCommandStatus->unk_54 = actionCommandStatus->unk_52;
+            sfx_play_sound_with_params(0x80000041, 0, 0, 0);
+            actionCommandStatus->state = 11;
+        case 11:
+            btl_set_popup_duration(99);
+
+            if (actionCommandStatus->unk_68 == 0) {
+                cutoff = actionCommandStatus->mashMeterCutoffs[actionCommandStatus->mashMeterIntervals];
+                actionCommandStatus->barFillLevel -= D_802A9950_427320[actionCommandStatus->barFillLevel / cutoff / 20];
+                if (actionCommandStatus->barFillLevel < 0) {
+                    actionCommandStatus->barFillLevel = 0;
+                }
+                if (actionCommandStatus->unk_68 == 0) {
+                    if (battleStatus->currentButtonsDown & BUTTON_STICK_LEFT) {
+                        actionCommandStatus->unk_5C = 1;
+                    }
+
+                    if (!(battleStatus->currentButtonsDown & BUTTON_STICK_LEFT) && (actionCommandStatus->unk_5C != 0)) {
+                        actionCommandStatus->barFillLevel += (battleStatus->unk_434[actionCommandStatus->unk_50] * 950) / 100;
+                        actionCommandStatus->unk_5C = 0;
+                    }
+
+                    if (battleStatus->currentButtonsPressed & BUTTON_STICK_RIGHT) {
+                        actionCommandStatus->barFillLevel -= (battleStatus->unk_434[actionCommandStatus->unk_50] * 950) / 100;
+                    }
+                }
+            }
+
+            if (actionCommandStatus->barFillLevel < 0) {
+                actionCommandStatus->barFillLevel = 0;
+            }
+
+            if (actionCommandStatus->barFillLevel > 10000) {
+                id = actionCommandStatus->hudElements[2];
+                actionCommandStatus->barFillLevel = 10000;
+                actionCommandStatus->unk_68 = 1;
+                hud_element_set_render_pos(id, actionCommandStatus->hudElementX + 50, actionCommandStatus->hudElementY + 28);
+                hud_element_clear_flags(id, 2);
+            }
+
+            battleStatus->unk_84 = actionCommandStatus->barFillLevel / 100;
+            sfx_adjust_env_sound_params(0x80000041, 0, 0, battleStatus->unk_84 * 12);
+            switch (partner->actorBlueprint->level) {
+                case 0:
+                    if (battleStatus->unk_84 >= D_802A9964_427334[battleStatus->unk_85]) {
+                        battleStatus->unk_85++;
+                    }
+                    if (battleStatus->unk_85 > 0 && (battleStatus->unk_84 < D_802A9964_427334[battleStatus->unk_85 - 1])) {
+                        battleStatus->unk_85--;
+                    }
+                    break;
+                case 1:
+                    if (battleStatus->unk_84 >= D_802A9974_427344[battleStatus->unk_85]) {
+                        battleStatus->unk_85++;
+                    }
+                    if (battleStatus->unk_85 > 0 && (battleStatus->unk_84 < D_802A9974_427344[battleStatus->unk_85 - 1])) {
+                        battleStatus->unk_85--;
+                    }
+                    break;
+                case 2:
+                    if (battleStatus->unk_84 >= D_802A9988_427358[battleStatus->unk_85]) {
+                        battleStatus->unk_85++;
+                    }
+                    if (battleStatus->unk_85 > 0 && (battleStatus->unk_84 < D_802A9988_427358[battleStatus->unk_85 - 1])) {
+                        battleStatus->unk_85--;
+                    }
+                    break;
+            }
+
+            if (actionCommandStatus->unk_54 != 0) {
+                actionCommandStatus->unk_54--;
+                return;
+            }
+
+            if (actionCommandStatus->barFillLevel == 0) {
+                battleStatus->actionSuccess = -1;
+            } else {
+                battleStatus->actionSuccess = actionCommandStatus->barFillLevel / 100;
+            }
+
+            cutoff = actionCommandStatus->mashMeterCutoffs[actionCommandStatus->mashMeterIntervals - 1];
+            if (cutoff >= battleStatus->actionSuccess) {
+                battleStatus->unk_86 = -2;
+            } else {
+                battleStatus->unk_86 = 1;
+            }
+
+            if (battleStatus->actionSuccess == 100) {
+                func_80269160();
+            }
+            sfx_stop_sound(0x80000041);
+            btl_set_popup_duration(0);
+            actionCommandStatus->unk_54 = 5;
+            actionCommandStatus->state = 12;
+            break;
+        case 12:
+            if (actionCommandStatus->unk_54 != 0) {
+                actionCommandStatus->unk_54--;
+                return;
+            }
+            func_80268C9C();
+            break;
+    }
+}
 
 #include "common/UnkActionCommandFunc1.inc.c"
 
