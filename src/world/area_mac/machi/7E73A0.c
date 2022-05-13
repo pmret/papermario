3746411d89101307ac8f3625b350359b16718d83
Mathyland <34604237+Mathyland@users.noreply.github.com>
2022-05-12 23:53:11 -0500

    matched func_80240038_7E73D8, func_80240040_7E73E0 (#707)
    * matched func_80240038_7E73D8, func_80240040_7E73E0
    
    * Update src/world/area_mac/machi/7E73A0.c
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    * Update src/world/area_mac/machi/7E73A0.c
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    * Update src/world/area_mac/machi/7E73A0.c
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    * Update src/world/area_mac/machi/7E73A0.c
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>

--- src/world/area_mac/machi/7E73A0.c
+++ src/world/area_mac/machi/7E73A0.c
@@ -77,9 +77,13 @@ ApiStatus N(80240030_7E73D0)(Evt* script, s32 isInitialCall) {
     return ApiStatus_DONE2;
 }
 
-INCLUDE_ASM(s32, "world/area_mac/machi/7E73A0", func_80240038_7E73D8);
+ApiStatus func_80240038_7E73D8(Evt* script, s32 isInitialCall) {
+    return ApiStatus_DONE2;
+}
 
-INCLUDE_ASM(s32, "world/area_mac/machi/7E73A0", func_80240040_7E73E0);
+ApiStatus func_80240040_7E73E0(Evt* script, s32 isInitialCall) {
+    return ApiStatus_BLOCK;
+}
 
 INCLUDE_ASM(s32, "world/area_mac/machi/7E73A0", func_80240048_7E73E8);
 
