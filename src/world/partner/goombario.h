5b0cf78903b050b34efb46bcdaeb1040d083a944
Nasina <nasina987@gmail.com>
2022-05-26 13:01:29 -0400

    Matched the rest of world/actions (#715)
    * Matched func_802B6000_E28A30
    
    * oops
    
    * Attempted to match func_802B6000_E236E0 and added UB check
    
    * Removed Goto for walk.c func
    
    * Matched func_802B609C_E28ACC
    
    * Cleanup
    
    * oops
    
    * rodata moment
    
    * autoformat why
    
    * oop
    
    * oop but again
    
    * Fixed Warnings
    
    * Fixed Misc Whitespace

--- src/world/partner/goombario.h
+++ src/world/partner/goombario.h
@@ -8,6 +8,7 @@ void world_goombario_init(Npc* partner);
 s32 world_goombario_can_pause(Npc* partner);
 ApiStatus func_802BDB84(Evt* script, s32 isInitialCall);
 void world_goombario_pre_battle(Npc* partner);
+s32 func_802BD100_317020(s32 arg0);
 
 extern EvtScript world_goombario_take_out;
 extern EvtScript world_goombario_update;
