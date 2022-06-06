2762891daba025ca369f0aa6650c88e81fa39979
Ethan Roseman <ethteck@gmail.com>
2022-06-07 00:15:33 +0900

    OK

--- tools/build/bin_inc_c.py
+++ tools/build/bin_inc_c.py
@@ -5,17 +5,17 @@ from sys import argv
 import re
 
 if __name__ == "__main__":
-    infile, outfile = argv[1:]
+    infile, outfile, cname = argv[1:]
 
-    fname = Path(infile).stem
+    #fname = Path(infile).stem
 
-    cname = re.sub(r"[^0-9a-zA-Z_]", "_", fname)
+    # cname = re.sub(r"[^0-9a-zA-Z_]", "_", outfile)
 
-    if cname.startswith("ver_"):
-        cname = "_".join(cname.split("_")[2:])
+    # if cname.startswith("ver_"):
+    #     cname = "_".join(cname.split("_")[2:])
 
-    cname = re.sub(r"^build_include_", "", cname)
-    cname = re.sub(r"_inc_c$", "", cname)
+    # cname = re.sub(r"^build_include_", "", cname)
+    # cname = re.sub(r"_inc_c$", "", cname)
 
     with open(outfile, "w") as f:
         f.write(f"unsigned char {cname}[] = {{")
