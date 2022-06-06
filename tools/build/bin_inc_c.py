3c05750a1a508bfdf47984900f0da5771f7e9d49
Ethan Roseman <ethteck@gmail.com>
2022-06-07 00:55:57 +0900

    cleanup

--- tools/build/bin_inc_c.py
+++ tools/build/bin_inc_c.py
@@ -7,16 +7,6 @@ import re
 if __name__ == "__main__":
     infile, outfile, cname = argv[1:]
 
-    #fname = Path(infile).stem
-
-    # cname = re.sub(r"[^0-9a-zA-Z_]", "_", outfile)
-
-    # if cname.startswith("ver_"):
-    #     cname = "_".join(cname.split("_")[2:])
-
-    # cname = re.sub(r"^build_include_", "", cname)
-    # cname = re.sub(r"_inc_c$", "", cname)
-
     with open(outfile, "w") as f:
         f.write(f"unsigned char {cname}[] = {{")
 
