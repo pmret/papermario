2672cfdc7c69de12e0aa0080f8cdd60fe9f0c74f
JaThePlayer <janmalenta@gmail.com>
2022-06-15 20:16:58 +0200

    Matched the unmatched

--- src/battle/area_tik2/608CB0.c
+++ src/battle/area_tik2/608CB0.c
@@ -22,18 +22,19 @@ INCLUDE_ASM(s32, "battle/area_tik2/608CB0", func_80218404_608EB4);
 #include "common/StartRumbleWithParams.inc.c"
 #define NAMESPACE b_area_tik2
 
-#ifdef NON_MATCHING // regalloc
 ApiStatus func_802186D8_609188(Evt* script, s32 isInitialCall) {
     Bytecode* args = script->ptrReadPos;
     Bytecode arg0 = *args++;
+    f32 var0, var1;
 
-    evt_set_float_variable(script, arg0, evt_get_float_variable(script, *args++) * sin_rad(evt_get_float_variable(script, arg0) * TAU / 360.0f));
+    var1 = evt_get_float_variable(script, *args++);
+    do {} while(0);
+    var0 = evt_get_float_variable(script, arg0);
+
+    evt_set_float_variable(script, arg0, var1 * sin_rad(var0 * TAU / 360.0f));
 
     return ApiStatus_DONE2;
 }
-#else
-INCLUDE_ASM(s32, "battle/area_tik2/608CB0", func_802186D8_609188)
-#endif
 
 #define NAMESPACE dup_b_area_tik2
 #include "common/FadeBackgroundToBlack.inc.c"
