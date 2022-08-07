b04187c149a9fee1e4959162d16e0bd2010664ab
Jake Kallos <yourface255@yahoo.com>
2022-08-06 19:53:10 -0700

    match some functions in kmr_20/8EBE50.c (#742)
    * Update 8EBE50.c
    
    * removed asm source files
    
    * whupz
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>

--- src/world/area_kmr/kmr_20/8EBE50.c
+++ src/world/area_kmr/kmr_20/8EBE50.c
@@ -1,16 +1,21 @@
 #include "kmr_20.h"
 
+extern s32 D_8025B2A8;
+
 #include "world/common/atomic/Pipe.inc.c"
 
-INCLUDE_ASM(s32, "world/area_kmr/kmr_20/8EBE50", func_80240310_8EC130);
+ApiStatus func_80240310_8EC130(Evt* script, s32 isInitialCall) {
+    set_map_transition_effect(4);
+    return ApiStatus_DONE2;
+}
 
 ApiStatus func_80240330_8EC150(Evt* script, s32 isInitialCall) {
-    au_ambience_set_volume(0, 1000, 0x3F);
+    au_ambience_set_volume(0, 1000, 63);
     return ApiStatus_DONE2;
 }
 
 ApiStatus func_80240358_8EC178(Evt* script, s32 isInitialCall) {
-    au_ambience_set_volume(0, 1000, 0x7F);
+    au_ambience_set_volume(0, 1000, 127);
     return ApiStatus_DONE2;
 }
 
@@ -22,7 +27,10 @@ INCLUDE_ASM(s32, "world/area_kmr/kmr_20/8EBE50", func_802406BC_8EC4DC);
 
 INCLUDE_ASM(s32, "world/area_kmr/kmr_20/8EBE50", func_802406C8_8EC4E8);
 
-INCLUDE_ASM(s32, "world/area_kmr/kmr_20/8EBE50", func_80240B20_8EC940);
+ApiStatus func_80240B20_8EC940(Evt* script, s32 isInitialCall) {
+    free_generic_entity(D_8025B2A8);
+    return ApiStatus_DONE2;
+}
 
 INCLUDE_ASM(s32, "world/area_kmr/kmr_20/8EBE50", func_80240B48_8EC968);
 
