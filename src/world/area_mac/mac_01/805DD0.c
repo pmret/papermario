e6b5bcac56f88a0718e1ee971f78ccb3a712eda4
Ethan Roseman <ethteck@gmail.com>
2022-08-12 15:06:32 +0900

    3 mo

--- src/world/area_mac/mac_01/805DD0.c
+++ src/world/area_mac/mac_01/805DD0.c
@@ -8,5 +8,15 @@
 
 static char* N(exit_str_0) = "osr_00";
 
-INCLUDE_ASM(s32, "world/area_mac/mac_01/805DD0", func_802463E0_806C60);
-
+ApiStatus func_802463E0_806C60(Evt* script, s32 isInitialCall) {
+    if (evt_get_variable(NULL, EVT_SAVE_FLAG(260)) == 0) {
+        set_item_entity_flags(gGameStatusPtr->shopItemEntities[0].index, 0x40);
+    }
+    if (evt_get_variable(NULL, EVT_SAVE_FLAG(261)) == 0) {
+        set_item_entity_flags(gGameStatusPtr->shopItemEntities[1].index, 0x40);
+    }
+    if (evt_get_variable(NULL, EVT_SAVE_FLAG(262)) == 0) {
+        set_item_entity_flags(gGameStatusPtr->shopItemEntities[2].index, 0x40);
+    }
+    return ApiStatus_DONE2;
+}
