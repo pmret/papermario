993c09a70edfc50b09fa59a714d703879494a162
mrshigure <112147187+mrshigure@users.noreply.github.com>
2022-08-27 16:33:23 -0700

    Matched all functions in kkj_19/AF7C00.c, kmr_20/8ED5A0.c, and kmr_20/8EDB70.c (#752)
    * Matched all functions in kkj_19/AF7C00.c, kmr_20/8ED5A0.c, and kmr_20/8EDB70.c
    
    * Update src/world/area_kkj/kkj_19/AF7C00.c
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    * Update src/world/area_kkj/kkj_19/AF7C00.c
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    * Update src/world/area_kkj/kkj_19/AF7C00.c
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    * Update src/world/area_kkj/kkj_19/AF7C00.c
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    * Update src/world/area_kkj/kkj_19/AF7C00.c
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    * Update src/world/area_kkj/kkj_19/AF7C00.c
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    * Update src/world/area_kkj/kkj_19/AF7C00.c
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    * Update src/world/area_kmr/kmr_20/8EDB70.c
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    * Update src/world/area_kkj/kkj_19/AF7C00.c
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    * Update src/world/area_kkj/kkj_19/AF7C00.c
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    * Addressed comments
    
    * Fixed index usage
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>

--- src/world/area_kmr/kmr_20/8EDB70.c
+++ src/world/area_kmr/kmr_20/8EDB70.c
@@ -4,9 +4,36 @@
 #include "world/common/atomic/Pipe.inc.c"
 #define NAMESPACE kmr_20
 
-INCLUDE_ASM(s32, "world/area_kmr/kmr_20/8EDB70", func_80242030_8EDE50);
+ApiStatus func_80242030_8EDE50(Evt* script, s32 isInitialCall) {
+    func_8011B950(269, -1, 0, 1);
+    set_background_color_blend(0, 0, 0, 0);
 
-INCLUDE_ASM(s32, "world/area_kmr/kmr_20/8EDB70", func_80242084_8EDEA4);
+    gCameras[CAM_DEFAULT].bgColor[0] = 0;
+    gCameras[CAM_DEFAULT].bgColor[1] = 0;
+    gCameras[CAM_DEFAULT].bgColor[2] = 0;
+
+    return ApiStatus_DONE2;
+}
+
+ApiStatus func_80242084_8EDEA4(Evt* script, s32 isInitialCall) {
+    if (isInitialCall) {
+        script->functionTemp[1] = 0;
+    }
+
+    script->functionTemp[1] += 16;
+
+    if (script->functionTemp[1] > 255) {
+        script->functionTemp[1] = 255;
+    }
+
+    set_screen_overlay_params_front(0, script->functionTemp[1]);
+
+    if (script->functionTemp[1] == 255) {
+        return ApiStatus_DONE2;
+    }
+
+    return ApiStatus_BLOCK;
+}
 
 ApiStatus func_802420EC_8EDF0C(Evt* script, s32 isInitialCall) {
     if (isInitialCall) {
