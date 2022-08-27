21a3a1fa321033f0c67c739108b2458436369531
mrshigure <mrshigure64@gmail.com>
2022-08-27 04:32:44 -0700

    Matched all functions in kkj_19/AF7C00.c, kmr_20/8ED5A0.c, and kmr_20/8EDB70.c

--- src/world/area_kmr/kmr_20/8ED5A0.c
+++ src/world/area_kmr/kmr_20/8ED5A0.c
@@ -8,9 +8,21 @@
 #include "common/foliage.inc.c"
 #define NAMESPACE kmr_20
 
-INCLUDE_ASM(s32, "world/area_kmr/kmr_20/8ED5A0", func_80241C9C_8EDABC);
+ApiStatus func_80241C9C_8EDABC(Evt* script, s32 isInitialCall) {
+    func_8011B950(269, -1, 1, 1);
+    set_background_color_blend(0, 0, 0, 255);
 
-INCLUDE_ASM(s32, "world/area_kmr/kmr_20/8ED5A0", func_80241CF0_8EDB10);
+    gCameras->bgColor[0] = 0;
+    gCameras->bgColor[1] = 0;
+    gCameras->bgColor[2] = 0;
+
+    return ApiStatus_DONE2;
+}
+
+ApiStatus func_80241CF0_8EDB10(Evt* script, s32 isInitialCall) {
+    gCameras->moveSpeed = evt_get_float_variable(script, *script->ptrReadPos);
+    return ApiStatus_DONE2;
+}
 
 ApiStatus func_80241D1C_8EDB3C(Evt* script, s32 isInitialCall) {
     au_ambience_set_volume(0, 1000, 0x3F);
