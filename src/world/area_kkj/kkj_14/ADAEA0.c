9a75cfa2c94f7fc98a38a77300a18602b37e77bb
mrshigure <mrshigure64@gmail.com>
2022-08-31 06:30:44 +0000

    Addressed comments

--- src/world/area_kkj/kkj_14/ADAEA0.c
+++ src/world/area_kkj/kkj_14/ADAEA0.c
@@ -4,7 +4,9 @@ extern char D_800D9668[];
 
 ApiStatus func_80240000_ADAEA0(Evt* script, s32 isInitialCall) {
     if (evt_get_variable(NULL, GSW(0)) == -128) {
-        sprintf(D_800D9668, "nok_bg\0\0pra_01\0\0kkj_11\0\0kkj_01\0\0kkj_26\0\0kkj_15\0\0kkj_27\0\0kkj_26");
+        sprintf(D_800D9668, "nok_bg");
     }
     return ApiStatus_BLOCK;
 }
+
+static char* N(exit_str_0) = "pra_01\0\0kkj_11\0\0kkj_01\0\0kkj_26\0\0kkj_15\0\0kkj_27\0\0kkj_26";
