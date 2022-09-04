fabcdbd7151f5bfae2a362e812f87c8eb0ba2c51
stephenJohnGiardina <stephengiar@gmail.com>
2022-09-04 15:42:19 -0400

    Decompile func_802400BC_A4C01C

--- src/world/area_kpa/kpa_04/A4BF60.c
+++ src/world/area_kpa/kpa_04/A4BF60.c
@@ -5,4 +5,30 @@ static char* N(exit_str_1) = "kpa_81";
 
 #include "world/common/UnkFunc11.inc.c"
 
-INCLUDE_ASM(s32, "world/area_kpa/kpa_04/A4BF60", func_802400BC_A4C01C);
+ApiStatus func_802400BC_A4C01C(Evt *script, s32 isInitialCall) {
+    PlayerStatus* playerstatus = &gPlayerStatus;
+    f32 sp24;
+    f32 x;
+    f32 y;
+    f32 z;
+
+    if (script->varTable[5] == 0) {
+        playerstatus->position.x = script->varTable[0];
+    } else {
+        playerstatus->position.z = script->varTable[0];
+    }
+
+    x = playerstatus->position.x;
+    y = playerstatus->position.y + 10.0f;
+    z = playerstatus->position.z;
+
+    sp24 = 40.0f;
+
+    npc_raycast_down_sides(0, &x, &y, &z, &sp24);
+
+    playerstatus->position.x = x;
+    playerstatus->position.y = y;
+    playerstatus->position.z = z;
+
+    return ApiStatus_DONE2;
+}
