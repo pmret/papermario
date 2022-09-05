f0ce95a37a2fb1bd76e12b54c026dce0495ccb5c
KazuOfficial <kazuofficial.contact@gmail.com>
2022-09-05 15:39:24 +0100

    Separated strings in func_80240000_AAFFF0

--- src/world/area_osr/osr_01/AAFFF0.c
+++ src/world/area_osr/osr_01/AAFFF0.c
@@ -4,7 +4,9 @@ extern s8 D_800D9668;
 
 ApiStatus func_80240000_AAFFF0(Evt* script, s32 isInitialCall) {
     if (gGameStatusPtr->entryID == 3) {
-        sprintf(&D_800D9668, "hos_bg\0\0mac_01\0\0hos_00\0\0mac_04");
+        sprintf(&D_800D9668, "hos_bg");
     }
     return ApiStatus_BLOCK;
 }
+
+const char* N(exit_str_0) = "mac_01\0\0hos_00\0\0mac_04";
