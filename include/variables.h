0652e9f28fccb87054a7270f804e6aa23067e718
KazuOfficial <kazuofficial.contact@gmail.com>
2022-09-06 08:42:24 +0100

    Addressed PR comments continuation

--- include/variables.h
+++ include/variables.h
@@ -162,8 +162,8 @@ extern s32 D_800A0924;
 extern s8 D_800A0921;
 extern s16 D_800A0922;
 extern s32 wMapTexName;
-extern char* wMapHitName;
-extern s32 wMapShapeName;
+extern char wMapHitName[];
+extern char wMapShapeName[];
 extern s32* D_80210000;
 extern u8 D_802D9D70; // player alpha1 copy?
 extern u8 D_802D9D71; // player alpha2 copy?
--- src/world/area_kpa/kpa_114/A9E980.c
+++ src/world/area_kpa/kpa_114/A9E980.c
@@ -1,8 +1,8 @@
 #include "kpa_114.h"
 
 ApiStatus func_80240000_A9E980(Evt* script, s32 isInitialCall) {
-    sprintf(&wMapShapeName, "kpa_112_shape");
-    sprintf(&wMapHitName, "kpa_112_hit");
+    sprintf(wMapShapeName, "kpa_112_shape");
+    sprintf(wMapHitName, "kpa_112_hit");
     return ApiStatus_BLOCK;
 }
 
