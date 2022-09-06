1bff9803208571cd45d7e60a2341ed327fbf2055
KazuOfficial <kazuofficial.contact@gmail.com>
2022-09-06 14:02:07 +0100

    Match functions from area_pra

--- src/world/area_pra/pra_37/D92F40.c
+++ src/world/area_pra/pra_37/D92F40.c
@@ -1,6 +1,13 @@
 #include "pra_37.h"
 
-INCLUDE_ASM(s32, "world/area_pra/pra_37/D92F40", func_80240000_D92F40);
+ApiStatus func_80240000_D92F40(Evt* script, s32 isInitialCall) {
+    gGameStatusPtr->playerSpriteSet = 1;
+    sprintf(wMapShapeName, "pra_10_shape");
+    sprintf(wMapHitName, "pra_10_hit");
+    return ApiStatus_BLOCK;
+}
+
+const char* N(exit_str_0) = "pra_22\0\0pra_28";
 
 #include "world/common/atomic/Reflection.inc.c"
 
