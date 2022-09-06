85062f3f82729504f0bbb337d8093b9e0f7f4f3b
KazuOfficial <kazuofficial.contact@gmail.com>
2022-09-06 08:48:44 +0100

    Added Bytecode args in func_80240020_AB2190

--- src/world/area_osr/osr_02/AB2190.c
+++ src/world/area_osr/osr_02/AB2190.c
@@ -1,6 +1,8 @@
 #include "osr_02.h"
 
 ApiStatus func_80240020_AB2190(Evt* script, s32 isInitialCall) {
-    gPlayerStatus.unk_0D = evt_get_float_variable(script, *script->ptrReadPos);
+    Bytecode* args = script->ptrReadPos;
+
+    gPlayerStatus.unk_0D = evt_get_float_variable(script, *args++);
     return ApiStatus_DONE2;
 }
