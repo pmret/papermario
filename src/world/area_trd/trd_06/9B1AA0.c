a580f19ab8ab550fb628c5005344f5a139067ed8
Kevin Klions <kklions1@gmail.com>
2022-09-07 09:19:01 -0400

    Matched a couple random functions  (#761)
    * matched function rising_bubble_update
    
    * matched function func_80240A5C_9B21EC
    
    * matched func_802417AC_EAA28C
    
    * fixed build, addressed PR comments
    
    * addressed more PR comments

--- src/world/area_trd/trd_06/9B1AA0.c
+++ src/world/area_trd/trd_06/9B1AA0.c
@@ -1,5 +1,7 @@
 #include "trd_06.h"
 
+extern s32 D_802451F0;
+
 INCLUDE_ASM(s32, "world/area_trd/trd_06/9B1AA0", func_80240310_9B1AA0);
 
 INCLUDE_ASM(s32, "world/area_trd/trd_06/9B1AA0", func_80240724_9B1EB4);
@@ -10,7 +12,10 @@ INCLUDE_ASM(s32, "world/area_trd/trd_06/9B1AA0", func_80240854_9B1FE4);
 
 INCLUDE_ASM(s32, "world/area_trd/trd_06/9B1AA0", func_8024093C_9B20CC);
 
-INCLUDE_ASM(s32, "world/area_trd/trd_06/9B1AA0", func_80240A5C_9B21EC);
+ApiStatus func_80240A5C_9B21EC(void) {
+    D_802451F0 = 1;
+    return ApiStatus_DONE2;
+}
 
 ApiStatus func_80240A70_9B2200(Evt* script, s32 isInitialCall) {
     f32 partnerCollisionHeight = get_npc_unsafe(NPC_PARTNER)->collisionHeight;
