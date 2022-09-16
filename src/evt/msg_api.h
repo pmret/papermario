1ad0f7b1499f14b29370bbdab7db65d539bf43e3
Alex Bates <alex@nanaian.town>
2022-09-16 19:27:35 +0100

    Fix 'badly punctuated parameter list in define' issues (#777)
    * remove defines of old SCRIPT macro
    
    * get rid of uses of unnamed ...

--- src/evt/msg_api.h
+++ src/evt/msg_api.h
@@ -3,20 +3,20 @@
 
 #include "script_api/macros.h"
 
-#define SpeakToPlayer(...) \
-    _EVT_CMD(EVT_OP_CALL, evt_SpeakToPlayer, ##__VA_ARGS__),
+#define SpeakToPlayer(ARGS...) \
+    _EVT_CMD(EVT_OP_CALL, evt_SpeakToPlayer, ##ARGS),
 ApiStatus evt_SpeakToPlayer(Evt* script, s32 isInitialCall);
 
-#define EndSpeech(...) \
-    _EVT_CMD(EVT_OP_CALL, evt_EndSpeech, ##__VA_ARGS__),
+#define EndSpeech(ARGS...) \
+    _EVT_CMD(EVT_OP_CALL, evt_EndSpeech, ##ARGS),
 ApiStatus evt_EndSpeech(Evt* script, s32 isInitialCall);
 
-#define ContinueSpeech(...) \
-    _EVT_CMD(EVT_OP_CALL, evt_ContinueSpeech, ##__VA_ARGS__),
+#define ContinueSpeech(ARGS...) \
+    _EVT_CMD(EVT_OP_CALL, evt_ContinueSpeech, ##ARGS),
 ApiStatus evt_ContinueSpeech(Evt* script, s32 isInitialCall);
 
-#define SpeakToNpc(...) \
-    _EVT_CMD(EVT_OP_CALL, evt_SpeakToNpc, ##__VA_ARGS__),
+#define SpeakToNpc(ARGS...) \
+    _EVT_CMD(EVT_OP_CALL, evt_SpeakToNpc, ##ARGS),
 ApiStatus evt_SpeakToNpc(Evt* script, s32 isInitialCall);
 
 #endif
