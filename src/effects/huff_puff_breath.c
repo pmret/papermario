b61417625857c813ef4a2bd30c6650b6785e1da0
Spencer Vaughn <svaughn57@gmail.com>
2022-10-01 23:11:05 -0500

    huff_puff_breath_update: Compiled c

--- src/effects/huff_puff_breath.c
+++ src/effects/huff_puff_breath.c
@@ -8,58 +8,66 @@ INCLUDE_ASM(s32, "effects/huff_puff_breath", huff_puff_breath_main);
 void huff_puff_breath_init(void) {
 }
 
+typedef struct unk {
+    s32 unk_10;
+    s32 unk_14;
+    s32 unk_24;
+    f32 unk_38;
+    f32 unk_3C;
+    f32 unk_40;
+    f32 unk_44;
+} unk;
 // INCLUDE_ASM(s32, "effects/huff_puff_breath", huff_puff_breath_update);
-void huff_puff_breath_update(EffectInstance *arg0) {
+void huff_puff_breath_update(EffectInstance* effect) {
     f32 temp_f0;
     f32 temp_f0_2;
     f32 temp_f0_3;
     f32 temp_f0_4;
-    s32 *temp_v1;
+    unk *data = effect->data.huffPuffBreath;
     s32 temp_a1;
     s32 temp_a1_2;
     s32 temp_a1_3;
     s32 temp_a2;
 
-    temp_a1 = arg0->flags;
-    temp_v1 = arg0->data.any;
-    if (temp_a1 & 0x10) {
-        arg0->flags = temp_a1 & ~0x10;
-        temp_v1->unk10 = 0x10;
+    temp_a1 = effect->flags;
+    if (temp_a1 & 16) {
+        effect->flags = temp_a1 & ~16;
+        data->unk_10 = 16;
     }
-    temp_a1_2 = temp_v1->unk10;
-    if (temp_a1_2 < 0x3E8) {
-        temp_v1->unk10 = (s32) (temp_a1_2 - 1);
+    temp_a1_2 = data->unk_10;
+    if (temp_a1_2 < 1000) {
+        data->unk_10 = (s32) (temp_a1_2 - 1);
     }
-    temp_a1_3 = temp_v1->unk10;
-    temp_a2 = temp_v1->unk14 + 1;
-    temp_v1->unk14 = temp_a2;
+    temp_a1_3 = data->unk_10;
+    temp_a2 = data->unk_14 + 1;
+    data->unk_14 = temp_a2;
     if (temp_a1_3 < 0) {
-        shim_remove_effect(arg0);
+        shim_remove_effect(effect);
         return;
     }
-    if (temp_a1_3 < 0x10) {
-        temp_v1->unk24 = (s32) (temp_a1_3 * 0x10);
+    if (temp_a1_3 < 16) {
+        data->unk_24 = (s32) (temp_a1_3 * 16);
     }
-    if (temp_a2 < 0x10) {
-        temp_v1->unk24 = (s32) ((temp_a2 * 0x10) + 0xF);
+    if (temp_a2 < 16) {
+        data->unk_24 = (s32) ((temp_a2 * 16) + 15);
     }
-    temp_f0 = temp_v1->unk38 + temp_v1->unk3C;
-    temp_v1->unk38 = temp_f0;
+    temp_f0 = data->unk_38 + data->unk_3C;
+    data->unk_38 = temp_f0;
     if (temp_f0 >= 256.0f) {
-        temp_v1->unk38 = (f32) (temp_f0 - 256.0f);
+        data->unk_38 = (f32) (temp_f0 - 256.0f);
     }
-    temp_f0_2 = temp_v1->unk38;
+    temp_f0_2 = data->unk_38;
     if (temp_f0_2 < 0.0f) {
-        temp_v1->unk38 = (f32) (temp_f0_2 + 256.0f);
+        data->unk_38 = (f32) (temp_f0_2 + 256.0f);
     }
-    temp_f0_3 = temp_v1->unk40 + temp_v1->unk44;
-    temp_v1->unk40 = temp_f0_3;
+    temp_f0_3 = data->unk_40 + data->unk_44;
+    data->unk_40 = temp_f0_3;
     if (temp_f0_3 >= 256.0f) {
-        temp_v1->unk40 = (f32) (temp_f0_3 - 256.0f);
+        data->unk_40 = (f32) (temp_f0_3 - 256.0f);
     }
-    temp_f0_4 = temp_v1->unk40;
+    temp_f0_4 = data->unk_40;
     if (temp_f0_4 < 0.0f) {
-        temp_v1->unk40 = (f32) (temp_f0_4 + 256.0f);
+        data->unk_40 = (f32) (temp_f0_4 + 256.0f);
     }
 }
 
