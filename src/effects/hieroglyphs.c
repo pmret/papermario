e73d816a773e743147705422e8b1ef3b91b7a990
Spencer Vaughn <svaughn57@gmail.com>
2022-10-03 00:41:24 -0500

    hieroglyphs_update: Matched clean up

--- src/effects/hieroglyphs.c
+++ src/effects/hieroglyphs.c
@@ -11,30 +11,26 @@ void hieroglyphs_init(void) {
 void hieroglyphs_update(EffectInstance* effect) {
     HieroglyphsFXData* data = effect->data.hieroglyphs;
     s32 temp_a2;
-    s32 temp_v1 = effect->flags;
-    s32 temp_v1_2;
-    s32 temp_v1_3;
 
-    if (temp_v1 & 16) {
-        effect->flags = temp_v1 & ~16;
+    if (effect->flags & 16) {
+        effect->flags &= ~16;
         data->unk_10 = 16;
     }
-    temp_v1_2 = data->unk_10;
-    if (temp_v1_2 < 1000) {
-        data->unk_10 = (s32) (temp_v1_2 - 1);
+
+    if (data->unk_10 < 1000) {
+        data->unk_10--;
     }
-    temp_v1_3 = data->unk_10;
-    temp_a2 = data->unk_14 + 1;
-    data->unk_14 = temp_a2;
-    if (temp_v1_3 < 0) {
+    temp_a2 = data->unk_14 + 1; // Needed for match?
+    data->unk_14++;
+    if (data->unk_10 < 0) {
         shim_remove_effect(effect);
         return;
     }
-    if (temp_v1_3 < 16) {
-        data->unk_24 = (s32) (temp_v1_3 * 16);
+    if (data->unk_10 < 16) {
+        data->unk_24 = data->unk_10 * 16;
     }
     if (temp_a2 < 16) {
-        data->unk_24 = (s32) ((temp_a2 * 16) + 15);
+        data->unk_24 = (temp_a2 * 16) + 15;
     }
 }
 
