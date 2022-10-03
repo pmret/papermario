7353e2fe29473a8e2e89a317be4964b38a08c546
Spencer Vaughn <svaughn57@gmail.com>
2022-10-02 23:00:12 -0500

    sweat_update: Initial c

--- src/effects/sweat.c
+++ src/effects/sweat.c
@@ -8,7 +8,30 @@ INCLUDE_ASM(s32, "effects/sweat", sweat_main);
 void sweat_init(void) {
 }
 
-INCLUDE_ASM(s32, "effects/sweat", sweat_update);
+// INCLUDE_ASM(s32, "effects/sweat", sweat_update);
+void sweat_update(EffectInstance *arg0) {
+    f32 temp_f0;
+    s32 *temp_v1;
+    s32 temp_v0;
+
+    temp_v1 = arg0->data.any;
+    temp_v0 = temp_v1->unk24 - 1;
+    temp_v1->unk24 = temp_v0;
+    if (temp_v0 < 0) {
+        shim_remove_effect(arg0);
+        return;
+    }
+    temp_v1->unk4 = (f32) (temp_v1->unk4 + temp_v1->unk18);
+    temp_v1->unk8 = (f32) (temp_v1->unk8 + temp_v1->unk1C);
+    temp_v1->unkC = (f32) (temp_v1->unkC + temp_v1->unk20);
+    temp_f0 = (f32) ((f64) temp_v1->unk1C - 0.05);
+    temp_v1->unk1C = temp_f0;
+    temp_v1->unk18 = (f32) ((f64) temp_v1->unk18 * 0.94);
+    if ((f64) temp_f0 < -0.1) {
+        temp_v1->unk1C = -0.1f;
+    }
+    temp_v1->unk20 = (f32) ((f64) temp_v1->unk20 * 0.94);
+}
 
 void sweat_render(EffectInstance* effect) {
     RenderTask renderTask;
