d830886a53cf94cd3e9d34a5aa89538d5fcb3148
Sergey Zorkin <serge_zorkin@mail.ru>
2022-10-04 01:04:54 +0300

    fixed sprite include

--- src/battle/area_pra/actor/albino_dino.c
+++ src/battle/area_pra/actor/albino_dino.c
@@ -2,7 +2,7 @@
 #include "battle/battle.h"
 #include "script_api/battle.h"
 #include "effects.h"
-#include "sprite/npc/albino_dino.h"
+#include "sprite/npc/AlbinoDino.h"
 
 #define NAMESPACE b_area_pra_albino_dino
 
@@ -12,15 +12,15 @@ extern EvtScript N(takeTurn);
 extern EvtScript N(handleEvent);
 
 s32 N(idleAnimations)[] = {
-    STATUS_NORMAL, NPC_ANIM_albino_dino_Palette_00_Anim_3,
-    STATUS_STONE, NPC_ANIM_albino_dino_Palette_00_Anim_0,
-    STATUS_SLEEP, NPC_ANIM_albino_dino_Palette_00_Anim_3,
-    STATUS_POISON, NPC_ANIM_albino_dino_Palette_00_Anim_3,
-    STATUS_STOP, NPC_ANIM_albino_dino_Palette_00_Anim_0,
-    STATUS_STATIC, NPC_ANIM_albino_dino_Palette_00_Anim_3,
-    STATUS_PARALYZE, NPC_ANIM_albino_dino_Palette_00_Anim_0,
-    STATUS_DIZZY, NPC_ANIM_albino_dino_Palette_00_Anim_0,
-    STATUS_FEAR, NPC_ANIM_albino_dino_Palette_00_Anim_0,
+    STATUS_NORMAL, ANIM_AlbinoDino_Anim03,
+    STATUS_STONE, ANIM_AlbinoDino_Anim00,
+    STATUS_SLEEP, ANIM_AlbinoDino_Anim03,
+    STATUS_POISON, ANIM_AlbinoDino_Anim03,
+    STATUS_STOP, ANIM_AlbinoDino_Anim00,
+    STATUS_STATIC, ANIM_AlbinoDino_Anim03,
+    STATUS_PARALYZE, ANIM_AlbinoDino_Anim00,
+    STATUS_DIZZY, ANIM_AlbinoDino_Anim00,
+    STATUS_FEAR, ANIM_AlbinoDino_Anim00,
     STATUS_END,
 };
 
@@ -108,11 +108,11 @@ EvtScript N(idle) = {
 };
 
 EvtScript N(returnHome) = {
-    EVT_CALL(SetAnimation, ACTOR_SELF, 1, NPC_ANIM_albino_dino_Palette_00_Anim_5)
+    EVT_CALL(SetAnimation, ACTOR_SELF, 1, ANIM_AlbinoDino_Anim05)
     EVT_CALL(SetGoalToHome, ACTOR_SELF)
     EVT_CALL(SetActorSpeed, ACTOR_SELF, EVT_FLOAT(8.0))
     EVT_CALL(RunToGoal, ACTOR_SELF, 0, FALSE)
-    EVT_CALL(SetAnimation, ACTOR_SELF, 1, NPC_ANIM_albino_dino_Palette_00_Anim_3)
+    EVT_CALL(SetAnimation, ACTOR_SELF, 1, ANIM_AlbinoDino_Anim03)
     EVT_RETURN
     EVT_END
 };
@@ -125,83 +125,83 @@ EvtScript N(handleEvent) = {
         EVT_CASE_OR_EQ(EVENT_HIT_COMBO)
         EVT_CASE_OR_EQ(EVENT_HIT)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_8)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim08)
             EVT_EXEC_WAIT(DoNormalHit)
         EVT_END_CASE_GROUP
         EVT_CASE_EQ(EVENT_BURN_HIT)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_A)
-            EVT_SET_CONST(LVar2, NPC_ANIM_albino_dino_Palette_00_Anim_B)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim0A)
+            EVT_SET_CONST(LVar2, ANIM_AlbinoDino_Anim0B)
             EVT_EXEC_WAIT(DoBurnHit)
         EVT_CASE_EQ(EVENT_BURN_DEATH)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_A)
-            EVT_SET_CONST(LVar2, NPC_ANIM_albino_dino_Palette_00_Anim_B)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim0A)
+            EVT_SET_CONST(LVar2, ANIM_AlbinoDino_Anim0B)
             EVT_EXEC_WAIT(DoBurnHit)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_B)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim0B)
             EVT_EXEC_WAIT(DoDeath)
             EVT_RETURN
         EVT_CASE_EQ(EVENT_SPIN_SMASH_HIT)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_8)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim08)
             EVT_EXEC_WAIT(DoSpinSmashHit)
         EVT_CASE_EQ(EVENT_SPIN_SMASH_DEATH)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_8)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim08)
             EVT_EXEC_WAIT(DoSpinSmashHit)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_9)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim09)
             EVT_EXEC_WAIT(DoDeath)
             EVT_RETURN
         EVT_CASE_EQ(EVENT_SHOCK_HIT)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_8)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim08)
             EVT_EXEC_WAIT(DoShockHit)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_8)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim08)
             EVT_EXEC_WAIT(DoJumpBack)
             EVT_EXEC_WAIT(N(returnHome))
         EVT_CASE_EQ(EVENT_SHOCK_DEATH)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_8)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim08)
             EVT_EXEC_WAIT(DoShockHit)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_9)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim09)
             EVT_EXEC_WAIT(DoDeath)
             EVT_RETURN
         EVT_CASE_OR_EQ(EVENT_UNKNOWN_TRIGGER)
         EVT_CASE_OR_EQ(EVENT_IMMUNE)
         EVT_CASE_OR_EQ(EVENT_AIR_LIFT_FAILED)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_3)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim03)
             EVT_EXEC_WAIT(DoImmune)
         EVT_END_CASE_GROUP
         EVT_CASE_EQ(EVENT_DEATH)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_8)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim08)
             EVT_EXEC_WAIT(DoNormalHit)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_9)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim09)
             EVT_EXEC_WAIT(DoDeath)
             EVT_RETURN
         EVT_CASE_EQ(EVENT_RECOVER_STATUS)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_3)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim03)
             EVT_EXEC_WAIT(DoRecover)
         EVT_CASE_EQ(EVENT_SCARE_AWAY)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_5)
-            EVT_SET_CONST(LVar2, NPC_ANIM_albino_dino_Palette_00_Anim_8)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim05)
+            EVT_SET_CONST(LVar2, ANIM_AlbinoDino_Anim08)
             EVT_EXEC_WAIT(DoScareAway)
             EVT_RETURN
         EVT_CASE_EQ(EVENT_BEGIN_AIR_LIFT)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_5)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim05)
             EVT_EXEC_WAIT(DoAirLift)
         EVT_CASE_EQ(EVENT_BLOW_AWAY)
             EVT_SET_CONST(LVar0, 1)
-            EVT_SET_CONST(LVar1, NPC_ANIM_albino_dino_Palette_00_Anim_8)
+            EVT_SET_CONST(LVar1, ANIM_AlbinoDino_Anim08)
             EVT_EXEC_WAIT(DoBlowAway)
             EVT_RETURN
         EVT_CASE_DEFAULT
@@ -250,7 +250,7 @@ EvtScript N(takeTurn) = {
             EVT_WAIT(4)
         EVT_END_LOOP
     EVT_END_THREAD
-    EVT_CALL(SetAnimation, ACTOR_SELF, 1, NPC_ANIM_albino_dino_Palette_00_Anim_5)
+    EVT_CALL(SetAnimation, ACTOR_SELF, 1, ANIM_AlbinoDino_Anim05)
     EVT_WAIT(20)
     EVT_CALL(EnemyTestTarget, ACTOR_SELF, LVarF, 0, 0, 1, BS_FLAGS1_10)
     EVT_SWITCH(LVarF)
@@ -307,7 +307,7 @@ EvtScript N(takeTurn) = {
             EVT_CALL(SetGoalPos, ACTOR_SELF, LVar0, LVar1, LVar2)
             EVT_CALL(JumpToGoal, ACTOR_SELF, 15, FALSE, TRUE, FALSE)
             EVT_CALL(PlaySoundAtActor, ACTOR_SELF, SOUND_2FD)
-            EVT_CALL(SetAnimation, ACTOR_SELF, 1, NPC_ANIM_albino_dino_Palette_00_Anim_3)
+            EVT_CALL(SetAnimation, ACTOR_SELF, 1, ANIM_AlbinoDino_Anim03)
             EVT_CALL(ShakeCam, 1, 0, 3, EVT_FLOAT(1.0))
             EVT_WAIT(10)
             EVT_CALL(YieldTurn)
