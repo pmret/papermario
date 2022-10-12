fec4a513acedf2cb293b827ddae8c91cde58d5d8
Spencer Vaughn <svaughn57@gmail.com>
2022-10-12 07:39:03 -0500

    Matched effects funcs (#818)
    * thunderbolt_ring: Matched update and main
    
    * snowfall_main: Matched
    
    * shiny_flare_main: Matched
    
    * shiny_flare_update: Matched
    
    * thunderbolt_ring_update: clean up
    
    * rising_bubble_main: matched
    
    * ring_blast: Matched main and update
    
    * quizmo_stage: Matched main and update
    
    * ring_blast_main: Fixed warning message
    
    * PR_818: PR clean up

--- src/effects/shiny_flare.c
+++ src/effects/shiny_flare.c
@@ -2,13 +2,70 @@
 #include "effects_internal.h"
 
 void shiny_flare_appendGfx(void* effect);
+void shiny_flare_init(EffectInstance* effect);
+void shiny_flare_update(EffectInstance* effect);
+void shiny_flare_render(EffectInstance* effect);
 
-INCLUDE_ASM(s32, "effects/shiny_flare", shiny_flare_main);
+EffectInstance* shiny_flare_main(s32 arg0, f32 arg1, f32 arg2, f32 arg3, f32 arg4) {
+    EffectBlueprint effectBp;
+    EffectInstance* effect;
+    ShinyFlareFXData* data;
+    s32 numParts = 1;
 
-void shiny_flare_init(void) {
+    effectBp.init = shiny_flare_init;
+    effectBp.update = shiny_flare_update;
+    effectBp.renderWorld = shiny_flare_render;
+    effectBp.unk_00 = 0;
+    effectBp.unk_14 = 0;
+    effectBp.effectID = EFFECT_SHINY_FLARE;
+    effect = shim_create_effect_instance(&effectBp);
+    effect->numParts = numParts;
+
+    data = effect->data.shinyFlare = shim_general_heap_malloc(numParts * sizeof(*data));
+    ASSERT(data != NULL);
+
+    data->unk_00 = arg0;
+    data->lifeTime = 0;
+    data->timeLeft = 10;
+    data->unk_24 = 255;
+    data->pos.x = arg1;
+    data->pos.y = arg2;
+    data->pos.z = arg3;
+    data->unk_28 = arg4;
+    data->unk_18 = 70;
+    data->unk_1C = 180;
+    data->unk_20 = 120;
+    data->unk_2C = 0;
+
+    return effect;
 }
 
-INCLUDE_ASM(s32, "effects/shiny_flare", shiny_flare_update);
+void shiny_flare_init(EffectInstance* effect) {
+}
+
+void shiny_flare_update(EffectInstance *effect) {
+    ShinyFlareFXData *data = effect->data.shinyFlare;
+
+    if (effect->flags & 16) {
+        effect->flags &= ~16;
+        data->timeLeft = 10;
+    }
+    if (data->timeLeft < 1000) {
+        data->timeLeft--;
+    }
+
+    data->lifeTime++;
+    if (data->timeLeft < 0) {
+        shim_remove_effect(effect);
+        return;
+    }
+    if (data->timeLeft < 4) {
+        data->unk_2C *= 0.5;
+        return;
+    }
+
+    data->unk_2C = data->lifeTime * 0.2f + 0.3;
+}
 
 void shiny_flare_render(EffectInstance* effect) {
     RenderTask renderTask;
--- src/effects/snowfall.c
+++ src/effects/snowfall.c
@@ -1,13 +1,53 @@
 #include "common.h"
 #include "effects_internal.h"
 
+
+void snowfall_init(EffectInstance* effect);
+void snowfall_update(EffectInstance* effect);
+void snowfall_render(EffectInstance* effect);
 void snowfall_appendGfx(void* effect);
 
 INCLUDE_ASM(s32, "effects/snowfall", func_E008A000);
 
-INCLUDE_ASM(s32, "effects/snowfall", snowfall_main);
+void func_E008A000(SnowfallFXData* data);
 
-void snowfall_init(void) {
+EffectInstance* snowfall_main(s32 arg0, s32 arg1) {
+    EffectBlueprint effectBp;
+    EffectInstance* effect;
+    SnowfallFXData* data;
+    s32 numParts = arg1 + 1;
+    s32 i;
+
+    effectBp.init = snowfall_init;
+    effectBp.update = snowfall_update;
+    effectBp.renderWorld = snowfall_render;
+    effectBp.unk_00 = 0;
+    effectBp.unk_14 = 0;
+    effectBp.effectID = EFFECT_SNOWFALL;
+
+    effect = shim_create_effect_instance(&effectBp);
+    effect->numParts = numParts;
+
+    data = effect->data.snowfall = shim_general_heap_malloc(numParts * sizeof(*data));
+    ASSERT(data != NULL);
+
+    data->timeLeft = 100;
+    data->lifeTime = 0;
+    data->unk_28 = 255;
+    data->unk_04 = arg0;
+    data->unk_2C = arg1;
+    data->unk_00 = 1;
+
+    data++;
+    for (i = 0; i < arg1; i++, data++) {
+        func_E008A000(data);
+        data->unk_30 = 0;
+    }
+
+    return effect;
+}
+
+void snowfall_init(EffectInstance* effect) {
 }
 
 INCLUDE_ASM(s32, "effects/snowfall", func_E008A290);
