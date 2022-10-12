683ebe37de18c4bf227cdef506b5982eb27b16b6
Ethan Roseman <ethteck@gmail.com>
2022-10-12 15:30:03 +0900

    progz

--- src/battle/partner/goombario.c
+++ src/battle/partner/goombario.c
@@ -74,7 +74,180 @@ ApiStatus func_802380E4_6F11C4(Evt* script, s32 isInitialCall) {
     return ApiStatus_DONE2;
 }
 
-INCLUDE_ASM(s32, "battle/partner/goombario", func_8023817C_6F125C);
+ApiStatus func_8023817C_6F125C(Evt* script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
+    BattleStatus* battleStatus = &gBattleStatus;
+    Actor* actor = battleStatus->partnerActor;
+    ActorState* state = &actor->state;
+    f32 stateCurrentX;
+    f32 stateCurrentZ;
+    f32 stateGoalX;
+    f32 stateGoalZ;
+    f32 oldActorY;
+    f32 oldActorX;
+    f64 currentPosX64;
+    f64 acc1, acc2;
+    f64 acc3, acc4;
+    f64 vel1, vel2;
+    f64 vel3, vel4;
+    f32 temp;
+
+    if (isInitialCall) {
+        actor->state.moveTime = evt_get_variable(script, *args++);
+        actor->state.moveArcAmplitude = evt_get_variable(script, *args++);
+        script->functionTemp[1] = 0;
+        script->functionTemp[0] = 0;
+        if (actor->state.moveArcAmplitude == 1) {
+            script->functionTemp[0] = 2;
+        }
+    }
+
+    if (script->functionTemp[0] == 0) {
+        state->currentPos.x = actor->currentPos.x;
+        state->currentPos.y = actor->currentPos.y;
+        stateGoalX = state->goalPos.x;
+        stateGoalZ = state->goalPos.z;
+        stateCurrentX = state->currentPos.x;
+        stateCurrentZ = actor->currentPos.z;
+        state->currentPos.z = stateCurrentZ;
+        state->angle = atan2(stateCurrentX, stateCurrentZ, stateGoalX, stateGoalZ);
+        state->distance = dist2D(stateCurrentX, stateCurrentZ, stateGoalX, stateGoalZ);
+        if (state->moveTime == 0) {
+            state->moveTime = state->distance / state->speed;
+            temp = state->distance - (state->moveTime * state->speed);
+        } else {
+            state->speed = state->distance / state->moveTime;
+            temp = state->distance - (state->moveTime * state->speed);
+        }
+
+        if (state->moveTime == 0) {
+            return ApiStatus_DONE2;
+        }
+
+        state->unk_30.x = (state->goalPos.x - state->currentPos.x) / state->moveTime;
+        state->unk_30.y = (state->goalPos.y - state->currentPos.y) / state->moveTime;
+        state->unk_30.z = (state->goalPos.z - state->currentPos.z) / state->moveTime;
+        state->acceleration = PI_S / state->moveTime;
+        state->velocity = 0.0f;
+        state->speed += temp / state->moveTime;
+
+        if (state->moveArcAmplitude < 3) {
+            state->unk_24 = 90.0f;
+            state->unk_28 = 360 / state->moveTime;
+            temp = state->distance;
+            temp -= 20.0;
+            temp /= 6.0;
+            temp += 47.0;
+            state->bounceDivisor = temp;
+            if (state->moveArcAmplitude == 2) {
+                state->bounceDivisor *= 1.12;
+            }
+            state->unk_18.x = 0.0f;
+            state->unk_18.y = 0.0f;
+            vel3 = state->velocity;
+            acc3 = state->acceleration;
+            state->velocity = vel3 + ((sin_rad(DEG_TO_RAD(state->unk_24)) * 0.53 * acc3) + acc3);
+        } else {
+            state->unk_24 = 90.0f;
+            state->unk_28 = 360 / state->moveTime;
+            temp = state->distance;
+            temp -= 20.0;
+            temp /= 6.0;
+            temp += 47.0;
+            state->bounceDivisor = temp;
+            if (state->moveArcAmplitude == 4) {
+                state->bounceDivisor *= 1.25;
+            }
+            state->unk_18.x = 0.0f;
+            state->unk_18.y = 0.0f;
+            vel4 = state->velocity;
+            acc4 = state->acceleration;
+            state->velocity = vel4 + ((sin_rad(DEG_TO_RAD(state->unk_24)) * 0.8 * acc4) + acc4);
+        }
+        set_animation(ACTOR_PARTNER, 1, state->animJumpRise);
+        script->functionTemp[0] = 1;
+    }
+
+    switch (script->functionTemp[0]) {
+        case 1:
+            if (state->velocity > PI_S / 2) {
+                set_animation(ACTOR_PARTNER, 1, state->animJumpFall);
+            }
+            oldActorX = actor->currentPos.x;
+            oldActorY = actor->currentPos.y;
+            state->currentPos.x += state->unk_30.x;
+            state->currentPos.y = state->currentPos.y + state->unk_30.y;
+            state->currentPos.z = state->currentPos.z + state->unk_30.z;
+            state->unk_18.x = actor->currentPos.y;
+            actor->currentPos.x = state->currentPos.x;
+            actor->currentPos.y = state->currentPos.y + (state->bounceDivisor * sin_rad(state->velocity));
+            actor->currentPos.z = state->currentPos.z;
+            if (state->goalPos.y > actor->currentPos.y && state->moveTime < 3) {
+                actor->currentPos.y = state->goalPos.y;
+            }
+
+            actor->rotation.z = -atan2(oldActorX, -oldActorY, actor->currentPos.x, -actor->currentPos.y);
+            state->unk_18.y = actor->currentPos.y;
+            if (state->moveArcAmplitude < 3) {
+                vel1 = state->velocity;
+                acc1 = state->acceleration;
+                state->velocity = vel1 + ((sin_rad(DEG_TO_RAD(state->unk_24)) * 0.53 * acc1) + acc1);
+            } else {
+                vel2 = state->velocity;
+                acc2 = state->acceleration;
+                state->velocity = vel2 + ((sin_rad(DEG_TO_RAD(state->unk_24)) * 0.8 * acc2) + acc2);
+            }
+
+            state->unk_24 += state->unk_28;
+            state->unk_24 = clamp_angle(state->unk_24);
+            state->moveTime--;
+            if (state->moveTime == 0) {
+                actor->currentPos.y = state->goalPos.y;
+                state->acceleration = 1.8f;
+                state->velocity = -(state->unk_18.x - state->unk_18.y);
+                set_animation(ACTOR_PARTNER, 1, state->animJumpLand);
+                return ApiStatus_DONE1;
+            }
+            break;
+        case 2:
+            if (battleStatus->unk_83 == 0) {
+                return ApiStatus_DONE2;
+            }
+            state->moveTime = 1;
+            state->acceleration = 1.8f;
+            state->unk_24 = 90.0f;
+            state->velocity = -(state->unk_18.x - state->unk_18.y);
+            state->bounceDivisor = fabsf(state->unk_18.x - state->unk_18.y) / 16.5;
+            state->unk_28 = 360 / state->moveTime;
+            state->currentPos.x = actor->currentPos.x;
+            state->currentPos.y = actor->currentPos.y;
+            state->currentPos.z = actor->currentPos.z;
+            script->functionTemp[0] = 3;
+            // fallthrough
+        case 3:
+            currentPosX64 = state->currentPos.x; // required to match
+            state->currentPos.x = currentPosX64 + state->bounceDivisor * sin_rad(DEG_TO_RAD(state->unk_24)) / 33.0;
+            state->currentPos.y -= state->bounceDivisor * sin_rad(DEG_TO_RAD(state->unk_24));
+            state->unk_24 += state->unk_28;
+            state->unk_24 = clamp_angle(state->unk_24);
+            actor->currentPos.x = state->currentPos.x;
+            actor->currentPos.y = state->currentPos.y;
+            actor->currentPos.z = state->currentPos.z;
+
+            if (gBattleStatus.flags1 & BS_FLAGS1_2000) {
+                return ApiStatus_DONE2;
+            }
+
+            state->moveTime--;
+            if (state->moveTime == 0) {
+                return ApiStatus_DONE1;
+            }
+            break;
+    }
+
+    return ApiStatus_BLOCK;
+}
+
 
 ApiStatus N(func_80238A20_6F1B00)(Evt* script, s32 isInitialCall) {
     BattleStatus* battleStatus = &gBattleStatus;
