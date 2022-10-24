e4ae550b196c2f3bef83649ed771734fe2c06a99
Ethan Roseman <ethteck@gmail.com>
2022-10-24 13:00:18 +0900

    more sams

--- src/world/area_sam/sam_11/D3A930.c
+++ src/world/area_sam/sam_11/D3A930.c
@@ -55,7 +55,10 @@ ApiStatus func_80240360_D3A930(Evt* script, s32 isInitialCall) {
 INCLUDE_ASM(s32, "world/area_sam/sam_11/D3A930", func_80240360_D3A930);
 #endif
 
-INCLUDE_ASM(s32, "world/area_sam/sam_11/D3A930", func_8024052C_D3AAFC);
+ApiStatus func_8024052C_D3AAFC(Evt* script, s32 isInitialCall) {
+    gEntityHideMode = 1;
+    return ApiStatus_DONE2;
+}
 
 #define NAMESPACE dup_sam_11
 ApiStatus N(Set80151310_2)(Evt* script, s32 isInitialCall) {
@@ -73,8 +76,54 @@ ApiStatus N(Set80151310_0)(Evt* script, s32 isInitialCall) {
 
 #include "world/common/todo/SetCamera0MoveFlag1.inc.c"
 
-INCLUDE_ASM(s32, "world/area_sam/sam_11/D3A930", func_8024059C_D3AB6C);
+ApiStatus func_8024059C_D3AB6C(Evt* script, s32 isInitialCall) {
+    PlayerStatus* playerStatus = &gPlayerStatus;
+    f32 xComp, zComp;
+    f32 x, y, z;
+    f32 hitDepth;
 
-INCLUDE_ASM(s32, "world/area_sam/sam_11/D3A930", func_8024072C_D3ACFC);
+    if (isInitialCall) {
+        script->functionTemp[1] = 10;
+    }
+    playerStatus->targetYaw = 270.0f;
+    script->functionTemp[1] += 4;
+    xComp = (script->functionTemp[1] / 10) * sin_deg(playerStatus->targetYaw);
+    zComp = (script->functionTemp[1] / 10) * -cos_deg(playerStatus->targetYaw);
+    x = playerStatus->position.x + xComp;
+    y = playerStatus->position.y + playerStatus->colliderHeight * 0.5f;
+    z = playerStatus->position.z + zComp;
+    hitDepth = 500.0f;
 
-INCLUDE_ASM(s32, "world/area_sam/sam_11/D3A930", func_80240754_D3AD24);
+    if (npc_raycast_down_sides(0, &x, &y, &z, &hitDepth)) {
+        if (hitDepth < 100.0f) {
+            playerStatus->position.x = x;
+            playerStatus->position.y = y;
+            playerStatus->position.z = z;
+            return ApiStatus_BLOCK;
+        }
+    }
+    return ApiStatus_DONE2;
+}
+
+ApiStatus func_8024072C_D3ACFC(Evt* script, s32 isInitialCall) {
+    PlayerStatus* playerStatus = &gPlayerStatus;
+
+    if (playerStatus->flags & (PS_FLAGS_INPUT_DISABLED | PS_FLAGS_1000)) {
+        script->varTable[0] = 1;
+    } else {
+        script->varTable[0] = 0;
+    }
+    return ApiStatus_DONE2;
+}
+
+ApiStatus func_80240754_D3AD24(Evt* script, s32 isInitialCall) {
+    PlayerStatus* playerStatus = &gPlayerStatus;
+
+    if (playerStatus->lastGoodPosition.y == 385) {
+        evt_set_variable(script, MapVar(7), 0);
+    }
+    if (playerStatus->lastGoodPosition.y == 150) {
+        evt_set_variable(script, MapVar(7), 1);
+    }
+    return ApiStatus_BLOCK;
+}
--- src/world/area_sam/sam_12/D4BAB0.c
+++ src/world/area_sam/sam_12/D4BAB0.c
@@ -1,9 +1,56 @@
 #include "sam_12.h"
+#include "effects.h"
 
-INCLUDE_ASM(s32, "world/area_sam/sam_12/D4BAB0", func_80240090_D4BAB0);
+ApiStatus func_80240090_D4BAB0(Evt* script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
 
-INCLUDE_ASM(s32, "world/area_sam/sam_12/D4BAB0", func_802400C0_D4BAE0);
+    set_screen_overlay_params_front(0, evt_get_float_variable(script, *args++));
+    return ApiStatus_DONE2;
+}
 
-INCLUDE_ASM(s32, "world/area_sam/sam_12/D4BAB0", func_80240194_D4BBB4);
+ApiStatus func_802400C0_D4BAE0(Evt* script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
+    s32 idx = evt_get_variable(script, *args++);
+    s32 x = evt_get_float_variable(script, *args++);
+    s32 y = evt_get_float_variable(script, *args++);
+    s32 z = evt_get_float_variable(script, *args++);
+    ItemEntity* item = get_item_entity(idx);
 
-INCLUDE_ASM(s32, "world/area_sam/sam_12/D4BAB0", func_80240238_D4BC58);
+    item->position.x = x;
+    item->position.y = y;
+    item->position.z = z;
+    return ApiStatus_DONE2;
+}
+
+ApiStatus func_80240194_D4BBB4(Evt* script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
+    s32 temp_s2 = evt_get_variable(script, *args++);
+    s32 temp_s0_3 = evt_get_variable(script, *args++);
+    s32 t3 = evt_get_variable(script, *args++);
+
+    fx_rising_bubble(0, temp_s2, temp_s0_3, t3, 0);
+    return ApiStatus_DONE2;
+}
+
+ApiStatus func_80240238_D4BC58(Evt* script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
+    f32 temp_f26 = evt_get_float_variable(script, *args++);
+    f32 x = evt_get_float_variable(script, *args++);
+    f32 y = evt_get_float_variable(script, *args++);
+    f32 z = evt_get_float_variable(script, *args++);
+    f32 t = evt_get_float_variable(script, *args++);
+    s32 outVar = *args++;
+    EffectInstance* effect;
+
+    fx_sleep_bubble(
+        0,
+        gPlayerStatus.position.x + x,
+        gPlayerStatus.position.y + ((gPlayerStatus.colliderHeight * 2) / 3) + y,
+        gPlayerStatus.position.z + z,
+        (gPlayerStatus.colliderHeight / 3) + t,
+        temp_f26,
+        &effect
+    );
+    evt_set_variable(script, outVar, effect);
+    return ApiStatus_DONE2;
+}
