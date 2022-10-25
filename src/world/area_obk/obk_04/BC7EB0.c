83a059c51c2fad85ceb26b056a087351d09ee0bb
Ethan Roseman <ethteck@gmail.com>
2022-10-25 15:16:42 +0900

    7 more

--- src/world/area_obk/obk_04/BC7EB0.c
+++ src/world/area_obk/obk_04/BC7EB0.c
@@ -2,11 +2,44 @@
 
 static char* N(exit_str_0) = "obk_03";
 
-INCLUDE_ASM(s32, "world/area_obk/obk_04/BC7EB0", func_80240050_BC7EB0);
+ApiStatus func_80240050_BC7EB0(Evt* script, s32 isInitialCall) {
+    Npc* npc = get_npc_unsafe(script->owner2.npcID);
+    s32* ptr = heap_malloc(4); // todo what is this
 
-INCLUDE_ASM(s32, "world/area_obk/obk_04/BC7EB0", func_802400D0_BC7F30);
+    npc->blur.any = ptr;
+    *ptr = NULL;
+    npc->planarFlyDist = 125.0f;
+    npc->yaw = 0.0f;
+    npc->pos.x = 0.0f;
+    npc->pos.y = -875.0f;
+    npc->pos.z = 0.0f;
+    npc->duration = 60;
+    script->functionTemp[1] = 0;
+    return ApiStatus_DONE2;
+}
 
-INCLUDE_ASM(s32, "world/area_obk/obk_04/BC7EB0", func_80240198_BC7FF8);
+void func_802400D0_BC7F30(Npc* npc) {
+    if (npc->yaw > 340.0f || npc->yaw < 20.0f) {
+        npc->renderMode = RENDER_MODE_ALPHATEST;
+        npc->unk_A2 = 0;
+        func_8003D624(npc, 0, 0, 0, 0, 0, npc->unk_A2);
+    } else {
+        npc->renderMode = RENDER_MODE_SURFACE_XLU_LAYER2;
+        func_8003D624(npc, 7, gPlayerStatusPtr->alpha1, 255, 0, 0, npc->unk_A2);
+        npc->unk_9A = 255;
+    }
+}
+
+void func_80240198_BC7FF8(void) {
+    func_802400D0_BC7F30(get_npc_unsafe(1));
+    func_802400D0_BC7F30(get_npc_unsafe(2));
+    func_802400D0_BC7F30(get_npc_unsafe(3));
+    func_802400D0_BC7F30(get_npc_unsafe(4));
+    func_802400D0_BC7F30(get_npc_unsafe(5));
+    func_802400D0_BC7F30(get_npc_unsafe(6));
+    func_802400D0_BC7F30(get_npc_unsafe(7));
+    func_802400D0_BC7F30(get_npc_unsafe(8));
+}
 
 void func_8024022C_BC808C(void) {
     get_npc_unsafe(1)->unk_A2 = 0;
@@ -19,9 +52,75 @@ void func_8024022C_BC808C(void) {
     get_npc_unsafe(8)->unk_A2 = 0;
 }
 
+s32 func_802402A0_BC8100(s32);
 INCLUDE_ASM(s32, "world/area_obk/obk_04/BC7EB0", func_802402A0_BC8100);
 
-INCLUDE_ASM(s32, "world/area_obk/obk_04/BC7EB0", func_802402F4_BC8154);
+ApiStatus func_802402F4_BC8154(Evt* script, s32 isInitialCall) {
+    Npc* npc = get_npc_unsafe(script->owner2.npcID);
+    s32* temp_s3 = npc->blur.any;
+    s32 temp_v0;
+
+    switch (script->functionTemp[1]) {
+        case 0:
+            temp_v0 = evt_get_variable(script, MapVar(1));
+            if (temp_v0 == 1) {
+                *temp_s3 = temp_v0;
+                script->functionTemp[1] = 10;
+            }
+            break;
+        case 1:
+            break;
+        case 10:
+            npc->yaw = clamp_angle(npc->yaw + 2.0f);
+            npc->duration--;
+            if (npc->duration == 0) {
+                script->functionTemp[1] = 11;
+            }
+            break;
+        case 11:
+            if (npc->pos.y <= -920.0f) {
+                func_80240198_BC7FF8();
+            }
+            npc->yaw = clamp_angle(npc->yaw + 2.0f);
+            npc->pos.y -= 0.5f;
+            if (npc->pos.y <= -988.0f) {
+                evt_set_variable(script, MapVar(2), 1);
+                func_8024022C_BC808C();
+                script->functionTemp[1] = 12;
+            }
+            break;
+        case 12:
+            script->functionTemp[1] = func_802402A0_BC8100(evt_get_variable(script, MapVar(3)));
+            npc->yaw = clamp_angle(npc->yaw + 2.0f);
+            break;
+        case 13:
+            script->functionTemp[1] = func_802402A0_BC8100(evt_get_variable(script, MapVar(3)));
+            npc->yaw = clamp_angle(npc->yaw - 2.0f);
+            break;
+        case 14:
+            evt_set_variable(script, MapVar(3), 0);
+            npc->duration++;
+            if (npc->duration == 50) {
+                script->functionTemp[1] = 15;
+            }
+            npc->planarFlyDist -= 2.0f;
+            npc->yaw = clamp_angle(npc->yaw + 2.0f);
+            break;
+        case 15:
+            npc->duration--;
+            if (npc->duration == 0) {
+                script->functionTemp[1] = 12;
+            }
+            npc->planarFlyDist += 2.0f;
+            npc->yaw = clamp_angle(npc->yaw + 2.0f);
+            break;
+        case 100:
+            script->functionTemp[1] = func_802402A0_BC8100(evt_get_variable(script, MapVar(3)));
+            break;
+    }
+
+    return ApiStatus_DONE2;
+}
 
 ApiStatus func_802405B4_BC8414(Evt* script, s32 isInitialCall) {
     Npc* npc = get_npc_unsafe(script->owner2.npcID);
@@ -35,7 +134,7 @@ ApiStatus func_802405B4_BC8414(Evt* script, s32 isInitialCall) {
 
 INCLUDE_ASM(s32, "world/area_obk/obk_04/BC7EB0", func_80240624_BC8484);
 
-ApiStatus func_80240910_BC8770(void) {
+ApiStatus func_80240910_BC8770(Evt* script, s32 isInitialCall) {
     if (gPlayerStatus.position.y < -50.0f) {
         return ApiStatus_DONE2;
     }
@@ -46,9 +145,67 @@ ApiStatus func_80240910_BC8770(void) {
 
 #include "world/common/todo/GetItemName.inc.c"
 
-INCLUDE_ASM(s32, "world/area_obk/obk_04/BC7EB0", func_80240A58_BC88B8);
+ApiStatus func_80240A58_BC88B8(Evt* script, s32 isInitialCall) {
+    Npc* npc1 = get_npc_unsafe(1);
+    Npc* npc2 = get_npc_unsafe(2);
+    Npc* npc3 = get_npc_unsafe(3);
+    Npc* npc4 = get_npc_unsafe(4);
+    Npc* npc5 = get_npc_unsafe(5);
+    Npc* npc6 = get_npc_unsafe(6);
+    Npc* npc7 = get_npc_unsafe(7);
+    Npc* npc8 = get_npc_unsafe(8);
 
-INCLUDE_ASM(s32, "world/area_obk/obk_04/BC7EB0", func_80240C10_BC8A70);
+    switch (script->varTable[0]) {
+        case 0:
+            script->varTable[0] = npc1->yaw;
+            evt_set_variable(NULL, MapVar(5), 1);
+            break;
+        case 1:
+            script->varTable[0] = npc2->yaw;
+            evt_set_variable(NULL, MapVar(5), 2);
+            break;
+        case 2:
+            script->varTable[0] = npc3->yaw;
+            evt_set_variable(NULL, MapVar(5), 3);
+            break;
+        case 3:
+            script->varTable[0] = npc4->yaw;
+            evt_set_variable(NULL, MapVar(5), 4);
+            break;
+        case 4:
+            script->varTable[0] = npc5->yaw;
+            evt_set_variable(NULL, MapVar(5), 5);
+            break;
+        case 5:
+            script->varTable[0] = npc6->yaw;
+            evt_set_variable(NULL, MapVar(5), 6);
+            break;
+        case 6:
+            script->varTable[0] = npc7->yaw;
+            evt_set_variable(NULL, MapVar(5), 7);
+            break;
+        case 7:
+            script->varTable[0] = npc8->yaw;
+            evt_set_variable(NULL, MapVar(5), 8);
+            break;
+    }
+    script->varTable[0] += script->varTable[1];
+    return ApiStatus_DONE2;
+}
+
+ApiStatus func_80240C10_BC8A70(Evt* script) {
+    Npc npc;
+
+    npc.pos.x = 0;
+    npc.pos.y = 0.0f;
+    npc.pos.z = 0.0f;
+    npc.planarFlyDist = 125.0f;
+    npc_move_heading(&npc, 125.0f, script->varTable[0]);
+    script->varTable[0] = npc.pos.x;
+    script->varTable[1] = npc.pos.y + 10.0f;
+    script->varTable[2] = npc.pos.z + 20.0f;
+    return ApiStatus_DONE2;
+}
 
 ApiStatus func_80240CA0_BC8B00(Evt* script, s32 isInitialCall) {
     Npc* npc = get_npc_unsafe(evt_get_variable(NULL, MapVar(5)));
@@ -64,7 +221,43 @@ ApiStatus func_80240D00_BC8B60(Evt* script, s32 isInitialCall) {
     return ApiStatus_DONE2;
 }
 
-INCLUDE_ASM(s32, "world/area_obk/obk_04/BC7EB0", func_80240D14_BC8B74);
+ApiStatus func_80240D14_BC8B74(Evt* script, s32 isInitialCall) {
+    PlayerStatus* playerStatus = &gPlayerStatus;
+    Npc npc;
+    f32 dist = dist2D(playerStatus->position.x, playerStatus->position.z, 0.0f, 0.0f);
+    f32 yaw;
+    s32 gt, lt;
+
+    if (!(dist > 90.0f)) {
+        gt = FALSE;
+    } else {
+        gt = TRUE;
+    }
+
+    if (!(dist < 150.0f)) {
+        lt = FALSE;
+    } else {
+        lt = TRUE;
+    }
+
+    if ((gt | lt) != 0) {
+        yaw = atan2(playerStatus->position.x, playerStatus->position.z, 0.0f, 0.0f) + 180.0f;
+        npc.pos.x = 0.0f;
+        npc.pos.y = 0.0f;
+        npc.pos.z = 0.0f;
+        npc_move_heading(&npc, 80.0f, yaw);
+        script->varTable[0] = npc.pos.x;
+        script->varTable[1] = npc.pos.y;
+        script->varTable[2] = npc.pos.z;
+        script->varTable[3] = 1;
+    } else {
+        script->varTable[0] = playerStatus->position.x;
+        script->varTable[1] = playerStatus->position.y;
+        script->varTable[2] = playerStatus->position.z;
+        script->varTable[3] = 0;
+    }
+    return ApiStatus_DONE2;
+}
 
 ApiStatus func_80240E58_BC8CB8(Evt* script, s32 isInitialCall) {
     if (get_npc_unsafe(1)->duration == 0) {
