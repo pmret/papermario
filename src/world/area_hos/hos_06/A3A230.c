2d21966c85bf5e97d816d6423df3537ecf67836f
Ethan Roseman <ethteck@gmail.com>
2022-10-27 23:24:03 +0900

    4 more

--- src/world/area_hos/hos_06/A3A230.c
+++ src/world/area_hos/hos_06/A3A230.c
@@ -5,6 +5,8 @@
 
 #include "world/common/todo/AddPlayerHandsOffset.inc.c"
 
+extern u8 D_802447D0_A3DCB0[];
+
 INCLUDE_ASM(s32, "world/area_hos/hos_06/A3A230", func_80240F7C_A3A45C);
 
 INCLUDE_ASM(s32, "world/area_hos/hos_06/A3A230", func_80240FD0_A3A4B0);
@@ -65,11 +67,34 @@ INCLUDE_ASM(s32, "world/area_hos/hos_06/A3A230", func_802414E4_A3A9C4);
 
 INCLUDE_ASM(s32, "world/area_hos/hos_06/A3A230", func_80241668_A3AB48);
 
-INCLUDE_ASM(s32, "world/area_hos/hos_06/A3A230", func_802417EC_A3ACCC);
+ApiStatus func_802417EC_A3ACCC(Evt* script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
+    s32 idx = evt_get_variable(script, *args++);
+    s32 outVar = *args++;
 
-INCLUDE_ASM(s32, "world/area_hos/hos_06/A3A230", func_80241860_A3AD40);
+    if (gPlayerData.coins < D_802447D0_A3DCB0[idx]) {
+        evt_set_variable(script, outVar, TRUE);
+    } else {
+        evt_set_variable(script, outVar, FALSE);
+    }
+    return ApiStatus_DONE2;
+}
 
-INCLUDE_ASM(s32, "world/area_hos/hos_06/A3A230", func_802418A4_A3AD84);
+ApiStatus func_80241860_A3AD40(Evt* script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
+    s32 idx = evt_get_variable(script, *args++);
+
+    gPlayerData.coins -= D_802447D0_A3DCB0[idx];
+    return ApiStatus_DONE2;
+}
+
+ApiStatus func_802418A4_A3AD84(Evt* script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
+    s32 idx = evt_get_variable(script, *args++);
+
+    gPlayerData.coins += D_802447D0_A3DCB0[idx];
+    return ApiStatus_DONE2;
+}
 
 INCLUDE_ASM(s32, "world/area_hos/hos_06/A3A230", func_802418E8_A3ADC8);
 
@@ -131,4 +156,11 @@ ApiStatus func_80241CCC_A3B1AC(Evt* script, s32 isInitialCall) {
     return ApiStatus_BLOCK;
 }
 
-INCLUDE_ASM(s32, "world/area_hos/hos_06/A3A230", func_80241F98_A3B478);
+ApiStatus func_80241F98_A3B478(Evt* script, s32 isInitialCall) {
+    Bytecode* args = script->ptrReadPos;
+    EffectInstance* effect = (EffectInstance*) evt_get_variable(script, *args++);
+
+    // TODO effect may be wrong
+    effect->data.energyOrbWave->unk_1C++;
+    return ApiStatus_DONE2;
+}
