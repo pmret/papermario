46323810da08bd7c1d7c8a7abcdb0c4257294e4f
Ethan Roseman <ethteck@gmail.com>
2022-10-27 23:11:43 +0900

    more hos funcs

--- src/world/area_hos/hos_06/A3A230.c
+++ src/world/area_hos/hos_06/A3A230.c
@@ -1,4 +1,5 @@
 #include "hos_06.h"
+#include "effects.h"
 
 #include "world/common/todo/GetNpcCollisionHeight.inc.c"
 
@@ -76,6 +77,58 @@ INCLUDE_ASM(s32, "world/area_hos/hos_06/A3A230", func_80241A58_A3AF38);
 
 INCLUDE_ASM(s32, "world/area_hos/hos_06/A3A230", func_80241B74_A3B054);
 
-INCLUDE_ASM(s32, "world/area_hos/hos_06/A3A230", func_80241CCC_A3B1AC);
+// TODO may not be motionBlurFlame
+ApiStatus func_80241CCC_A3B1AC(Evt* script, s32 isInitialCall) {
+    EffectInstance* effects[3];
+    Matrix4f sp28, sp68;
+    f32 tx;
+    f32 ty;
+    f32 temp_f24;
+    f32 temp_f28;
+    f32 temp_f30;
+    s32 i;
+
+    if (isInitialCall) {
+        script->functionTemp[0] = 180;
+        script->functionTempPtr[1] = (EffectInstance*) evt_get_variable(script, ArrayVar(3));
+        script->functionTempPtr[2] = (EffectInstance*) evt_get_variable(script, ArrayVar(4));
+        script->functionTempPtr[3] = (EffectInstance*) evt_get_variable(script, ArrayVar(5));
+    }
+
+    effects[0] = script->functionTempPtr[1];
+    effects[1] = script->functionTempPtr[2];
+    effects[2] = script->functionTempPtr[3];
+    temp_f30 = (sin_deg(script->functionTemp[0]) * 10.0f) + 10.0f;
+    temp_f28 = (sin_deg(script->functionTemp[0]) * 25.0f) + 10.0f;
+    temp_f24 = script->functionTemp[0] * 10;
+
+    for (i = 0; i < ARRAY_COUNT(effects); i++) {
+        guRotateF(sp28, -gCameras[gCurrentCameraID].currentYaw, 0.0f, 1.0f, 0.0f);
+        guRotateF(sp68, i * 120, 0.0f, 0.0f, 1.0f);
+        guMtxCatF(sp68, sp28, sp28);
+        tx = temp_f30 * sin_deg(temp_f24);
+        ty = temp_f28 * cos_deg(temp_f24);
+        guTranslateF(sp68, tx, ty, 0.0f);
+        guMtxCatF(sp68, sp28, sp28);
+        effects[i]->data.motionBlurFlame->pos.x = sp28[3][0];
+        effects[i]->data.motionBlurFlame->pos.y = sp28[3][1];
+        effects[i]->data.motionBlurFlame->pos.z = sp28[3][2];
+    }
+
+    script->functionTemp[0]--;
+    if (script->functionTemp[0] < 16) {
+        for (i = 0; i < ARRAY_COUNT(effects); i++) {
+            effects[i]->data.motionBlurFlame->unk_4C = script->functionTemp[0];
+        }
+    }
+
+    if (script->functionTemp[0] < 0) {
+        for (i = 0; i < ARRAY_COUNT(effects); i++) {
+            remove_effect(effects[i]);
+        }
+        return ApiStatus_DONE2;
+    }
+    return ApiStatus_BLOCK;
+}
 
 INCLUDE_ASM(s32, "world/area_hos/hos_06/A3A230", func_80241F98_A3B478);
