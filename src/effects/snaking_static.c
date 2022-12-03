f1fdcf53d28f378176e434cb12bd0a2d2478e2f4
Spencer Vaughn <svaughn57@gmail.com>
2022-12-03 13:15:16 -0600

    snaking_static_update: Matched

--- src/effects/snaking_static.c
+++ src/effects/snaking_static.c
@@ -68,7 +68,77 @@ EffectInstance* snaking_static_main(s32 arg0, f32 posX, f32 posY, f32 posZ, f32 
 void snaking_static_init(EffectInstance* effect) {
 }
 
-INCLUDE_ASM(s32, "effects/snaking_static", snaking_static_update);
+void snaking_static_update(EffectInstance* effect) {
+    SnakingStaticFXData* data = effect->data.snakingStatic;
+    f32 tempOffsetY;
+    f32 tempOffsetX;
+    f32 tempPosX;
+    f32 tempPosY;
+    s32 lifeTime;
+    s32 timeLeft;
+    s32 i;
+
+    if (effect->flags & 16) {
+        effect->flags &= ~16;
+        data->timeLeft = 16;
+    }
+
+    if (data->timeLeft < 1000) {
+        data->timeLeft--;
+    }
+
+    timeLeft = data->timeLeft;
+    data->lifeTime++;
+    lifeTime = data->lifeTime;
+
+    if (timeLeft < 0) {
+        shim_remove_effect(effect);
+        return;
+    }
+    if (timeLeft < 16) {
+        data->unk_24 = timeLeft * 16;
+    }
+    if (lifeTime < 16) {
+        data->unk_24 = (lifeTime * 16) + 0xF;
+    }
+
+    data++;
+    for (i = 1; i < effect->numParts; i += 3, data += 3) {
+        if (data->lifeTime >= data->timeLeft) {
+            data->timeLeft = shim_rand_int(50) + 20;
+            data->lifeTime = 0;
+            data->unk_3C = shim_rand_int(360);
+            data->pos.x = 0.0f;
+            data->pos.y = 0.0f;
+        }
+        if (shim_rand_int(100) < 51) {
+            tempPosX = data->pos.x;
+            tempPosY = data->pos.y;
+            tempOffsetX = shim_rand_int(16) - 8;
+            tempOffsetY = shim_rand_int(16) - 8;
+            data[2] = data[1];
+            data[1] = data[0];
+            data->pos.x = tempPosX + tempOffsetX;
+            data->pos.y = tempPosY + tempOffsetY;
+            data->unk_3C = shim_atan2(0.0f, 0.0f, tempOffsetX, tempOffsetY);
+            if (tempOffsetX != 0.0f || tempOffsetY != 0.0f) {
+                data->unk_34 = shim_sqrtf((tempOffsetX * tempOffsetX) + (tempOffsetY * tempOffsetY));
+            } else {
+                data->unk_34 = 0.0f;
+            }
+        }
+        data->unk_24 = (shim_sin_deg(((data->lifeTime * 180) / data->timeLeft)) * 1024.0f);
+        if (data->unk_24 >= 256) {
+            data->unk_24 = 255;
+        }
+        data->lifeTime++;
+    }
+    data = effect->data.snakingStatic;
+    data++;
+    for (i = 1; i < effect->numParts; i++, data++) {
+        data->unk_40 = shim_rand_int(15);
+    }
+}
 
 void snaking_static_render(EffectInstance* effect) {
     RenderTask renderTask;
