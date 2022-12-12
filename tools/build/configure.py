54249aadd218b22c981a37b46860e3825e193101
Ethan Roseman <ethteck@gmail.com>
2022-12-12 10:23:01 +0900

    new splat segment

--- tools/build/configure.py
+++ tools/build/configure.py
@@ -240,8 +240,9 @@ class Configure:
 
         modes = ["ld"]
         if assets:
-            modes.extend(["bin", "yay0", "img", "vtx", "gfx", "pm_map_data", "pm_msg", "pm_npc_sprites", "pm_charset",
-                          "pm_charset_palettes", "pm_effect_loads", "pm_effect_shims", "pm_sprite_shading_profiles"])
+            modes.extend(["bin", "yay0", "img", "vtx", "gfx", "gfx_common", "pm_map_data", "pm_msg", "pm_npc_sprites",
+                          "pm_charset","pm_charset_palettes", "pm_effect_loads", "pm_effect_shims",
+                          "pm_sprite_shading_profiles"])
         if code:
             modes.extend(["code", "c", "data", "rodata"])
 
