cf83d8c6cc8225b59e4f4ed8b42aed017eac9415
Ethan Roseman <ethteck@gmail.com>
2022-12-12 10:26:06 +0900

    fix custom segment

--- tools/splat_ext/gfx_common.py
+++ tools/splat_ext/gfx_common.py
@@ -2,4 +2,4 @@ from segtypes.n64.gfx import N64SegGfx
 
 class N64SegGfx_common(N64SegGfx):
     def format_sym_name(self, sym):
-        return f"N({sym.name[6:]})"
+        return f"N({sym.name[7:]})"
