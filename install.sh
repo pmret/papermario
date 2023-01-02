2fa9cab8497cf050583d1f296dd2f9da8128ec26
Ethan Roseman <ethteck@gmail.com>
2023-01-02 16:41:32 +0900

    Fix ido recomp link

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ if [[ "$uname" == "Darwin" ]]; then
     curl -L "https://github.com/decompals/mips-binutils-2.6/releases/download/main/binutils-2.6-mac.tar.gz" | tar zx -C tools/build/cc/gcc2.7.2
 
     echo "Downloading IDO 5.3 for macOS"
-    curl -L "https://github.com/ethteck/ido-static-recomp/releases/download/per-function/ido-5.3-recomp-macos-latest.tar.gz" | tar zx -C tools/build/cc/ido5.3
+    curl -L "https://github.com/decompals/ido-static-recomp/releases/download/v0.2/ido-5.3-recomp-macos-latest.tar.gz" | tar zx -C tools/build/cc/ido5.3
 
     echo "Installing packages for macOS"
 
@@ -206,4 +206,4 @@ curl -L "https://github.com/decompals/mips-gcc-2.7.2/releases/download/main/gcc-
 curl -L "https://github.com/decompals/mips-binutils-2.6/releases/download/main/binutils-2.6-linux.tar.gz" | tar zx -C tools/build/cc/gcc2.7.2
 
 echo "Downloading IDO 5.3 for Linux"
-curl -L "https://github.com/ethteck/ido-static-recomp/releases/download/per-function/ido-5.3-recomp-ubuntu-latest.tar.gz" | tar zx -C tools/build/cc/ido5.3
+curl -L "https://github.com/decompals/ido-static-recomp/releases/download/v0.2/ido-5.3-recomp-ubuntu-latest.tar.gz" | tar zx -C tools/build/cc/ido5.3
