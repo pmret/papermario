69187fe3e7c0835aab3f37f928da5ea08f4d0272
Unnunu <serge_zorkin@mail.ru>
2023-01-02 13:11:08 +0300

    5 sprite funcs

--- src/101b90_len_8f0.c
+++ src/101b90_len_8f0.c
@@ -64,80 +64,82 @@ void spr_swizzle_anim_offsets(s32 arg0, s32 base, void* spriteData) {
     }
 }
 
-#ifdef NON_EQUIVALENT
 SpriteAnimData* spr_load_sprite(s32 idx, s32 arg1, s32 arg2) {
     SpriteAnimData* animData;
     s32 base;
     s32 i;
-
-    s32 temp_s0;
-    s32 temp_s0_2;
-    s32 temp_v0_2;
-    s32 temp_v0_3;
-    s32* temp_a1_3;
-    s32** var_s2;
-    u16** var_v1;
-    s32* temp_v0;
+    s32 compressedSize;
+    s32* ptr1;
+    s32* ptr2;
+    s32* ptr3;
+    s32 count;
+    s32** data;
+    s32** data2;
 
     if (arg1) {
         base = D_802E0C64;
     } else {
         base = D_802E0C68;
     }
+    // load sprite header
     nuPiReadRom(base + (idx * 4), &D_802DFEB0, 8U);
 
-    temp_s0 = ALIGN8(D_802DFEB0[1] - D_802DFEB0[0]);
-    temp_v0 = general_heap_malloc(temp_s0);
-    nuPiReadRom(base + D_802DFEB0[0], temp_v0, temp_s0);
+    compressedSize = ALIGN8(D_802DFEB0[1] - D_802DFEB0[0]); // rom start, rom end
+    data = general_heap_malloc(compressedSize);
+    nuPiReadRom(base + D_802DFEB0[0], data, compressedSize);
 
+    ptr1 = data;
+    ptr1++;
     if (arg2) {
-        temp_v0++;
-        animData = _heap_malloc_tail(&gSpriteHeapPtr, *temp_v0);
+        animData = _heap_malloc_tail(&gSpriteHeapPtr, *ptr1);
     } else {
-        temp_v0++;
-        animData = _heap_malloc(&gSpriteHeapPtr, *temp_v0);
+        animData = _heap_malloc(&gSpriteHeapPtr, *ptr1);
     }
+    decode_yay0(data, animData);
+    general_heap_free(data);
 
-    decode_yay0(temp_v0, animData);
-    general_heap_free(temp_v0);
+    data = animData->rastersOffset;
+    data = SPR_SWIZZLE(ALIGN4(animData), data);
+    animData->rastersOffset = data;
 
-    temp_v0_2 = ((u32) animData / 4) * 4;
-    var_s2 = animData->rastersOffset + temp_v0_2;
-    animData->rastersOffset = var_s2;
+    while (1) {
+        ptr1 = *data;
+        if (ptr1 == PTR_LIST_END) {
+            break;
+        }
+        ptr1 = SPR_SWIZZLE(ALIGN4(animData), ptr1);
+        *data++ = ptr1;
+        ptr2 = *ptr1;
 
-    while (*var_s2 != PTR_LIST_END) {
-        temp_a1_3 = *var_s2 + temp_v0_2;
-        *var_s2 = temp_a1_3;
-        var_s2++;
         if (!arg1) {
-            *temp_a1_3 += temp_v0_2;
+            ptr2 = SPR_SWIZZLE(ALIGN4(animData), ptr2);
+            *ptr1 = ptr2;
         }
     }
 
     if (arg1) {
-        D_802E0050[idx] = D_802E004C;
-        temp_s0_2 = D_802E0C20[idx + 1] - D_802E0C20[idx];
-        nuPiReadRom(D_802E0C60[0] + D_802E0C14[0] + D_802E0C20[idx], &D_802DFEB8, 0x194);
-        for (i = 0; i < temp_s0_2; i++) {
-            D_802E0090[D_802E004C++] = D_802DFEB8[i];
+        PlayerRasterBufferSetOffsets[idx] = PlayerRasterSetsLoaded;
+        count = D_802E0C20[idx + 1] - D_802E0C20[idx];
+        nuPiReadRom(SpriteDataHeader[0] + D_802E0C14[0] + sizeof(u32) * D_802E0C20[idx], D_802DFEB8, sizeof(D_802DFEB8));
+        for (i = 0; i < count; i++) {
+            D_802E0090[PlayerRasterSetsLoaded++] = D_802DFEB8[i];
         }
     }
 
-    temp_v0_3 = ((u32) animData / 4) * 4;
-    var_v1 = animData->palettesOffset + temp_v0_3;
-    animData->palettesOffset = var_v1;
-
-    while (*var_v1 != -1) {
-        *var_v1 += temp_v0_3;
-        var_v1++;
+    data2 = SPR_SWIZZLE(ALIGN4(animData), animData->palettesOffset);
+    animData->palettesOffset = data2;
+    while (1) {
+        ptr1 = *data2;
+        if (ptr1 == PTR_LIST_END) {
+            break;
+        }
+        ptr1 = SPR_SWIZZLE(ALIGN4(animData), ptr1);
+        *data2++ = ptr1;
     }
 
     spr_swizzle_anim_offsets(0, 0, animData);
     return animData;
 }
-#else
-INCLUDE_ASM(s32, "101b90_len_8f0", spr_load_sprite);
-#endif
 
 void spr_init_player_raster_cache(s32 cacheSize, s32 maxRasterSize) {
     void* raster;
@@ -209,13 +211,136 @@ void spr_update_player_raster_cache(void) {
     }
 }
 
-void spr_load_npc_extra_anims(SpriteAnimData*, u32*);
-INCLUDE_ASM(s32, "101b90_len_8f0", spr_load_npc_extra_anims);
+void spr_load_npc_extra_anims(SpriteAnimData* header, u32* extraAnimList) {
+    u8* src;
+    s32 imgID;
+    s32 rasterSize;
+    s32 remaining;
+    u16 animCmd;
+    u16* cmdList;
+    u32 extraAnimID;
+    u8* dataPos;
+    void* copyEnd;
+    SpriteAnimComponent* comp;
+    SpriteAnimComponent** compList;
+    void* writePos;
+    SpriteRasterCacheEntry** rasterList;
+    SpriteRasterCacheEntry* raster;
+    u16** oldPalList;
+    u16* palette;
+    // one extra required for 'done' sentinel
+    s32 sawRaster[0x64 + 1];
+    s32 i;
+
+    for (i = 0; i < ARRAY_COUNT(sawRaster) - 1; i++) {
+        sawRaster[i] = FALSE;
+    }
+
+    while ((extraAnimID = *extraAnimList++) != -1) {
+        compList = header->animListStart[extraAnimID & 0xFF]; // TODO WROGN
+        while ((comp = *compList++) != PTR_LIST_END) {
+            cmdList = comp->cmdList;
+            remaining = (s16) comp->cmdListSize / 2;
+            while (remaining > 0) {
+                animCmd = *cmdList++;
+                remaining--;
+                switch (animCmd & 0xF000) {
+                    case 0x1000:
+                        i = animCmd; // required to match
+                        imgID = i & 0xFF;
+                        if (imgID < ARRAY_COUNT(sawRaster) - 1) {
+                            sawRaster[imgID] = TRUE;
+                        }
+                        break;
+                    case 0x3000:
+                        cmdList++;
+                        remaining--;
+                        // fallthrough
+                    case 0x4000:
+                        cmdList++;
+                        remaining--;
+                        // fallthrough
+                    case 0x7000:
+                    case 0x5000:
+                        cmdList++;
+                        remaining--;
+                        // fallthrough
+                    case 0x0000:
+                    case 0x2000:
+                    case 0x6000:
+                    case 0x8000:
+                        break;
+                }
+            }
+        }
+    }
+
+    rasterList = header->rastersOffset;
+    raster = *rasterList;
+    dataPos = raster->image;
+
+    for (i = 0; i < ARRAY_COUNT(sawRaster) - 1; i++) {
+        if ((raster = *rasterList) == PTR_LIST_END) {
+            break;
+        }
+        if (sawRaster[i]) {
+            src = raster->image;
+            rasterSize = (raster->width * raster->height) / 2;
+            copyEnd = &dataPos[rasterSize];
+            rasterSize += 8;
+            if (dataPos != src) {
+                raster->image = dataPos;
+                bcopy(src, dataPos, rasterSize);
+            }
+            *rasterList = copyEnd;
+            dataPos += rasterSize;
+        }
+        rasterList++;
+    }
+    // sentinel value to mark end of valid data
+    sawRaster[i] = TRUE;
+
+    writePos = dataPos;
+
+    // copy raster list
+    rasterList = header->rastersOffset;
+    header->rastersOffset = writePos;
+
+    for (i = 0; i < ARRAY_COUNT(sawRaster) - 1; i++) {
+        raster = *rasterList++;
+        if (sawRaster[i]) {
+            *(SpriteRasterCacheEntry**) writePos = raster;
+        } else {
+            *(SpriteRasterCacheEntry**) writePos = (SpriteRasterCacheEntry*) copyEnd;
+        }
+        writePos += 4;
+        if (raster == PTR_LIST_END) {
+            break;
+        }
+    }
+
+    // copy palette list
+    oldPalList = header->palettesOffset;
+    header->palettesOffset = writePos;
+
+    for (i = 0; i < ARRAY_COUNT(sawRaster) - 1; i++) {
+        raster = *oldPalList++;
+        *(u16**) writePos = raster;
+        writePos += 4;
+        if (raster == PTR_LIST_END) {
+            break;
+        }
+    }
+
+    _heap_realloc(&gSpriteHeapPtr, header, (s32)writePos - (s32)header);
+}
+
 
 #ifdef NON_MATCHING
 SpriteComponent** spr_allocate_components(s32 count) {
     s32 listSize;
     void** heap;
+    SpriteComponent **new_var;
     SpriteComponent** listStart;
     SpriteComponent* component;
     SpriteComponent** listPos;
@@ -228,19 +353,18 @@ SpriteComponent** spr_allocate_components(s32 count) {
     totalSize = (count * sizeof(SpriteComponent)) + listSize;
 
     if (spr_allocateBtlComponentsOnWorldHeap) {
-        heap = _heap_malloc(&heap_generalHead, totalSize);
+        listStart = _heap_malloc(&heap_generalHead, totalSize);
     } else {
-        heap = _heap_malloc(&gSpriteHeapPtr, totalSize);
+        listStart = _heap_malloc(&gSpriteHeapPtr, totalSize);
     }
 
-    listStart = (SpriteComponent**) heap;
-    listPos = listStart;
-    component = (SpriteComponent*)(heap + (listSize / 4));
+    component = listPos = listStart;
+
+    component = (SpriteComponent*)((s32)(component) + (listSize / 4) * 4);
 
     // fill list values
     for (i = 0; i < count; i++) {
-        *listPos = component;
-        listPos++;
+        *listPos++ = component;
         component++;
     }
     *listPos = PTR_LIST_END;
