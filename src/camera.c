11030b852151a0361039a2265df6c3e73d25d21d
Unnunu <serge_zorkin@mail.ru>
2023-02-26 10:35:16 +0300

    matched two functions

--- src/camera.c
+++ src/camera.c
@@ -19,11 +19,205 @@ CameraControlSettings* test_ray_zone(f32 posX, f32 posY, f32 posZ, Collider** zo
     }
 }
 
-s32 func_800322DC(f32, f32, f32, f32, f32, f32, f32, f32, f32*, f32*, f32*);
-INCLUDE_ASM(s32, "camera", func_800322DC);
+s32 calculate_segment_intersection(f32 x1, f32 z1, f32 x2, f32 z2, f32 x3, f32 z3, f32 x4, f32 z4, f32* interX, f32* interZ, f32* squared_dist) {
+    f32 dx14;
+    f32 dx13;
+    f32 dz;
+    f32 dz12;
+    f32 dx34;
+    f32 dx12;
+    f32 dx;
+    f32 alpha;
+    f32 minus_dz12;
+    f32 p4_side;
+    f32 x;
+    f32 p3_side;
+    f32 z;
+    f32 dx12_copy;
+    f32 dz13;
+    f32 dz14;
+    f32 dz34;
+    f32 p3_side_;
 
-s32 func_800325E4(f32, f32, f32, f32, f32, f32, f32, f32, f32*, f32*, f32*);
-INCLUDE_ASM(s32, "camera", func_800325E4);
+    dx12_copy = x2 - x1;
+    dx12 = dx12_copy;
+    dz12 = z2 - z1;
+    dx13 = x3 - x1;
+    dz13 = z3 - z1;
+    dx14 = x4 - x1;
+    dz14 = z4 - z1;
+    minus_dz12 = -dz12;
+    dx34 = x4 - x3;
+    dz34 = z4 - z3;
+
+    // if distance between points on the line is 0
+    if (dx12_copy == 0.0f && dz12 == 0.0f) {
+        return FALSE;
+    }
+    // if length of second segment is 0
+    if (dx34 == 0.0f && dz34 == 0.0f) {
+        return FALSE;
+    }
+
+    p3_side_ = minus_dz12 * dx13 + dx12_copy * dz13;
+    if (p3_side_ < 0.0f) {
+        p3_side = -1.0f;
+    } else if (p3_side_ > 0.0f) {
+        p3_side = 1.0f;
+    } else {
+        p3_side = 0.0f;
+    }
+    p3_side_ = p3_side;
+
+    dx = minus_dz12 * dx14 + dx12_copy * dz14;
+    if (dx < 0.0f) {
+        p4_side = -1.0f;
+    } else if (dx > 0.0f) {
+        p4_side = 1.0f;
+    } else {
+        p4_side = 0.0f;
+    }
+
+    // P3 and P4 are on the same side relative to the line: no intersection
+    if (p3_side_ == p4_side) {
+        return FALSE;
+    }
+
+    if (fabsf(dx12) > fabsf(dx34)) {
+        /*
+        We represent intersection point P as P = P3 + alpha * (P4 - P3)
+        and solve the equation (P - P1) x (P2 - P1) = 0, where 'x' is cross product, P1 and P2 are points on the line
+        and P3 and P4 are the ends of the segment.
+        So, (P3 - P1 + alpha * (P4 - P3)) x (P2 - P1) = 0,
+        alpha = [-(P3 - P1) x (P2 - P1)] / [(P4 - P3) x (P2 - P1)]
+        */
+        // same as -(P3 - P1) x (P2 - P1)
+        alpha = x1 * dz12 - z1 * dx12 + dx12 * z3 - dz12 * x3;
+        // divide by (P4 - P3) x (P2 - P1)
+        alpha /= dz12 * dx34 - dx12 * dz34;
+        /*
+            Now we represent P as P = P1 + beta * (P2 - P1), and we are to find beta.
+            P3 + alpha * (P4 - P3) = P1 + beta * (P2 - P1)
+            beta * (P2 - P1) = P3 + alpha * (P4 - P3) - P1
+            We use only 'x' part of this equation to find beta.
+
+            Actually this step could be omitted and we calculate intersection directly as P3 + alpha * (P4 - P3).
+            Don't know why it's done this way.
+        */
+        alpha = (x3 + dx34 * alpha - x1) / dx12;
+        x = x1 + dx12 * alpha;
+        z = z1 + dz12 * alpha;
+    } else {
+        /*
+        We represent intersection point P as P = P1 + alpha * (P2 - P1)
+        and solve the equation (P4 - P3) x (P - P3) = 0
+        */
+        // same as (P4 - P3) x (P3 - P1)
+        alpha = z3 * dx34 + x1 * dz34 - z3 * dz34 - z1 * dx34;
+        // divide by (P4 - P3) x (P2 - P1)
+        alpha /= dz12 * dx34 - dx12 * dz34;
+        // Now we represent P as P = P3 + beta * (P4 - P3) and find beta
+        alpha = (x1 + dx12 * alpha - x3) / dx34;
+        x = x3 + dx34 * alpha;
+        z = z3 + dz34 * alpha;
+    }
+    // (P - P1) * (P - P2) > 0 when P is outside of segment P1-P2
+    if ((x - x1) * (x - x2) + (z - z1) * (z - z2) > 0.0f) {
+        return FALSE;
+    }
+
+    dx = x - x3;
+    dz = z - z3;
+    *interX = x;
+    *interZ = z;
+    // distance between P and P3
+    *squared_dist = SQ(dx) + SQ(dz);
+    return TRUE;
+}
+
+s32 calculate_line_segment_intersection(f32 x1, f32 z1, f32 x2, f32 z2, f32 x3, f32 z3, f32 x4, f32 z4, f32* interX, f32* interZ, f32* squared_dist) {
+    f32 dx14;
+    f32 dx13;
+    f32 dz;
+    f32 dz12;
+    f32 dx34;
+    f32 dx12;
+    f32 dx;
+    f32 alpha;
+    f32 minus_dz12;
+    f32 p4_side;
+    f32 x;
+    f32 p3_side;
+    f32 z;
+    f32 dx12_copy;
+    f32 dz13;
+    f32 dz14;
+    f32 dz34;
+    f32 p3_side_;
+
+    dx12_copy = x2 - x1;
+    dx12 = dx12_copy;
+    dz12 = z2 - z1;
+    dx13 = x3 - x1;
+    dz13 = z3 - z1;
+    dx14 = x4 - x1;
+    dz14 = z4 - z1;
+    minus_dz12 = -dz12;
+    dx34 = x4 - x3;
+    dz34 = z4 - z3;
+
+    if (dx12_copy == 0.0f && dz12 == 0.0f) {
+        return FALSE;
+    }
+    if (dx34 == 0.0f && dz34 == 0.0f) {
+        return FALSE;
+    }
+
+    p3_side_ = minus_dz12 * dx13 + dx12_copy * dz13;
+    if (p3_side_ < 0.0f) {
+        p3_side = -1.0f;
+    } else if (p3_side_ > 0.0f) {
+        p3_side = 1.0f;
+    } else {
+        p3_side = 0.0f;
+    }
+    p3_side_ = p3_side;
+
+    dx = minus_dz12 * dx14 + dx12_copy * dz14;
+    if (dx < 0.0f) {
+        p4_side = -1.0f;
+    } else if (dx > 0.0f) {
+        p4_side = 1.0f;
+    } else {
+        p4_side = 0.0f;
+    }
+
+    if (p3_side_ == p4_side) {
+        return FALSE;
+    }
+
+    if (fabsf(dx12) > fabsf(dx34)) {
+        alpha = x1 * dz12 - z1 * dx12 + dx12 * z3 - dz12 * x3;
+        alpha /= dz12 * dx34 - dx12 * dz34;
+        alpha = (x3 + dx34 * alpha - x1) / dx12;
+        x = x1 + dx12 * alpha;
+        z = z1 + dz12 * alpha;
+    } else {
+        alpha = z3 * dx34 + x1 * dz34 - z3 * dz34 - z1 * dx34;
+        alpha /= dz12 * dx34 - dx12 * dz34;
+        alpha = (x1 + dx12 * alpha - x3) / dx34;
+        x = x3 + dx34 * alpha;
+        z = z3 + dz34 * alpha;
+    }
+
+    dx = x - x3;
+    dz = z - z3;
+    *interX = x;
+    *interZ = z;
+    *squared_dist = SQ(dx) + SQ(dz);
+    return TRUE;
+}
+
 
 s32 func_800328A4(CameraControlSettings* camSettings, f32 x, f32 z) {
     f32 product1, product2;
@@ -243,12 +437,12 @@ void func_80032C64(Camera* camera) {
                 deltaPosX = settings2->posB.x - settings2->posA.x;
                 deltaPosZ = settings2->posB.z - settings2->posA.z;
 
-                if (func_800325E4(settings2->posA.x, settings2->posA.z, settings2->posA.x - deltaPosZ, settings2->posA.z + deltaPosX,
+                if (calculate_line_segment_intersection(settings2->posA.x, settings2->posA.z, settings2->posA.x - deltaPosZ, settings2->posA.z + deltaPosX,
                                   camera->targetPos.x, camera->targetPos.z, newPosX, newPosZ, &sp44, &sp48, &sp4C) && sp4C < 1000000.0f) {
                     dist = sp4C;
                 }
                 do {
-                    if (func_800325E4(settings2->posB.x, settings2->posB.z, settings2->posB.x - deltaPosZ, settings2->posB.z + deltaPosX,
+                    if (calculate_line_segment_intersection(settings2->posB.x, settings2->posB.z, settings2->posB.x - deltaPosZ, settings2->posB.z + deltaPosX,
                                   camera->targetPos.x, camera->targetPos.z, newPosX, newPosZ, &sp44, &sp48, &sp4C) && sp4C < dist) {
                         dist = sp4C;
                     }
@@ -256,17 +450,17 @@ void func_80032C64(Camera* camera) {
             }
             if (cond) {
                 for (i = 0; i < zone->numTriangles; i++) {
-                    if (func_800322DC(zone->triangleTable[i].v1->x, zone->triangleTable[i].v1->z,
+                    if (calculate_segment_intersection(zone->triangleTable[i].v1->x, zone->triangleTable[i].v1->z,
                                       zone->triangleTable[i].v2->x, zone->triangleTable[i].v2->z,
                                       camera->targetPos.x, camera->targetPos.z, newPosX, newPosZ, &sp44, &sp48, &sp4C) && sp4C < dist) {
                         dist = sp4C;
                     }
-                    if (func_800322DC(zone->triangleTable[i].v2->x, zone->triangleTable[i].v2->z,
+                    if (calculate_segment_intersection(zone->triangleTable[i].v2->x, zone->triangleTable[i].v2->z,
                                       zone->triangleTable[i].v3->x, zone->triangleTable[i].v3->z,
                                       camera->targetPos.x, camera->targetPos.z, newPosX, newPosZ, &sp44, &sp48, &sp4C) && sp4C < dist) {
                         dist = sp4C;
                     }
-                    if (func_800322DC(zone->triangleTable[i].v3->x, zone->triangleTable[i].v3->z,
+                    if (calculate_segment_intersection(zone->triangleTable[i].v3->x, zone->triangleTable[i].v3->z,
                                       zone->triangleTable[i].v1->x, zone->triangleTable[i].v1->z,
                                       camera->targetPos.x, camera->targetPos.z, newPosX, newPosZ, &sp44, &sp48, &sp4C) && sp4C < dist) {
                         dist = sp4C;
