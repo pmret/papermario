1a5560b142780b2b3e689b2b53a653c0fbe0c904
Unnunu <serge_zorkin@mail.ru>
2023-03-16 16:08:17 +0300

    fix

--- src/battle/common/actor/pokey.inc.c
+++ src/battle/common/actor/pokey.inc.c
@@ -1274,7 +1274,7 @@ EvtScript N(doSpinSmashHit) = {
         EVT_WAIT(4)
         EVT_EXEC_WAIT(N(bounceOnHit))
     EVT_END_CHILD_THREAD
-    EVT_CALL(EnemyCreateTargetList, 0x00018004)
+    EVT_CALL(EnemyCreateTargetList, TARGET_FLAG_4 | TARGET_FLAG_8000 | TARGET_FLAG_10000)
     EVT_CALL(InitTargetIterator)
     EVT_LABEL(0)
     EVT_CALL(GetOwnerID, LVar1)
