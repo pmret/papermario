0dbcf7dc5f40429d9f1c2a64784b20709a8e61dd
Ethan Roseman <ethteck@gmail.com>
2023-04-27 15:32:19 +0900

    separate compiler installation into separate script

--- Dockerfile
+++ Dockerfile
@@ -5,8 +5,9 @@ ENV DEBIAN_FRONTEND=noninteractive
 COPY requirements.txt /
 COPY requirements_extra.txt /
 COPY install.sh /
+COPY install_compilers.sh /
 
-RUN apt-get update && ./install.sh --extra
+RUN apt-get update && ./install.sh --extra && ./install_compilers.sh
 
 RUN mkdir /papermario
 WORKDIR /papermario
--- install.sh
+++ install.sh
@@ -12,17 +12,6 @@ unset supported
 if [[ "$uname" == "Darwin" ]]; then
     supported=true
 
-    echo "Downloading gcc/binutils for macOS"
-    curl -L "https://github.com/pmret/gcc-papermario/releases/download/master/mac.tar.gz" | tar zx -C tools/build/cc/gcc
-    curl -L "https://github.com/pmret/binutils-papermario/releases/download/master/mac.tar.gz" | tar zx -C tools/build/cc/GCC
-
-    # 2.7.2
-    curl -L "https://github.com/decompals/mips-gcc-2.7.2/releases/download/main/gcc-2.7.2-mac.tar.gz" | tar zx -C tools/build/cc/gcc2.7.2
-    curl -L "https://github.com/decompals/mips-binutils-2.6/releases/download/main/binutils-2.6-mac.tar.gz" | tar zx -C tools/build/cc/gcc2.7.2
-
-    echo "Downloading IDO 5.3 for macOS"
-    curl -L "https://github.com/decompals/ido-static-recomp/releases/download/v0.2/ido-5.3-recomp-macos-latest.tar.gz" | tar zx -C tools/build/cc/ido5.3
-
     echo "Installing packages for macOS"
 
     if ! command -v brew >/dev/null 2>&1; then
@@ -201,14 +190,3 @@ if [ "$supported" != true ]; then
     echo "Please consider contributing and adding an installation script for your distro."
     exit 1
 fi
-
-echo "Downloading gcc/binutils for Linux"
-curl -L "https://github.com/pmret/gcc-papermario/releases/download/master/linux.tar.gz" | tar zx -C tools/build/cc/gcc
-curl -L "https://github.com/pmret/binutils-papermario/releases/download/master/linux.tar.gz" | tar zx -C tools/build/cc/gcc
-
-# 2.7.2
-curl -L "https://github.com/decompals/mips-gcc-2.7.2/releases/download/main/gcc-2.7.2-linux.tar.gz" | tar zx -C tools/build/cc/gcc2.7.2
-curl -L "https://github.com/decompals/mips-binutils-2.6/releases/download/main/binutils-2.6-linux.tar.gz" | tar zx -C tools/build/cc/gcc2.7.2
-
-echo "Downloading IDO 5.3 for Linux"
-curl -L "https://github.com/decompals/ido-static-recomp/releases/download/v0.2/ido-5.3-recomp-ubuntu-latest.tar.gz" | tar zx -C tools/build/cc/ido5.3
