721772bcf26461152041ebed87d2ca46713ccb99
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2023-05-03 21:12:21 +0200

    Update shebangs

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [[ "$UID" -eq "0" ]]; then
     SUDO=""
--- tools/build/sprite/sprite.py
+++ tools/build/sprite/sprite.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3
+#!/usr/bin/env python3
 
 from math import floor, ceil
 from sys import argv, path
