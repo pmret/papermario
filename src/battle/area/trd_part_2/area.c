a238d9873efaf7f2b9b365b910aa7b6ebca26b34
HailSanta <Hail2Santa@gmail.com>
2023-05-22 18:08:43 -0400

    areas use headers

--- src/battle/area/trd_part_2/area.c
+++ src/battle/area/trd_part_2/area.c
@@ -1,53 +1,52 @@
 #include "common.h"
 #include "battle/battle.h"
 #include "script_api/battle.h"
+#include "area.h"
 
-#define NAMESPACE b_area_trd_part_2
+extern ActorBlueprint A(green_ninja_koopa);
+extern ActorBlueprint A(yellow_ninja_koopa);
+extern ActorBlueprint A(black_ninja_koopa);
+extern ActorBlueprint A(red_ninja_koopa);
+extern ActorBlueprint A(fake_bowser);
 
-extern ActorBlueprint N(green_ninja_koopa);
-extern ActorBlueprint N(yellow_ninja_koopa);
-extern ActorBlueprint N(black_ninja_koopa);
-extern ActorBlueprint N(red_ninja_koopa);
-extern ActorBlueprint N(fake_bowser);
+extern Stage A(trd_00);
+extern Stage A(trd_01);
+extern Stage A(trd_02);
+extern Stage A(trd_02b);
+extern Stage A(trd_03);
+extern Stage A(trd_04);
+extern Stage A(trd_05);
+extern Stage A(trd_05b);
+extern Stage A(trd_05c);
+extern Stage A(trd_05d);
+extern Stage A(trd_05e);
+extern Stage A(trd_05f);
 
-extern Stage N(trd_00);
-extern Stage N(trd_01);
-extern Stage N(trd_02);
-extern Stage N(trd_02b);
-extern Stage N(trd_03);
-extern Stage N(trd_04);
-extern Stage N(trd_05);
-extern Stage N(trd_05b);
-extern Stage N(trd_05c);
-extern Stage N(trd_05d);
-extern Stage N(trd_05e);
-extern Stage N(trd_05f);
-
-Formation N(Formation_00) = {
-    ACTOR_BY_IDX(N(green_ninja_koopa), BTL_POS_GROUND_A, 10),
-    ACTOR_BY_IDX(N(yellow_ninja_koopa), BTL_POS_GROUND_A, 9),
-    ACTOR_BY_IDX(N(black_ninja_koopa), BTL_POS_GROUND_A, 8),
-    ACTOR_BY_IDX(N(red_ninja_koopa), BTL_POS_GROUND_A, 7),
-    ACTOR_BY_IDX(N(fake_bowser), BTL_POS_GROUND_D, 6),
+Formation A(Formation_00) = {
+    ACTOR_BY_IDX(A(green_ninja_koopa), BTL_POS_GROUND_A, 10),
+    ACTOR_BY_IDX(A(yellow_ninja_koopa), BTL_POS_GROUND_A, 9),
+    ACTOR_BY_IDX(A(black_ninja_koopa), BTL_POS_GROUND_A, 8),
+    ACTOR_BY_IDX(A(red_ninja_koopa), BTL_POS_GROUND_A, 7),
+    ACTOR_BY_IDX(A(fake_bowser), BTL_POS_GROUND_D, 6),
 };
 
-BattleList N(Formations) = {
-    BATTLE(N(Formation_00), N(trd_00), "ノコブロス"),
+BattleList A(Formations) = {
+    BATTLE(A(Formation_00), A(trd_00), "ノコブロス"),
     {},
 };
 
-StageList N(Stages) = {
-    STAGE("trd_00",  N(trd_00)),
-    STAGE("trd_01",  N(trd_01)),
-    STAGE("trd_02",  N(trd_02)),
-    STAGE("trd_02b", N(trd_02b)),
-    STAGE("trd_03",  N(trd_03)),
-    STAGE("trd_04",  N(trd_04)),
-    STAGE("trd_05",  N(trd_05)),
-    STAGE("trd_05b", N(trd_05b)),
-    STAGE("trd_05c", N(trd_05c)),
-    STAGE("trd_05d", N(trd_05d)),
-    STAGE("trd_05e", N(trd_05e)),
-    STAGE("trd_05f", N(trd_05f)),
+StageList A(Stages) = {
+    STAGE("trd_00",  A(trd_00)),
+    STAGE("trd_01",  A(trd_01)),
+    STAGE("trd_02",  A(trd_02)),
+    STAGE("trd_02b", A(trd_02b)),
+    STAGE("trd_03",  A(trd_03)),
+    STAGE("trd_04",  A(trd_04)),
+    STAGE("trd_05",  A(trd_05)),
+    STAGE("trd_05b", A(trd_05b)),
+    STAGE("trd_05c", A(trd_05c)),
+    STAGE("trd_05d", A(trd_05d)),
+    STAGE("trd_05e", A(trd_05e)),
+    STAGE("trd_05f", A(trd_05f)),
     {},
 };
