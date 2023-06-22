f0a9274c0182d11ba50a399127504360a33fc065
Marlon Pohl <56829999+Keksnet@users.noreply.github.com>
2023-06-22 02:44:51 +0200

    Implemented kkj_19_5_baking_GetItemNameRaw for pal (#1066)
    * Implemented kkj_19_5_baking_GetItemNameRaw for pal
    
    * Usage of decimals instead of hexadecimals
    
    * Implemented mgm_01_2_npc SetMsgVars_BlocksRemaining
    
    * Implemented mgm_02_0_header.c
    
    * Removed .s file
    
    I'm such an idiot for forgetting this
    
    * removed implemented methods
    
    * optimized decomp
    
    * found the value for the hex number
    
    thanks to @marijnvdwerf and @Brotenko
    
    * Fixed args indexing
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    * Added empty line after definitions
    
    * Apply suggestions from code review
    
    Co-authored-by: Marijn van der Werf <marijn.vanderwerf@gmail.com>
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    
    * adjusted format (fixed compiler warning)
    
    * made review changes
    
    * removed duplicated code
    
    * namespaced pal_var
    
    ---------
    
    Co-authored-by: Ethan Roseman <ethteck@gmail.com>
    Co-authored-by: Marijn van der Werf <marijn.vanderwerf@gmail.com>

--- src/world/area_mgm/mgm_01/mgm_01_2_npc.c
+++ src/world/area_mgm/mgm_01/mgm_01_2_npc.c
@@ -656,20 +656,19 @@ API_CALLABLE(N(GetCoinCount)) {
     return ApiStatus_DONE2;
 }
 
-#if VERSION_PAL
-API_CALLABLE(N(SetMsgVars_BlocksRemaining));
-INCLUDE_ASM(ApiStatus, "world/area_mgm/mgm_01/mgm_01_2_npc", mgm_01_SetMsgVars_BlocksRemaining)
-#else
 API_CALLABLE(N(SetMsgVars_BlocksRemaining)) {
     Enemy* scorekeeper = get_enemy(SCOREKEEPER_ENEMY_IDX);
     s32 remaining = (scorekeeper->varTable[TOTAL_BLOCKS_VAR_IDX] - scorekeeper->varTable[BROKEN_BLOCKS_VAR_IDX]) + 1;
 
     set_message_value(remaining, 0);
+#if VERSION_PAL
+    evt_set_variable(script, LVarD, remaining);
+#else
     set_message_msg((remaining == 1) ? (s32)&MessageSingular : (s32)&MessagePlural, 1);
+#endif
 
     return ApiStatus_DONE2;
 }
-#endif
 
 API_CALLABLE(N(HideCoinCounter)) {
     hide_coin_counter_immediately();
--- src/world/area_mgm/mgm_02/mgm_02_0_header.c
+++ src/world/area_mgm/mgm_02/mgm_02_0_header.c
@@ -1,8 +1,14 @@
 #include "mgm_02.h"
 
 #if VERSION_PAL
-s32 N(get_tattle)(void);
-INCLUDE_ASM(void, "world/area_mgm/mgm_02/mgm_02_0_header", mgm_02_get_tattle);
+extern s32 N(pal_variable);
+s32 N(get_tattle)(void) {
+    s32 msgID = MSG_MapTattle_mgm_02;
+    if (N(pal_variable) != 0) {
+        msgID = MSG_NONE;
+    }
+    return msgID;
+}
 #endif
 
 EntryList N(Entrances) = {
