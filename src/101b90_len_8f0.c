6e2027222e050d3001b46c2844d10944925e67f1
Alex Bates <alex@nanaian.town>
2023-06-26 23:19:52 +0100

    fix SPRITE_ROM_START in --shift

--- src/101b90_len_8f0.c
+++ src/101b90_len_8f0.c
@@ -5,7 +5,7 @@
 #include "sprite/player.h"
 
 #ifdef SHIFT
-#define SPRITE_ROM_START (u32) _1943000_ROM_START + 0x10
+#define SPRITE_ROM_START (u32) sprites_ROM_START + 0x10
 #else
 #define SPRITE_ROM_START 0x1943000 + 0x10
 #endif
