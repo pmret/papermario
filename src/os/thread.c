716cba512a342ec9f0c59a577b3ac2de0ee3c943
AltoXorg <56553686+Alto1772@users.noreply.github.com>
2023-08-12 13:24:48 +0800

    os/setthreadpri(.data) -> os/thread

--- src/os/thread.c
+++ src/os/thread.c
@@ -1,4 +1,11 @@
+#include "ultra64.h"
 #include "PR/osint.h"
+
+struct __osThreadTail __osThreadTail = {0, -1};
+OSThread* __osRunQueue = (OSThread*) &__osThreadTail;
+OSThread* __osActiveQueue = (OSThread*) &__osThreadTail;
+OSThread* __osRunningThread = NULL;
+OSThread* __osFaultedThread = NULL;
 
 void __osDequeueThread(OSThread **queue, OSThread *t) {
     register OSThread *pred;
