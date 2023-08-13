d5bb0a6655b21558b9bdbbcf6564286245f8ed89
AltoXorg <56553686+Alto1772@users.noreply.github.com>
2023-08-13 16:55:10 +0800

    final blow

--- src/os/_setcompare.c
+++ src/os/_setcompare.c
@@ -7,7 +7,7 @@ extern u32 __osBbVCountWraps;
 
 void __osSetCompare(u32 v) {
     if (v != 0) {
-        register u32 mask = __osDisableInt();
+        u32 mask = __osDisableInt();
         u32 wraps = (v < __osBbLastVCount) ? __osBbVCountWraps + 1 : __osBbVCountWraps;
 
         v = (((u64)wraps << 32) | v) * 192ull / 125ull;
