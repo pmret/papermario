34033026f62b5c2cebe0a8c2a826e07bbf933eb8
Ethan Roseman <ethteck@gmail.com>
2023-12-04 17:21:33 +0900

    by gorsh I think I've nabbed it

--- tools/warnings_count/check_new_warnings.sh
+++ tools/warnings_count/check_new_warnings.sh
@@ -22,7 +22,7 @@ then
     ninja | grep warning | sort > tools/warnings_count/warnings_new.txt
 else
     touch tools/warnings_count/warnings_new.txt
-    cat build_log.txt | grep warning || [[ $? == 1 ]] | sort > tools/warnings_count/warnings_new.txt
+    cat build_log.txt | grep warning | sort > tools/warnings_count/warnings_new.txt
 fi
 
 curl -L https://papermar.io/reports/warnings.txt > tools/warnings_count/warnings.txt
