d4208bfb29c00dcb62fc79477423ee3c41f6e071
Alex Bates <alex@nanaian.town>
2024-01-21 13:07:00 +0000

    black

--- tools/build/iconv.py
+++ tools/build/iconv.py
@@ -3,11 +3,11 @@
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(description='Convert a file from one encoding to another')
+parser = argparse.ArgumentParser(description="Convert a file from one encoding to another")
 parser.add_argument("f")
 parser.add_argument("t")
-parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
-parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+parser.add_argument("infile", nargs="?", type=argparse.FileType("r"), default=sys.stdin)
+parser.add_argument("outfile", nargs="?", type=argparse.FileType("w"), default=sys.stdout)
 
 args = parser.parse_args()
 
