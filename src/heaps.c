ba4499e2a788ff78f01549dace40a2c4ad19758d
z64a <53960937+z64a@users.noreply.github.com>
2024-09-30 00:03:14 -0400

    some effect symbols (#1210)

--- src/heaps.c
+++ src/heaps.c
@@ -5,8 +5,8 @@
 typedef s32 TlbEntry[0x1000 / 4];
 typedef TlbEntry TlbMappablePage[15];
 
-BSS TlbMappablePage D_80197000 ALIGNED(0x1000);
-BSS u8 D_801A6000[0x1000] ALIGNED(0x1000);
+BSS TlbMappablePage gEffectDataBuffer ALIGNED(0x1000);
+BSS u8 gEffectGlobals[0x1000] ALIGNED(0x1000);
 BSS u8 BattleEntityHeapBottom[0x3000] ALIGNED(0x1000);
 BSS u8 AuHeapBase[AUDIO_HEAP_SIZE] ALIGNED(0x1000);
 BSS u8 D_80200000[0x4000] ALIGNED(0x1000);
